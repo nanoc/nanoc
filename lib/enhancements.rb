0f2eee969669cb5630bc71bb2858a158942c048c
Denis Defreyne <denis.defreyne@stoneship.org>
2007-04-11 09:41:53 +0000

    cleaned up enhancements; added support for templates to CLI tool; added several CLI arguments to CLI tool

--- lib/enhancements.rb
+++ lib/enhancements.rb
@@ -1,14 +1,17 @@
 class Date
+  # Formats the date in a human-readable format
   def format_nicely
     "#{Date::MONTHNAMES[mon]} #{mday}, #{year}"
   end
   
+  # Formats the date in the format required by Atom feeds
   def to_atom_date
     self.strftime("%Y-%m-%d")
   end
 end
 
 class File
+  # Reads the contents of the entire file
   def self.read_file(a_filename)
     content = ''
     File.open(a_filename) do |io|
@@ -19,24 +22,25 @@ class File
     content
   end
   
+  # Returns the contents of an entire file interpreted as YAML
   def self.read_yaml(a_filename)
     YAML::load(self.read_file(a_filename))
   end
 end
 
 class Hash
+  # Converts all keys to symbols, and converts *_at and *_on
+  # keys to Times and Dates, respectively
   def clean
     hash = {}
     
     self.each_pair do |key, value|
-      # Convert all keys to symbols
-      hash[key.to_sym] = value
-      
-      # Convert *_on and *_at to Dates and Times
       if key =~ /_on$/
         hash[key.to_sym] = Date.parse(value)
       elsif key =~ /_at$/
         hash[key.to_sym] = Time.parse(value)
+      else
+        hash[key.to_sym] = value
       end
     end
     
@@ -44,59 +48,59 @@ class Hash
   end
 end
 
-class NilClass
-  def true?
-    false
-  end
-  
-  def false?
-    true
-  end
-end
-
 class String
-  def filter!(a_filters, a_context)
+  # Runs the string through the filters as given by the array of
+  # filter names. Available filters include 'markdown' and 'eruby'.
+  def filter!(a_filters, a_params={})
     a_filters.each do |filter|
       case filter
       when 'markdown'
         self.markdown!
       when 'eruby'
-        self.eruby!(a_context)
+        self.eruby!(a_params[:eruby_context])
       end
     end
   end
   
+  # Converts the string to HTML using Markdown.
   def markdown
     BlueCloth::new(self).to_html
   end
   
+  # Converts the string to HTML using Markdown.
   def markdown!
     self.replace(self.markdown)
   end
   
+  # Converts the string using eRuby.
   def eruby(a_context)
     Erubis::Eruby.new(self).evaluate(a_context)
   end
   
+  # Converts the string using eRuby.
   def eruby!(a_context)
     self.replace(self.eruby(a_context))
   end
 end
 
 class Time
+  # Formats the time in a human-readable format
   def format_nicely
     "#{Date::MONTHNAMES[mon]} #{mday}, #{year}"
   end
   
+  # Formats the time as a date in the format required by Atom feeds
   def to_atom_date
     self.strftime("%Y-%m-%d")
   end
   
+  # Formats the time in the format required by Atom feeds
   def to_atom_time
     self.strftime("%Y-%m-%dT%H:%M:%SZ")
   end
 end
 
+# Escapes the given string to make it harmless
 def html_escape(a_string)
   a_string.gsub('&', '&amp;').gsub('<', '&lt;')
 end
