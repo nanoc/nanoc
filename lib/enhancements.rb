50e3b27538bb03974202e55d88402cb48104b168
Denis Defreyne <denis.defreyne@stoneship.org>
2007-04-25 13:04:44 +0000

    cleaned up enhancements; removed Stoneship-specific enhancements

--- lib/enhancements.rb
+++ lib/enhancements.rb
@@ -1,38 +1,16 @@
-require 'erubis'
-require 'time'
-require 'yaml'
+def try ; yield rescue nil ; end
 
-begin
-  require 'bluecloth'
-  require 'rubypants'
-rescue
-end
+try { require 'rubygems' }
+try { require 'bluecloth' }
+try { require 'rubypants' }
 
 class Array
   # Ensures that the array contains only one element
-  def ensure_single(a_params={})
-    noun    = a_params[:noun]
-    context = a_params[:context]
-
-    if self.empty?
-      puts 'ERROR: no ' + noun + ' found' + (context.nil? ? '' : " (#{context})")
-      exit
-    elsif self.size != 1
-      puts 'ERROR: multiple ' + noun + ' found' + (context.nil? ? '' : " (#{context})")
+  def ensure_single(a_noun, a_context)
+    if self.size != 1
+      puts "ERROR: expected 1 #{a_noun}, found #{self.size} (#{a_context})"
       exit
     end
-  end
-end
-
-class Date
-  # Formats the date in a human-readable format
-  def format_nicely
-    "#{Date::MONTHNAMES[mon]} #{mday}, #{year}"
-  end
-
-  # Formats the date in the format required by Atom feeds
-  def to_atom_date
-    self.strftime("%Y-%m-%d")
   end
 end
 
@@ -40,11 +18,7 @@ class File
   # Reads the contents of the entire file
   def self.read_file(a_filename)
     content = ''
-    File.open(a_filename) do |io|
-      io.each_line do |line|
-        content += line
-      end
-    end
+    File.open(a_filename) { |io| content = io.read }
     content
   end
 
@@ -56,16 +30,6 @@ class File
   # Returns the contents of an entire file interpreted as YAML and cleaned
   def self.read_clean_yaml(a_filename)
     self.read_yaml(a_filename).clean
-  end
-end
-
-class Fixnum
-  def to_mon_s
-    Date::MONTHNAMES[self]
-  end
-  
-  def to_abbr_mon_s
-    Date::ABBR_MONTHNAMES[self]
   end
 end
 
@@ -89,28 +53,18 @@ class Hash
   end
 end
 
-class Numeric
-  def ordinal
-    if (10...20).include?(self) then
-      self.to_s + 'th'
-    else
-      self.to_s + %w{th st nd rd th th th th th th}[self % 10]
-    end
-  end
-end
-
 class String
   # Runs the string through the filters as given by the array of
-  # filter names. Available filters include 'markdown' and 'eruby'.
+  # filter names. Available filters include 'markdown', 'rubypants' and 'eruby'.
   def filter!(a_filters, a_params={})
     a_filters.each do |filter|
       case filter
       when 'markdown'
-        self.markdown!
+        self.replace(self.markdown)
       when 'rubypants'
-        self.rubypants!
+        self.replace(self.rubypants)
       when 'eruby'
-        self.eruby!(a_params[:eruby_context])
+        self.replace(self.eruby(a_params[:eruby_context]))
       end
     end
   end
@@ -119,54 +73,14 @@ class String
   def markdown
     BlueCloth::new(self).to_html
   end
-
-  # Converts the string to HTML using Markdown.
-  def markdown!
-    self.replace(self.markdown)
-  end
-
+  
   # Styles the string as HTML by converting quotes, dashes, ... using RubyPants
   def rubypants
     RubyPants::new(self).to_html
-  end
-
-  # Styles the string as HTML by converting quotes, dashes, ... using RubyPants
-  def rubypants!
-    self.replace(self.rubypants)
   end
 
   # Converts the string using eRuby.
   def eruby(a_context)
     Erubis::Eruby.new(self).evaluate(a_context)
   end
-
-  # Converts the string using eRuby.
-  def eruby!(a_context)
-    self.replace(self.eruby(a_context))
-  end
 end
-
-class Time
-  # Formats the time in a human-readable format
-  def format_nicely
-    "#{Date::MONTHNAMES[mon]} #{mday}, #{year}"
-  end
-
-  # Formats the time as a date in the format required by Atom feeds
-  def to_iso8601_date
-    self.strftime("%Y-%m-%d")
-  end
-  alias to_atom_date to_iso8601_date
-
-  # Formats the time in the format required by Atom feeds
-  def to_iso8601_time
-    self.gmtime.strftime("%Y-%m-%dT%H:%M:%SZ")
-  end
-  alias to_atom_time to_iso8601_time
-end
-
-# Escapes the given string to make it harmless
-def html_escape(a_string)
-  a_string.gsub('&', '&amp;').gsub('<', '&lt;')
-end
-alias h html_escape
