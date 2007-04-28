e189cde866ad708d4fb5ae40b7c511691f70ec45
Denis Defreyne <denis.defreyne@stoneship.org>
2007-04-28 18:01:40 +0000

    fixed tiny bug which could cause some extra directories to be created sometimes

--- lib/nanoc/file_management.rb
+++ lib/nanoc/file_management.rb
@@ -5,15 +5,17 @@ class FileManager
   
   def self.create_dir(a_name)
     @@stack.push(a_name)
-    puts '     create ' + @@stack.join('/')
-    FileUtils.mkdir_p(@@stack.join('/'))
+    unless File.directory?(File.join(@@stack))
+      puts '     create ' + @@stack.join('/')
+      FileUtils.mkdir_p(@@stack.join('/'))
+    end
     yield if block_given?
     @@stack.pop
   end
   
   def self.create_file(a_name)
     path = @@stack.empty? ? a_name : @@stack.join('/') + '/' + a_name
-    FileManager.create_dir(path.sub(/\/[^\/]+$/, ''))
+    FileManager.create_dir(path.sub(/\/[^\/]+$/, '')) if @@stack.empty?
     puts "     #{File.exist?(a_name) ? 'update' : 'create'} " + path
     if block_given?
       open(path, 'w') { |io| io.write(yield) }
