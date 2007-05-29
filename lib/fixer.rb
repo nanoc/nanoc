578e68fa261507d18f93d5fb8a0a41517a7326d5
Denis Defreyne <denis.defreyne@stoneship.org>
2007-05-29 19:10:28 +0000

    added Nanoc::Fixer.check_site method; fix_site now requires a valid nanoc site
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/trunk%40150

--- lib/fixer.rb
+++ lib/fixer.rb
@@ -1,7 +1,17 @@
 module Nanoc
   class Fixer
 
+    def self.check_site
+      Nanoc::Application.ensure_in_site
+
+      Dir["content/**/index.*"].each do |file|
+        puts 'Needs fixing: ' + file
+      end
+    end
+
     def self.fix_site
+      Nanoc::Application.ensure_in_site
+
       Dir["content/**/index.*"].each do |file|
         FileManager.rename_file(file, file.sub(/\/([^\/]+)\/index\./, '/\1/\1.'))
       end
