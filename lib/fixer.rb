63eb28c2d5fefeaff52b3d75dffb7fcbf111e017
Denis Defreyne <denis.defreyne@stoneship.org>
2007-05-30 08:45:25 +0000

    fixer now checks/creates assets directory
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/trunk%40154

--- lib/fixer.rb
+++ lib/fixer.rb
@@ -4,16 +4,28 @@ module Nanoc
     def self.check_site
       Nanoc::Application.ensure_in_site
 
+      # Check for old-style index files
       Dir["content/**/index.*"].each do |file|
-        puts 'Needs fixing: ' + file
+        puts 'Needs renaming: ' + file
+      end
+
+      # Check whether assets directory exists
+      unless File.directory?('assets')
+        puts 'Needs creating: assets'
       end
     end
 
     def self.fix_site
       Nanoc::Application.ensure_in_site
 
+      # Move blah/index.* to blah/blah.*
       Dir["content/**/index.*"].each do |file|
         FileManager.rename_file(file, file.sub(/\/([^\/]+)\/index\./, '/\1/\1.'))
+      end
+
+      # Create assets directory if non-existant
+      unless File.directory?('assets')
+        FileMananger.create_dir 'assets'
       end
     end
 
