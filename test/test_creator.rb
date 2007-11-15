c5f5aade1b647ab60e2cbfd2418ec254f3e25f03
Denis Defreyne <denis.defreyne@stoneship.org>
2007-11-15 09:40:01 +0000

    moved relevant parts of test_creator.rb into test_site.rb (will be cleaned up later)
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40498

--- test/test_creator.rb
+++ test/test_creator.rb
@@ -53,46 +53,4 @@ class CreatorTest < Test::Unit::TestCase
     FileUtils.cd('..')
   end
 
-  def test_create_page
-    FileUtils.cd('tmp')
-    $nanoc_creator.create_site('site')
-    FileUtils.cd('site')
-
-    assert_nothing_raised()   { $nanoc_site = Nanoc::Site.from_cwd }
-
-    assert_nothing_raised()   { $nanoc_site.create_page('test') }
-    assert_raise(SystemExit)  { $nanoc_site.create_page('test') }
-
-    assert_nothing_raised()   { $nanoc_site.create_page('foo/bar') }
-    assert_raise(SystemExit)  { $nanoc_site.create_page('foo/bar') }
-
-    assert(File.directory?('content/test/'))
-    assert(File.file?('content/test/test.txt'))
-    assert(File.file?('content/test/meta.yaml'))
-
-    assert(File.directory?('content/foo/bar/'))
-    assert(File.file?('content/foo/bar/bar.txt'))
-    assert(File.file?('content/foo/bar/meta.yaml'))
-
-    FileUtils.cd('..')
-    FileUtils.cd('..')
-  end
-
-  def test_create_template
-    FileUtils.cd('tmp')
-    $nanoc_creator.create_site('site')
-    FileUtils.cd('site')
-  
-    assert_nothing_raised()   { $nanoc_site = Nanoc::Site.from_cwd }
-    assert_nothing_raised()   { $nanoc_site.create_template('test') }
-    assert_raise(SystemExit)  { $nanoc_site.create_template('test') }
-
-    assert(File.directory?('templates/test/'))
-    assert(File.file?('templates/test/test.txt'))
-    assert(File.file?('templates/test/meta.yaml'))
-
-    FileUtils.cd('..')
-    FileUtils.cd('..')
-  end
-
 end
