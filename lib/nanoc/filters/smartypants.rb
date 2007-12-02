64bc5b137bf73ee9b421e81a074ae0687449efc9
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-02 14:17:54 +0000

    adding updated Haml, Markdown/BlueCloth and SmartyPants/RubyPants filters
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40554

---
+++ lib/nanoc/filters/smartypants.rb
@@ -0,0 +1,13 @@
+module Nanoc::Filter::SmartyPants
+  class SmartyPantsFilter < Nanoc::Filter
+
+    identifiers :smartypants, :rubypants
+
+    def run(content)
+      nanoc_require 'rubypants'
+
+      RubyPants.new(content).to_html
+    end
+
+  end
+end
