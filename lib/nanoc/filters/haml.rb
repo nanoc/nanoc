f802b35212289ffe7283b2f130623dc9cd47f491
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-02 14:17:54 +0000

    adding updated Haml, Markdown/BlueCloth and SmartyPants/RubyPants filters

---
+++ lib/nanoc/filters/haml.rb
@@ -0,0 +1,16 @@
+module Nanoc::Filter::Haml
+  class HamlFilter < Nanoc::Filter
+
+    identifiers :haml
+
+    def run(content)
+      nanoc_require 'haml'
+
+      options = @page[:haml_options] || {}
+      options[:locals] = { :page => @page, :pages => @pages, :config => @config, :site => @site }
+
+      ::Haml::Engine.new(content, options).to_html
+    end
+
+  end
+end
---
+++ lib/nanoc/filters/markdown.rb
@@ -0,0 +1,13 @@
+module Nanoc::Filter::Markdown
+  class MarkdownFilter < Nanoc::Filter
+
+    identifiers :markdown, :bluecloth
+
+    def run(content)
+      nanoc_require 'bluecloth'
+
+      BlueCloth.new(content).to_html
+    end
+
+  end
+end
