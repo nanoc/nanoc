1f87ad39061f3be9494964ba5e3ee353f5a12b2c
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-04 15:08:37 +0000

    pages can now be compiled independently, i.e. the compiler no longer has filter-pre/layout/filter-post stages
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40560

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -7,85 +7,31 @@ module Nanoc
       @site = site
     end
 
-    def run
+    def run(pages=nil)
       # Require all Ruby source files in lib/
       eval(@site.code, $nanoc_binding)
 
       # Create output directory if necessary
       FileUtils.mkdir_p(@site.config[:output_dir])
 
-      # Filter, layout, and filter again
-      filter(:pre)
-      layout
-      filter(:post)
-
-      # Save pages
-      write_pages
-    end
-
-  private
-
-    # Main methods
-
-    def filter(stage)
       # Reinit
       @stack = []
 
-      # Prepare pages
-      @site.pages.each do |page|
-        page.stage        = stage
-        page.is_filtered  = false
-      end
+      # Get pages
+      pages = @site.pages if pages.nil?
 
       # Give feedback
-      print_immediately "Filtering pages #{stage == :pre ? '(first pass) ' : '(second pass)'} "
+      puts "Compiling site..." unless $quiet
       time_before = Time.now
 
-      # Filter each page
-      @site.pages.each do |page|
-        print_immediately '.'
-        begin
-          page.filter
-        rescue => exception
-          handle_exception(exception, "filtering page '#{page.path}'")
-        end
+      # Compile pages
+      pages.each do |page|
+        page.compile
+        page.write unless page.skip_output?
       end
 
       # Give feedback
-      print_immediately " [#{format('%.2f', Time.now - time_before)}s]\n"
-    end
-
-    def layout
-      # Give feedback
-      print_immediately 'Layouting pages               '
-      time_before = Time.now
-
-      # Layout each page
-      @site.pages.reject { |page| page.skip_output? }.each do |page|
-        print_immediately '.'
-        begin
-          page.layout
-        rescue => exception
-          handle_exception(exception, "layouting page '#{page.path}' in layout '#{page.layout[:name]}'")
-        end
-      end
-
-      # Give feedback
-      print_immediately ' ' * @site.pages.select { |page| page.skip_output? }.size
-      print_immediately " [#{format('%.2f', Time.now - time_before)}s]\n"
-    end
-
-    def write_pages
-      @site.pages.reject { |page| page.skip_output? }.each do |page|
-        FileManager.create_file(page.path_on_filesystem) { page.content }
-      end
-    end
-
-    # Helper methods
-
-    def print_immediately(text)
-      print text unless $quiet
-      $stdout.flush
+      puts "Site compiled in #{format('%.2f', Time.now - time_before)}s." unless $quiet
     end
 
   end
