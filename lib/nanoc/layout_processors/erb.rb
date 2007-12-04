f04efffd7f9acd1ca929bc5a4a37308b5a55c96d
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-04 13:18:02 +0000

    moving #render into enhancements, since it is not ERB-specific
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40557

--- lib/nanoc/layout_processors/erb.rb
+++ lib/nanoc/layout_processors/erb.rb
@@ -32,10 +32,3 @@ module Nanoc::LayoutProcessor::ERBLayoutProcessor
   end
 
 end
-
-def render(name, context={})
-  layout = @site.layouts.find { |l| l[:name] == name }
-  layout_processor_class = Nanoc::PluginManager.layout_processor_for_extension(layout[:extension])
-  layout_processor = layout_processor_class.new(@page, @pages, @site.config, @site)
-  layout_processor.run(layout[:content])
-end
