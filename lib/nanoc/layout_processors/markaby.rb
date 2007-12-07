c88b162c8b3b655beaed52cd01c3c6b3797b9313
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-07 11:35:34 +0000

    added Markaby layout processor
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40570

---
+++ lib/nanoc/layout_processors/markaby.rb
@@ -0,0 +1,16 @@
+module Nanoc::LayoutProcessor::Markaby
+  class MarkabyLayoutProcessor < Nanoc::LayoutProcessor
+
+    identifiers :markaby
+    extensions  '.mab'
+
+    def run(layout)
+      nanoc_require 'markaby'
+
+      assigns = { :page => @page, :pages => @pages, :config => @config, :site => @site }
+
+      Markaby::Builder.new(assigns).instance_eval(layout).to_s
+    end
+
+  end
+end
