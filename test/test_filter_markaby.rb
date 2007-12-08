0991c2be2acea6fcacecfb1af0b9b9f71af42d27
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-08 17:44:33 +0000

    adding test stubs

---
+++ test/test_filter_markaby.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+require File.join(File.dirname(__FILE__), 'helper.rb')
+
+class FilterMarkabyTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_filter
+    # TODO implement
+  end
+
+end
---
+++ test/test_layout_processor_markaby.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+require File.join(File.dirname(__FILE__), 'helper.rb')
+
+class LayoutProcessorMarkabyTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_layout_processor
+    # TODO implement
+  end
+
+end
