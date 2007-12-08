8940ad9b1ae8b19e6459a0119812ec5c6c872c04
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-08 17:44:33 +0000

    adding test stubs
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40576

---
+++ test/test_layout_processor_haml.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+require File.join(File.dirname(__FILE__), 'helper.rb')
+
+class LayoutProcessorHamlTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_layout_processor
+    # TODO implement
+  end
+
+end
