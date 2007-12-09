7acc1a0325b039369ed9f6b3f9e92acc7192ac18
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-09 12:35:53 +0000

    pages are now written to disk as soon as they have been compiled
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40580

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -14,21 +14,13 @@ module Nanoc
       # Create output directory if necessary
       FileUtils.mkdir_p(@site.config[:output_dir])
 
-      # Reinit
-      @stack = []
-
-      # Get pages
-      real_pages = pages.nil? ? @site.pages : pages
-
       # Give feedback
       puts "Compiling #{pages.nil? ? 'site' : 'pages'}..." unless $quiet
       time_before = Time.now
 
       # Compile pages
-      real_pages.each do |p|
-        p.compile
-        p.write unless p.skip_output?
-      end
+      @stack = []
+      (pages.nil? ? @site.pages : pages).each { |page| page.compile }
 
       # Give feedback
       puts "#{pages.nil? ? 'Site' : 'Pages'} compiled in #{format('%.2f', Time.now - time_before)}s." unless $quiet
