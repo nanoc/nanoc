b913cd4afd1363a1e12130077e5069799072298c
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-11 11:02:28 +0000

    removing String#starts_with since it isn't used anywhere

--- test/test_core_ext_string.rb
+++ test/test_core_ext_string.rb
@@ -7,15 +7,6 @@ class CoreExtStringTest < Test::Unit::TestCase
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
 
-  def test_starts_with
-    assert('bar'.starts_with?('b'))
-    assert('bar'.starts_with?('ba'))
-    assert('bar'.starts_with?('bar'))
-    assert(!'bar'.starts_with?('barasdf'))
-    assert(!'bar'.starts_with?('asdfbar'))
-    assert(!'bar'.starts_with?('xyzzy'))
-  end
-
   def test_ends_with
     assert('bar'.ends_with?('r'))
     assert('bar'.ends_with?('ar'))
