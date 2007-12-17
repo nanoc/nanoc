bf8aeb2000837f0dd8ac6b9b3c13c70d78ec0b52
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-17 18:18:11 +0000

    autocompiler now displays useful 404 and 500 error pages

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -24,6 +24,18 @@ module Nanoc
 
       # Give feedback
       puts "#{page.nil? ? 'Site' : 'Pages'} compiled in #{format('%.2f', Time.now - time_before)}s." unless $quiet
+    rescue => exception
+      if page.nil?
+        unless $quiet or exception.class == SystemExit
+          $stderr.puts "ERROR: Exception occured while compiling #{page.path}:\n"
+          $stderr.puts '  ' + exception.message
+          $stderr.puts 'Backtrace:'
+          $stderr.puts exception.backtrace.map { |t| '  - ' + t }.join("\n")
+        end
+        exit(1)
+      else
+        raise exception
+      end
     end
 
   end
