da777766a4be4ee8194b986ce1c10c95c5747028
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-17 21:03:17 +0000

    improved error reporting and fixed a bug which would mess up error messages during normal (non-auto) compilation
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40601

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -20,22 +20,36 @@ module Nanoc
 
       # Compile
       @stack = []
-      (page.nil? ? @site.pages : [ page ]).each { |p| p.compile }
+      (page.nil? ? @site.pages : [ page ]).each do |current_page|
+        begin
+          current_page.compile
+        rescue => exception
+          handle_exception(exception, current_page, !page.nil?)
+        end
+      end
 
       # Give feedback
       puts "#{page.nil? ? 'Site' : 'Pages'} compiled in #{format('%.2f', Time.now - time_before)}s." unless $quiet
-    rescue => exception
-      if page.nil?
-        unless $quiet or exception.class == SystemExit
-          $stderr.puts "ERROR: Exception occured while compiling #{page.path}:\n"
-          $stderr.puts '  ' + exception.message
-          $stderr.puts 'Backtrace:'
-          $stderr.puts exception.backtrace.map { |t| '  - ' + t }.join("\n")
-        end
-        exit(1)
-      else
+    end
+
+    def handle_exception(exception, page, single_page)
+      if single_page
         raise exception
+        return
       end
+
+      unless $quiet or exception.class == SystemExit
+        $stderr.puts "ERROR: An exception occured while compiling page #{page.path}."
+        $stderr.puts
+        $stderr.puts "If you think this is a bug in nanoc, please do report it at"
+        $stderr.puts "<http://nanoc.stoneship.org/trac/newticket> -- thanks!"
+        $stderr.puts
+        $stderr.puts 'Message:'
+        $stderr.puts '  ' + exception.message
+        $stderr.puts 'Backtrace:'
+        $stderr.puts exception.backtrace.map { |t| '  - ' + t }.join("\n")
+      end
+      exit(1)
     end
 
   end
