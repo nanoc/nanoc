89151bd2d399da8416c2bc63346422d0315b24a2
Denis Defreyne <denis.defreyne@stoneship.org>
2008-01-10 09:50:18 +0000

    removing 'eruby' filter--use 'ERB' instead ('eruby' is too ambiguous, just like 'markdown' or 'textile')
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40688

--- lib/nanoc/filters/old.rb
+++ lib/nanoc/filters/old.rb
@@ -1,12 +1,12 @@
 module Nanoc::Filter::Old
   class OldFilter < Nanoc::Filter
 
-    identifiers :markdown, :textile
+    identifiers :markdown, :textile, :eruby
 
     def run(content)
-      error "The 'markdown' and 'textile' filters no longer exist. " +
-            "Instead, use 'bluecloth' for Markdown, and 'redcloth' " +
-            "for Markdown or Textile formatting."
+      error "The 'markdown', 'textile' and 'eruby' filters no longer exist. " +
+            "Instead, use 'bluecloth' for Markdown, 'redcloth' " +
+            "for Markdown or Textile, and 'erb' for eRuby."
     end
 
   end
