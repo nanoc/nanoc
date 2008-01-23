d0940f1df9bcf8268032e50efafab088f5394472
Denis Defreyne <denis.defreyne@stoneship.org>
2008-01-23 12:12:23 +0000

    removing all unnecessary methods from PageProxy and documenting the class
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/nanoc/trunk%40714

--- lib/nanoc/base/page_proxy.rb
+++ lib/nanoc/base/page_proxy.rb
@@ -1,10 +1,19 @@
 module Nanoc
+
+  # Nanoc::PageProxy is a proxy object for a Nanoc::Page object, used when
+  # running filters and layout processors.
   class PageProxy
 
+    instance_methods.each { |m| undef_method m unless m =~ /^__/ }
+
+    # Creates a page proxy for the given page.
     def initialize(page)
       @page = page
     end
 
+    # Requests the attribute with the given name. +key+ can be a string or a
+    # symbol, and it can contain a trailing question mark (which will be
+    # stripped).
     def [](key)
       real_key = key.to_s.sub(/\?$/, '').to_sym
 
@@ -19,13 +28,18 @@ module Nanoc
       end
     end
 
+    # Sets a given attribute. The use of setting a page's attributes is not
+    # recommended but may be necessary in some cases.
     def []=(key, value)
       @page.attributes[key.to_sym] = value
     end
 
+    # Used for requesting attributes without accessing the page proxy like a
+    # hash.
     def method_missing(method, *args)
       self[method]
     end
 
   end
+
 end
