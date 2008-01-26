3f7de5c5b805459a77fa9a49062abae7483291d8
Denis Defreyne <denis.defreyne@stoneship.org>
2008-01-26 10:21:20 +0000

    making nanoc compatible with Ruby 1.8.5 (used to require 1.8.6)

--- lib/nanoc/base/plugin.rb
+++ lib/nanoc/base/plugin.rb
@@ -6,12 +6,12 @@ module Nanoc
     end
 
     def self.identifiers(*identifiers)
-      self._identifiers = [] unless instance_variable_defined?(:@_identifiers)
+      self._identifiers = [] unless instance_variables.include?('@_identifiers')
       identifiers.empty? ? self._identifiers || [] : self._identifiers = (self._identifiers || []) + identifiers
     end
 
     def self.identifier(identifier=nil)
-      self._identifiers = [] unless instance_variable_defined?(:@_identifiers)
+      self._identifiers = [] unless instance_variables.include?('@_identifiers')
       identifier.nil? ? self.identifiers.first : self.identifiers(identifier)
     end
 
