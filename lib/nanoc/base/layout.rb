afc07a5e4b4534656230bd92f0c5955c190ce977
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-10 13:08:19 +0200

    layouts now have a mtime

--- lib/nanoc/base/layout.rb
+++ lib/nanoc/base/layout.rb
@@ -10,15 +10,35 @@ module Nanoc
     attr_accessor :site
 
     # Creates a new layout.
-    def initialize(content, attributes, path)
+    def initialize(content, attributes, path, mtime=nil)
       @content    = content
       @attributes = attributes
       @path       = path.cleaned_path
+      @mtime      = mtime
     end
 
     # Returns a proxy for this layout.
     def to_proxy
       @proxy ||= LayoutProxy.new(self)
+    end
+
+    # Returns true if there exists a compiled page that is older than the
+    # lyaout, false otherwise.
+    def outdated?
+      # Outdated if we don't know
+      return true if @mtime.nil?
+
+      # Get pages for this layout
+      pages = @site.pages.select { |p| p.attribute_named(:layout).cleaned_path == @path }
+
+      # Get mtimes for pages for this layout
+      compiled_page_mtimes = pages.map do |page|
+        path = page.path_on_filesystem
+        File.file?(path) ? File.stat(path).mtime : nil
+      end.compact
+
+      # Check if there are any newer pages
+      compiled_page_mtimes.any? { |compiled_page_mtime| @mtime > compiled_page_mtime }
     end
 
     # Returns the attribute with the given name.
