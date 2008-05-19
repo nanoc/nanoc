10351d793f02a5f61bf8d3381b9ecfc0b67e6ad8
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-19 18:12:53 +0200

    adding missing router test

---
+++ test/test_router.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+
+require File.join(File.dirname(__FILE__), 'helper.rb')
+
+class RouterTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_paths_for
+    in_dir %w{ tmp } do
+      Nanoc::Site.create('site')
+
+      in_dir %w{ site } do
+        # Create site
+        site = Nanoc::Site.new(YAML.load_file('config.yaml'))
+        site.load_data
+
+        # Create page with normal path
+        foo_page = Nanoc::Page.new(
+          "Hello, I am Foo!",
+          { },
+          '/foo/'
+        )
+        foo_page.site = site
+
+        # Create page with custom path
+        bar_page = Nanoc::Page.new(
+          "Hello, I am Bar!",
+          { :custom_path => '/quack/zomg.html' },
+          '/bar/'
+        )
+        bar_page.site = site
+
+        # Check normal page paths
+        assert_equal('/foo/',             site.router.web_path_for(foo_page))
+        assert_equal('/foo/index.html',   site.router.disk_path_for(foo_page))
+
+        # Check custom page paths
+        assert_equal('/quack/zomg.html',  site.router.web_path_for(bar_page))
+        assert_equal('/quack/zomg.html',  site.router.disk_path_for(bar_page))
+      end
+
+    end
+  end
+
+end
