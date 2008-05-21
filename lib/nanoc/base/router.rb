2301898afcd27008f6a47bef547427df086217c6
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-21 21:03:54 +0200

    made Record an abstract superclass; added DefaultRouter

--- lib/nanoc/base/router.rb
+++ lib/nanoc/base/router.rb
@@ -1,11 +1,9 @@
 module Nanoc
 
-  # TODO turn Router into a Plugin subclass and give it an identifier
-
   # A router determines the paths of pages, both the path on the disk
   # (relative to the site's output directory) and the path as it appears on
   # the web site.
-  class Router
+  class Router < Plugin
 
     # Creates a new router for the given site.
     def initialize(site)
@@ -15,21 +13,13 @@ module Nanoc
     # Returns the path of the page on the disk, relative to the output
     # directory. It should start with a slash.
     def disk_path_for(page)
-      if page.attribute_named(:custom_path).nil?
-        # Get data we need
-        filename   = page.attribute_named(:filename)
-        extension  = page.attribute_named(:extension)
-
-        "#{page.path}#{filename}.#{extension}"
-      else
-        page.attribute_named(:custom_path)
-      end
+      raise NotImplementedError.new("Nanoc::Router subclasses must implement #disk_path_for.")
     end
 
     # Returns the path of the page relative to the web root. It should start
     # with a slash.
     def web_path_for(page)
-      page.attribute_named(:custom_path) || page.path
+      raise NotImplementedError.new("Nanoc::Router subclasses must implement #web_path_for.")
     end
 
   end
