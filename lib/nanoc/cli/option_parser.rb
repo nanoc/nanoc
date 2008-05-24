dac3d34434416c8c5c99ce79e8967d693b930208
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-24 15:45:50 +0200

    improved option parser error handling; cleanup

--- lib/nanoc/cli/option_parser.rb
+++ lib/nanoc/cli/option_parser.rb
@@ -1,65 +1,75 @@
-module Nanoc
+module Nanoc::CLI
 
-  module OptionParser
+  class OptionParser
 
-    class IllegalOptionError < RuntimeError
+    class IllegalOptionError < RuntimeError             ; end
+    class OptionRequiresAnArgumentError < RuntimeError  ; end
 
-      def initialize(option)
-        super("illegal option -- #{option}")
-      end
+    def self.parse(arguments_and_options, definitions)
+      # Don't touch original argument
+      unprocessed_arguments_and_options = arguments_and_options.dup
 
-    end
+      # Initialize 
+      arguments = []
+      options   = {}
 
-    class OptionRequiresAnArgumentError < RuntimeError
+      # Determines whether we've passed the '--' marker or not
+      no_more_options = false
 
-      def initialize(option)
-        super("option requires an argument -- #{option}")
-      end
+      loop do
+        # Get next item
+        e = unprocessed_arguments_and_options.shift
+        break if e.nil?
 
-    end
+        # Handle end-of-options marker
+        if e == '--'
+          no_more_options = true
+        # Handle incomplete options
+        elsif e =~ /^--./ and !no_more_options
+          # Get option key, and option value if included
+          if e =~ /^--([^=]+)=(.+)$/
+            option_key   = $1
+            option_value = $2
+          else
+            option_key    = e[2..-1]
+            option_value  = nil
+          end
 
-    class Base
+          # Find definition
+          definition = definitions.find { |d| d[:long] == option_key }
+          raise IllegalOptionError.new(option_key) if definition.nil?
 
-      def self.parse(arguments_and_options, definitions, ignore_illegal_options=false)
-        # Don't touch original argument
-        unprocessed_arguments_and_options = arguments_and_options.dup
-
-        # Initialize 
-        arguments = []
-        options   = {}
-
-        # Determines whether we've passed the '--' marker or not
-        no_more_options = false
-
-        loop do
-          # Get next item
-          e = unprocessed_arguments_and_options.shift
-          break if e.nil?
-
-          # Handle end-of-options marker
-          if e == '--'
-            no_more_options = true
-          # Handle incomplete options
-          elsif e =~ /^--./ and !no_more_options
-            # Get option key, and option value if included
-            if e =~ /^--([^=]+)=(.+)$/
-              option_key   = $1
-              option_value = $2
-            else
-              option_key    = e[2..-1]
-              option_value  = nil
+          if definition[:argument] == :required
+            # Get option value if necessary
+            if option_value.nil?
+              option_value = unprocessed_arguments_and_options.shift
+              raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
             end
 
-            # Find definition
-            definition = definitions.find { |d| d[:long] == option_key }
-            raise IllegalOptionError.new(option_key) if definition.nil? and !ignore_illegal_options
+            # Store option
+            options[definition[:long].to_sym] = option_value
+          else
+            # Store option
+            options[definition[:long].to_sym] = nil
+          end
+        # Handle -xyz options
+        elsif e =~ /^-./ and !no_more_options
+          # Get option keys
+          option_keys = e[1..-1].scan(/./)
 
-            if definition[:argument] == :required
-              # Get option value if necessary
-              if option_value.nil?
-                option_value = unprocessed_arguments_and_options.shift
-                raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
-              end
+          # For each key
+          option_keys.each do |option_key|
+            # Find definition
+            definition = definitions.find { |d| d[:short] == option_key }
+            raise IllegalOptionError.new(option_key) if definition.nil?
+
+            if option_keys.length > 1 and definition[:argument] == :required
+              # This is a combined option and it requires an argument, so complain
+              raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
+            elsif definition[:argument] == :required
+              # Get option value
+              option_value = unprocessed_arguments_and_options.shift
+              raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
 
               # Store option
               options[definition[:long].to_sym] = option_value
@@ -67,71 +77,16 @@ module Nanoc
               # Store option
               options[definition[:long].to_sym] = nil
             end
-          # Handle -xyz options
-          elsif e =~ /^-./ and !no_more_options
-            # Get option keys
-            option_keys = e[1..-1].scan(/./)
-
-            # For each key
-            option_keys.each do |option_key|
-              # Find definition
-              definition = definitions.find { |d| d[:short] == option_key }
-              raise IllegalOptionError.new(option_key) if definition.nil? and !ignore_illegal_options
-
-              if option_keys.length > 1 and definition[:argument] == :required
-                # This is a combined option and it requires an argument, so complain
-                raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
-              elsif definition[:argument] == :required
-                # Get option value
-                option_value = unprocessed_arguments_and_options.shift
-                raise OptionRequiresAnArgumentError.new(option_key) if option_value.nil?
-
-                # Store option
-                options[definition[:long].to_sym] = option_value
-              else
-                # Store option
-                options[definition[:long].to_sym] = nil
-              end
-            end
-          # Handle normal arguments
-          else
-            arguments << e
           end
+        # Handle normal arguments
+        else
+          arguments << e
         end
-
-        { :options => options, :arguments => arguments }
       end
 
+      { :options => options, :arguments => arguments }
     end
 
   end
 
 end
-
-
-
-# arguments_and_options = %w( aaa bbb --foo xxx -b yyy ccc -pq ddd -z gah -- --foo=abc --wrong -unknown test )
-# 
-# definitions = [
-#   { :long => 'foo',    :short => 'f', :argument => :required  },
-#   { :long => 'bar',    :short => 'b', :argument => :required  },
-#   { :long => 'quux',   :short => 'q', :argument => :forbidden },
-#   { :long => 'ploink', :short => 'p', :argument => :forbidden },
-#   { :long => 'zoink',  :short => 'z', :argument => :required  }
-# ]
-# 
-# result = Nanoc::OptionParser.parse(arguments_and_options, definitions)
-# 
-# puts "OPTIONS:"
-# puts "    " + result[:options].inspect
-# puts
-# puts "ARGUMENTS:"
-# puts "    " + result[:arguments].inspect
-
-
-
-# OPTIONS:
-#     {"quux"=>nil, "ploink"=>nil, "foo"=>"xxx", "bar"=>"yyy", "zoink"=>"gah"}
-#
-# ARGUMENTS:
-#     ["aaa", "bbb", "ccc", "ddd", "--foo=abc", "--wrong", "-unknown", "test"]
