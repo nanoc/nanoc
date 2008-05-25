b126a3220ef8e37107c4c06d97e8ebf174b4bc61
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-25 18:46:45 +0200

    adding incomplete and untested active_record_database data source
    This data source is similar to the old database data source with a few minor differences that make the new data source incompatible with the previous version (which will be fixed).

---
+++ test/data_sources/test_active_record_database.rb
@@ -0,0 +1,88 @@
+require 'helper'
+
+class Nanoc::DataSources::ActiveRecordDatabaseTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  # Test preparation
+
+  def test_setup
+    # TODO implement
+  end
+
+  # Test pages
+
+  def test_pages
+    # TODO implement
+  end
+
+  def test_save_page
+    # TODO implement
+  end
+
+  def test_move_page
+    # TODO implement
+  end
+
+  def test_delete_page
+    # TODO implement
+  end
+
+  # Test page defaults
+
+  def test_page_defaults
+    # TODO implement
+  end
+
+  def test_save_page_defaults
+    # TODO implement
+  end
+
+  # Test templates
+
+  def test_templates
+    # TODO implement
+  end
+
+  def test_save_template
+    # TODO implement
+  end
+
+  def test_move_template
+    # TODO implement
+  end
+
+  def test_delete_template
+    # TODO implement
+  end
+
+  # Test layouts
+
+  def test_layouts
+    # TODO implement
+  end
+
+  def test_save_layout
+    # TODO implement
+  end
+
+  def test_move_layout
+    # TODO implement
+  end
+
+  def test_delete_layout
+    # TODO implement
+  end
+
+  # Test code
+
+  def test_code
+    # TODO implement
+  end
+
+  def test_save_code
+    # TODO implement
+  end
+
+end
