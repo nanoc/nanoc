856c08c94a6d1e52c4142849967caa2e317a32f7
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-28 10:53:24 +0200

    completing filesystem and filesystem2 data source documentation

--- lib/nanoc/data_sources/filesystem.rb
+++ lib/nanoc/data_sources/filesystem.rb
@@ -1,15 +1,83 @@
 module Nanoc::DataSources
 
+  # The filesystem data source is the default data source for a new nanoc
+  # site. It stores all data as files on the hard disk.
+  #
+  # None of the methods are documented in this file. See Nanoc::DataSource for
+  # documentation on the overridden methods instead.
+  #
+  # = Pages
+  #
+  # The filesystem data source stores its pages in nested directories. Each
+  # directory represents a single page. The root directory is the 'content'
+  # directory.
+  #
+  # Every directory has a content file and a meta file. The content file
+  # contains the actual page content, while the meta file contains the page's
+  # metadata, formatted as YAML.
+  #
+  # Both content files and meta files are named after its parent directory
+  # (i.e. page). For example, a page named 'foo' will have a directorynamed
+  # 'foo', with e.g. a 'foo.markdown' content file and a 'foo.yaml' meta file.
+  #
+  # Content file extensions are ignored by nanoc. The content file extension
+  # does not determine the filters to run on it; the meta file defines the
+  # list of filters. The meta file extension must always be 'yaml', though.
+  #
+  # Content files can also have the 'index' basename. Similarly, meta files
+  # can have the 'meta' basename. For example, a parent directory named 'foo'
+  # can have an 'index.txt' content file and a 'meta.yaml' meta file. This is
+  # to preserve backward compatibility.
+  #
+  # = Page defaults
+  #
+  # The page defaults are loaded from a YAML-formatted file named 'meta.yaml'
+  # file at the top level of the nanoc site directory.
+  #
+  # = Layouts
+  #
+  # Layouts are stored as directories in the 'layouts' directory. Each layout
+  # contains a content file and a meta file. The content file contain the
+  # actual layout, and the meta file describes how the page should be handled
+  # (contains the filter that should be used).
+  #
+  # For backward compatibility, a layout can also be a single file in the
+  # 'layouts' directory. Such a layout cannot have any metadata; the filter
+  # used for this layout is determined from the file extension.
+  #
+  # = Templates
+  #
+  # Templates are located in the 'templates' directroy. Every template is a
+  # directory consisting of a content file and a meta file, both named after
+  # the template. This is very similar to the way pages are stored, except
+  # that templates cannot be nested.
+  #
+  # = Code
+  #
+  # Code is stored in '.rb' files in the 'lib' directory. Code can reside in
+  # sub-directories.
   class Filesystem < Nanoc::DataSource
 
+    # A FileProxy is a proxy for a File object. It is used to prevent a File
+    # object from being created until it is actually necessary.
+    #
+    # For example, a site with a few thousand pages would fail to compile
+    # because the massive amount of file descriptors necessary, but the file
+    # proxy will make sure the File object is not created until it is used.
     class FileProxy
 
       instance_methods.each { |m| undef_method m unless m =~ /^__/ }
 
+      # Creates a new file proxy for the given path. This is similar to
+      # creating a File object with the same path, except that the File object
+      # will not be created until it is accessed.
       def initialize(path)
         @path = path
       end
 
+      # Makes sure all method calls are relayed to a File object, which will
+      # be created right before the method call takes place and destroyed
+      # right after.
       def method_missing(sym, *args, &block)
         File.new(@path).__send__(sym, *args, &block)
       end
@@ -22,13 +90,13 @@ module Nanoc::DataSources
 
     ########## Preparation ##########
 
-    def up
+    def up # :nodoc:
     end
 
-    def down
+    def down # :nodoc:
     end
 
-    def setup
+    def setup # :nodoc:
       # Create page
       FileUtils.mkdir_p('content')
 
@@ -49,7 +117,7 @@ module Nanoc::DataSources
       populate { |i| yield i }
     end
 
-    def populate
+    def populate # :nodoc:
       # Create page
       FileUtils.mkdir_p('content')
       File.open('content/content.txt', 'w') do |io|
@@ -130,29 +198,7 @@ module Nanoc::DataSources
 
     ########## Pages ##########
 
-    # The filesystem data source stores its pages in nested directories.
-    # Each directory represents a single page. The root directory is the
-    # 'content' directory.
-    #
-    # Every directory has a content file and a meta file. The content file
-    # contains the actual page content, while the meta file contains the
-    # page's metadata.
-    #
-    # Both content files and meta files are named after its parent directory
-    # (i.e. page). For example, a page named 'foo' will have a
-    # directorynamed 'foo', with e.g. a 'foo.markdown' content file and a
-    # 'foo.yaml' meta file.
-    #
-    # Content file extensions are ignored by nanoc. The content file
-    # extension does not determine the filters to run on it; the meta file
-    # defines the list of filters. The meta file extension must always be
-    # 'yaml', though.
-    #
-    # Content files can also have the 'index' basename. Similarly, meta
-    # files can have the 'meta' basename. For example, a parent directory
-    # named 'foo' can have an 'index.txt' content file and a 'meta.yaml'
-    # meta file. This is to preserve backward compatibility.
-    def pages
+    def pages # :nodoc:
       meta_filenames('content').map do |meta_filename|
         # Read metadata
         meta = YAML.load_file(meta_filename) || {}
@@ -182,10 +228,7 @@ module Nanoc::DataSources
       end.compact
     end
 
-    # Saves the given page on the disk, creating it first if it's not
-    # already there yet. If the page already exists, the existing path will
-    # be used.
-    def save_page(page)
+    def save_page(page) # :nodoc:
       # Determine possible meta file paths
       last_component = page.path.split('/')[-1]
       meta_filename_worst = 'content' + page.path + 'index.yaml'
@@ -215,20 +258,17 @@ module Nanoc::DataSources
       File.open(content_filename, 'w') { |io| io.write(page.content(:raw)) }
     end
 
-    # TODO document
-    def move_page(page, new_path)
+    def move_page(page, new_path) # :nodoc:
       # TODO implement
     end
 
-    # TODO document
-    def delete_page(page)
+    def delete_page(page) # :nodoc:
       # TODO implement
     end
 
     ########## Page Defaults ##########
 
-    # The page defaults are loaded from a 'meta.yaml' file
-    def page_defaults
+    def page_defaults # :nodoc:
       # Get attributes
       attributes = YAML.load_file('meta.yaml') || {}
 
@@ -239,8 +279,7 @@ module Nanoc::DataSources
       Nanoc::PageDefaults.new(attributes, mtime)
     end
 
-    # TODO document
-    def save_page_defaults(page_defaults)
+    def save_page_defaults(page_defaults) # :nodoc:
       File.open('meta.yaml', 'w') do |io|
         io.write(hash_to_yaml(page_defaults.attributes))
       end
@@ -248,11 +287,7 @@ module Nanoc::DataSources
 
     ########## Layouts ##########
 
-    # Layouts are stored as directories in the 'layouts' directory. Each
-    # layout contains a content file and a meta file. The content file
-    # contain the actual layout, and the meta file describes how the page
-    # should be handled (contains the filter that should be used).
-    def layouts
+    def layouts # :nodoc:
       # Determine what layout directory structure is being used
       dir_count = Dir[File.join('layouts', '*')].select { |f| File.directory?(f) }.size
       is_old_school = (dir_count == 0)
@@ -301,28 +336,21 @@ module Nanoc::DataSources
       end
     end
 
-    # TODO document
-    def save_layout(layout)
+    def save_layout(layout) # :nodoc:
       # TODO implement
     end
 
-    # TODO document
-    def move_layout(layout, new_path)
+    def move_layout(layout, new_path) # :nodoc:
       # TODO implement
     end
 
-    # TODO document
-    def delete_layout(layout)
+    def delete_layout(layout) # :nodoc:
       # TODO implement
     end
 
     ########## Templates ##########
 
-    # Templates are located in the 'templates' directroy. Every template is
-    # a directory consisting of a content file and a meta file, both named
-    # after the template. This is very similar to the way pages are stored,
-    # except that templates cannot be nested.
-    def templates
+    def templates # :nodoc:
       meta_filenames('templates').map do |meta_filename|
         # Get name
         name = meta_filename.sub(/^templates\/(.*)\/[^\/]+\.yaml$/, '\1')
@@ -339,26 +367,21 @@ module Nanoc::DataSources
       end
     end
 
-    # TODO document
-    def save_template(template)
+    def save_template(template) # :nodoc:
       # TODO implement
     end
 
-    # TODO document
-    def move_template(template, new_name)
+    def move_template(template, new_name) # :nodoc:
       # TODO implement
     end
 
-    # TODO document
-    def delete_template(template)
+    def delete_template(template) # :nodoc:
       # TODO implement
     end
 
     ########## Code ##########
 
-    # Code is stored in '.rb' files in the 'lib' directory. Code can reside
-    # in sub-directories.
-    def code
+    def code # :nodoc:
       # Get files
       filenames = Dir['lib/**/*.rb'].sort
 
@@ -373,14 +396,13 @@ module Nanoc::DataSources
       Nanoc::Code.new(data, mtime)
     end
 
-    # TODO document
-    def save_code(code)
+    def save_code(code) # :nodoc:
       # TODO implement
     end
 
     ########## OLD ##########
 
-    # TODO remove this; outdated
+    # FIXME outdated, remove
     def create_page(path, template) # :nodoc:
       # Make sure path does not start or end with a slash
       sanitized_path = path.gsub(/^\/+|\/+$/, '')
@@ -399,7 +421,7 @@ module Nanoc::DataSources
       FileManager.create_file(content_path) { template[:content] }
     end
 
-    # TODO remove this; outdated
+    # FIXME outdated, remove
     def create_layout(name) # :nodoc:
       # Get details
       path = 'layouts/' + name
@@ -423,8 +445,8 @@ module Nanoc::DataSources
       end
     end
 
-    # TODO remove this; outdated
-    def create_template(name)
+    # FIXME outdated, remove
+    def create_template(name) # :nodoc:
       # Get paths
       meta_path    = 'templates/' + name + '/' + name + '.yaml'
       content_path = 'templates/' + name + '/' + name + '.txt'
@@ -489,6 +511,8 @@ module Nanoc::DataSources
       filenames.first
     end
 
+    # Converts the given hash into YAML format, splitting the YAML output into
+    # a 'builtin' and a 'custom' section.
     def hash_to_yaml(hash)
       # Get list of built-in keys
       builtin_keys = Nanoc::Page::PAGE_DEFAULTS
