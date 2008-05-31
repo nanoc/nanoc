94d9261aa0f6fa88b667c2a90c91a3f8dc0be8b1
Denis Defreyne <denis.defreyne@stoneship.org>
2008-05-31 11:42:30 +0200

    documented default router

--- lib/nanoc/routers/default.rb
+++ lib/nanoc/routers/default.rb
@@ -1,11 +1,14 @@
 module Nanoc::Routers
 
-  # TODO document
+  # The default router organises pages in the most obvious, but sometimes
+  # slightly restrictive, way: the hierarchy of compiled pages is the same as
+  # the hierarchy of uncompiled pages.
   class Default < Nanoc::Router
 
     identifier :default
 
-    # TODO document
+    # Returns the disk page for the path. This is simply the page's path as
+    # stored by the data source, followed by the filename and extension.
     def disk_path_for(page)
       # Get data we need
       filename   = page.attribute_named(:filename)
@@ -15,7 +18,10 @@ module Nanoc::Routers
       page.path + "#{filename}.#{extension}"
     end
 
-    # TODO document
+    # Returns the web path for the page. This is simply the page's path as it
+    # is stored, i.e. the default router simply make sure that the page
+    # hierarchy of uncompiled pages is the same as that of compiled pages
+    # (unless a custom path is specified).
     def web_path_for(page)
       # Return normal page path
       page.path
