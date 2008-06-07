b09678f1610c187c894cdb657e6f7fc01361312b
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-07 10:07:11 +0200

    added page rep attribute_named test

--- test/base/test_page_rep.rb
+++ test/base/test_page_rep.rb
@@ -36,6 +36,36 @@ class Nanoc::PageRepTest < Test::Unit::TestCase
 
   end
 
+  class TestAttributesSite
+
+    def page_defaults
+      @page_defaults ||= Nanoc::PageDefaults.new({
+        :four => 'four in page defaults',
+        :reps => {
+          :custom   => { :two   => 'two in page defaults rep'   },
+          :default  => { :three => 'three in page defaults rep' }
+        }
+      })
+    end
+
+  end
+
+  def test_initialize
+    # TODO implement
+  end
+
+  def test_to_proxy
+    # TODO implement
+  end
+
+  def test_created
+    # TODO implement
+  end
+
+  def test_modified
+    # TODO implement
+  end
+
   def test_disk_and_web_path
     # Create site
     site = TestSite.new
@@ -48,6 +78,93 @@ class Nanoc::PageRepTest < Test::Unit::TestCase
     # Check
     assert_equal('tmp/output/pages/path/index.html', page_rep.disk_path)
     assert_equal('/pages/path/',                     page_rep.web_path)
+  end
+
+  def test_attribute_named_with_custom_rep
+    # Should check in
+    # 1. page rep
+    # 2. page default's page rep
+    # 3. hardcoded defaults
+
+    # Create site
+    site = TestAttributesSite.new
+
+    # Create page and rep
+    page = Nanoc::Page.new(
+      "content",
+      { :reps => { :custom => { :one => 'one in page rep' } } },
+      '/path/'
+    )
+    page.site = site
+    page_rep = page.reps.find { |r| r.name == :custom }
+
+    # Test finding one
+    assert_equal('one in page rep', page_rep.attribute_named(:one))
+
+    # Test finding two
+    assert_equal('two in page defaults rep', page_rep.attribute_named(:two))
+
+    # Test finding three
+    assert_equal('default', page_rep.attribute_named(:layout))
+  end
+
+  def test_attribute_named_with_default_rep
+    # Should check in
+    # 1. page rep
+    # 2. page
+    # 3. page defaults' page rep
+    # 4. page defaults
+    # 5. hardcoded defaults
+
+    # Create site
+    site = TestAttributesSite.new
+
+    # Create page and rep
+    page = Nanoc::Page.new(
+      "content",
+      {
+        :two => 'two in page',
+        :reps => { :default => { :one => 'one in page rep' } }
+      },
+      '/path/'
+    )
+    page.site = site
+    page_rep = page.reps.find { |r| r.name == :default }
+
+    # Test finding one
+    assert_equal('one in page rep', page_rep.attribute_named(:one))
+
+    # Test finding two
+    assert_equal('two in page', page_rep.attribute_named(:two))
+
+    # Test finding three
+    assert_equal('three in page defaults rep', page_rep.attribute_named(:three))
+
+    # Test finding four
+    assert_equal('four in page defaults', page_rep.attribute_named(:four))
+
+    # Test finding five
+    assert_equal('default', page_rep.attribute_named(:layout))
+  end
+
+  def test_content
+    # TODO implement
+  end
+
+  def test_layout
+    # TODO implement
+  end
+
+  def test_compile
+    # TODO implement
+  end
+
+  def test_do_filter
+    # TODO implement
+  end
+
+  def test_do_layout
+    # TODO implement
   end
 
   def test_do_filter_with_outdated_filters_attribute
