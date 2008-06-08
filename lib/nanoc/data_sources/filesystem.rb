1e7a09941459964bd927efbf59d014c05eb586e9
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-08 19:05:13 +0200

    moving FileProxy to extra

--- lib/nanoc/data_sources/filesystem.rb
+++ lib/nanoc/data_sources/filesystem.rb
@@ -58,32 +58,6 @@ module Nanoc::DataSources
   # sub-directories.
   class Filesystem < Nanoc::DataSource
 
-    # A FileProxy is a proxy for a File object. It is used to prevent a File
-    # object from being created until it is actually necessary.
-    #
-    # For example, a site with a few thousand pages would fail to compile
-    # because the massive amount of file descriptors necessary, but the file
-    # proxy will make sure the File object is not created until it is used.
-    class FileProxy
-
-      instance_methods.each { |m| undef_method m unless m =~ /^__/ }
-
-      # Creates a new file proxy for the given path. This is similar to
-      # creating a File object with the same path, except that the File object
-      # will not be created until it is accessed.
-      def initialize(path)
-        @path = path
-      end
-
-      # Makes sure all method calls are relayed to a File object, which will
-      # be created right before the method call takes place and destroyed
-      # right after.
-      def method_missing(sym, *args, &block)
-        File.new(@path).__send__(sym, *args, &block)
-      end
-
-    end
-
     ########## Attributes ##########
 
     identifier :filesystem
@@ -181,7 +155,7 @@ module Nanoc::DataSources
           content = File.read(content_filename)
 
           # Get attributes
-          attributes = meta.merge(:file => FileProxy.new(content_filename))
+          attributes = meta.merge(:file => Nanoc::FileProxy.new(content_filename))
 
           # Get path
           path = meta_filename.sub(/^content/, '').sub(/[^\/]+\.yaml$/, '')
