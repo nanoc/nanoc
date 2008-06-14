744a8386db26a02176394b7bc8dd77d2cc893cb2
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-14 14:37:00 +0200

    DRYed up page/asset defaults by creating a Defaults superclass

---
+++ test/base/test_defaults.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+class Nanoc::DefaultsTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_initialize
+    # Make sure attributes are cleaned
+    page_defaults = Nanoc::PageDefaults.new({ 'foo' => 'bar' })
+    assert_equal({ :foo => 'bar' }, page_defaults.attributes)
+  end
+
+end
