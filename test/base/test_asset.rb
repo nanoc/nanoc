8524eec1bfe3ea516c0b7c2ee850c5bf5107d45a
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-14 17:25:58 +0200

    adding asset/binary filter test stubs

--- test/base/test_asset.rb
+++ test/base/test_asset.rb
@@ -5,7 +5,28 @@ class Nanoc::AssetTest < Test::Unit::TestCase
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
 
-  def test_stub
+  def test_initialize
+    # TODO implement
+  end
+
+  def test_build_reps
+    # TODO implement
+  end
+
+  def test_to_proxy
+    # TODO implement
+  end
+
+  def test_outdated
+    # TODO implement
+  end
+
+  def test_attribute_named
+    # TODO implement
+  end
+
+  def test_compile
+    # TODO implement
   end
 
 end
