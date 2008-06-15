877f808bac1ced2fd5a4fb269ebbcb09c12cfcb5
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-15 12:48:01 +0200

    compiler now compiles only the requested page or asset

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -31,18 +31,16 @@ module Nanoc
       @stack = []
 
       # Get pages and assets
-      pages  = (page_or_asset && page_or_asset.is_a?(Page)  ? [ page_or_asset ] : @site.pages )
-      assets = (page_or_asset && page_or_asset.is_a?(Asset) ? [ page_or_asset ] : @site.assets)
+      unless page_or_asset.nil?
+        objects = [ page_or_asset ]
+      else
+        objects = @site.pages + @site.assets
+      end
 
       # Compile pages and assets
-      compile_objects(pages,  include_outdated) { |p| yield(p) if block_given? }
-      compile_objects(assets, include_outdated) { |a| yield(a) if block_given? }
-    end
-
-    def compile_objects(objects, include_outdated)
       objects.each do |obj|
         obj.compile if obj.outdated? or include_outdated
-        yield obj
+        yield obj if block_given?
       end
     end
 
