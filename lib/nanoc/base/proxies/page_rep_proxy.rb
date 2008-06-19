03d703c16dc838352ad0b9c780054c275660f0d3
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-19 16:31:52 +0200

    asset and page rep proxies now handle name

--- lib/nanoc/base/proxies/page_rep_proxy.rb
+++ lib/nanoc/base/proxies/page_rep_proxy.rb
@@ -10,9 +10,9 @@ module Nanoc
     def [](key)
       real_key = key.to_s.sub(/\?$/, '').to_sym
 
-      # TODO add name
-
-      if real_key == :content
+      if real_key == :name
+        @obj.name
+      elsif real_key == :content
         @obj.content
       elsif real_key == :path
         @obj.web_path
