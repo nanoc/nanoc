b3aa28279fbff38f062ef6c9b5aebecb7f2be4e6
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-21 17:00:26 +0200

    moved ISO8601-related Time methods from extensions to extra

--- lib/nanoc/extensions/blogging.rb
+++ lib/nanoc/extensions/blogging.rb
@@ -1,17 +1,3 @@
-class Time
-
-  # Returns a string with the time in an ISO-8601 date format.
-  def to_iso8601_date
-    self.strftime("%Y-%m-%d")
-  end
-
-  # Returns a string with the time in an ISO-8601 time format.
-  def to_iso8601_time
-    self.gmtime.strftime("%Y-%m-%dT%H:%M:%SZ")
-  end
-
-end
-
 module Nanoc::Extensions
 
   # Nanoc::Extensions::Blogging provides some functionality for building
