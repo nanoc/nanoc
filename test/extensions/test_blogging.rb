cd8e4ceff7f1d08259d127a9d31e5b95ced3b952
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-21 14:56:43 +0200

    implemented atom_feed test

--- test/extensions/test_blogging.rb
+++ test/extensions/test_blogging.rb
@@ -10,9 +10,15 @@ class Nanoc::Extensions::BloggingTest < Test::Unit::TestCase
   def test_sorted_articles
     # Create pages
     @pages = [ mock, mock, mock ]
+
+    # Create page 0
     @pages[0].expects(:kind).returns('page')
+
+    # Create page 1
     @pages[1].expects(:kind).returns('article')
     @pages[1].expects(:created_at).returns(Time.now - 1000)
+
+    # Create page 2
     @pages[2].expects(:kind).returns('article')
     @pages[2].expects(:created_at).returns(Time.now - 500)
 
@@ -29,7 +35,45 @@ class Nanoc::Extensions::BloggingTest < Test::Unit::TestCase
   end
 
   def test_atom_feed
-    # TODO implement
+    # Create pages
+    @pages = [ mock, mock, mock ]
+
+    # Create page 0
+    @pages[0].expects(:kind).times(2).returns('page')
+
+    # Create page 1
+    @pages[1].expects(:kind).times(2).returns('article')
+    @pages[1].expects(:created_at).at_least_once.returns(Time.now - 1000)
+    @pages[1].expects(:mtime).returns(Time.now - 500)
+    @pages[1].expects(:title).returns('Page One')
+    @pages[1].expects(:custom_path_in_feed).returns(nil)
+    @pages[1].expects(:path).at_least_once.returns('/page1/')
+    @pages[1].expects(:content).returns('page 1 content')
+    @pages[1].expects(:excerpt).returns(nil)
+
+    # Create page 2
+    @pages[2].expects(:kind).times(2).returns('article')
+    @pages[2].expects(:created_at).at_least_once.returns(Time.now - 750)
+    @pages[2].expects(:mtime).returns(Time.now - 250)
+    @pages[2].expects(:title).returns('Page Two')
+    @pages[2].expects(:custom_path_in_feed).returns('/page2custom/')
+    @pages[2].expects(:path).at_least_once.returns('/page2/')
+    @pages[2].expects(:content).returns('page 2 content')
+    @pages[2].expects(:excerpt).times(2).returns('page 2 excerpt')
+
+    # Create feed page
+    @page = mock
+    @page.expects(:base_url).at_least_once.returns('http://example.com')
+    @page.expects(:title).returns('My Cool Blog')
+    @page.expects(:author_name).returns('Denis Defreyne')
+    @page.expects(:author_uri).returns('http://stoneship.org/')
+    @page.expects(:[]).with(:feed_url).returns(nil)
+    @page.expects(:path).returns('/journal/feed/')
+
+    # Check
+    assert_nothing_raised do
+      atom_feed
+    end
   end
 
   def test_url_for_without_custom_path_in_feed
