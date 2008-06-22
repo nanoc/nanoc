b26206d8cf9dfba93334a4cb3bbd9492879f438f
Denis Defreyne <denis.defreyne@stoneship.org>
2008-06-22 15:02:04 +0200

    removing unused #log and #error functions

--- lib/nanoc/base/enhancements.rb
+++ lib/nanoc/base/enhancements.rb
@@ -5,18 +5,6 @@ end
 
 ############################# OLD AND DEPRECATED #############################
 
-# Logging (level can be :off, :high, :low)
-$log_level = :high
-def log(log_level, s, io=$stdout)
-  io.puts s if ($log_level == :low or $log_level == log_level) and $log_level != :off
-end
-
-# Convenience function for printing errors
-def error(s, pre='ERROR')
-  log(:high, pre + ': ' + s, $stderr)
-  exit(1)
-end
-
 def nanoc_require(x)
   warn(
     "'nanoc_require' is deprecated and will be removed in a future version. Please use 'require' instead.",
