a04770596dedd39d17c5ead6f896709449b5d60b
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-05 17:57:38 +0200

    cleanup

--- lib/nanoc/cli/commands/compile.rb
+++ lib/nanoc/cli/commands/compile.rb
@@ -67,20 +67,7 @@ module Nanoc::CLI
         time_before = Time.now
         @filter_times ||= {}
         @times_stack  ||= []
-
-        # Set notifications
-        Nanoc::NotificationCenter.on(:compilation_started) do |rep|
-          rep_compilation_started(rep)
-        end
-        Nanoc::NotificationCenter.on(:compilation_ended) do |rep|
-          rep_compilation_ended(rep)
-        end
-        Nanoc::NotificationCenter.on(:filtering_started) do |rep, filter_name|
-          rep_filtering_started(rep, filter_name)
-        end
-        Nanoc::NotificationCenter.on(:filtering_ended) do |rep, filter_name|
-          rep_filtering_ended(rep, filter_name)
-        end
+        setup_notifications
 
         # Compile
         @base.site.compiler.run(
@@ -99,88 +86,129 @@ module Nanoc::CLI
         puts "#{obj.nil? ? 'Site' : 'Page'} compiled in #{format('%.2f', Time.now - time_before)}s."
 
         if options.has_key?(:verbose)
-          # Give page rep state feedback
-          rest            = reps
-          created, rest   = *rest.partition { |r| r.created? }
-          modified, rest  = *rest.partition { |r| r.modified? }
-          skipped, rest   = *rest.partition { |r| !r.compiled? }
-          identical       = rest
-          puts
-          puts format('  %4d  created',   created.size)
-          puts format('  %4d  modified',  modified.size)
-          puts format('  %4d  skipped',   skipped.size)
-          puts format('  %4d  identical', identical.size)
-
-          # Give profiling feedback
-          puts
-          max_filter_name_length = @filter_times.keys.map { |k| k.to_s.size }.max
-          puts ' ' * max_filter_name_length + ' | count    min    avg    max     tot'
-          puts '-' * max_filter_name_length + '-+-----------------------------------'
-          @filter_times.each_pair do |filter_name, samples|
-            # Calculate some stats
-            count = samples.size
-            min   = samples.min
-            tot   = samples.inject { |memo, i| memo + i}
-            avg   = tot/count
-            max   = samples.max
-
-            # Format stats
-            count = format('%4d',   count)
-            min   = format('%4.2f', min)
-            avg   = format('%4.2f', avg)
-            max   = format('%4.2f', max)
-            tot   = format('%5.2f', tot)
-
-            # Output stats
-            filter_name = format("%#{max_filter_name_length}s", filter_name)
-            puts "#{filter_name} |  #{count}  #{min}s  #{avg}s  #{max}s  #{tot}s"
-          end
+          print_state_feedback(reps)
+          print_profiling_feedback(reps)
         end
-     rescue Exception => e
-        # Get page rep
-        page_rep = @base.site.compiler.stack.select { |i| i.is_a?(Nanoc::PageRep) }[-1]
-        page_rep_name = page_rep.nil? ? 'the site' : "#{page_rep.page.path} (rep #{page_rep.name})"
-
-        # Build message
-        case e
-        when Nanoc::Errors::UnknownLayoutError
-          message = "Unknown layout: #{e.message}"
-        when Nanoc::Errors::UnknownFilterError
-          message = "Unknown filter: #{e.message}"
-        when Nanoc::Errors::CannotDetermineFilterError
-          message = "Cannot determine filter for layout: #{e.message}"
-        when Nanoc::Errors::RecursiveCompilationError
-          message = "Recursive call to page content."
-        when Nanoc::Errors::NoLongerSupportedError
-          message = "No longer supported: #{e.message}"
-        else
-          message = "Unknown error: #{e.message}"
-        end
-
-        # Print message
-        puts
-        puts "ERROR: An exception occured while compiling #{page_rep_name}."
-        puts
-        puts "If you think this is a bug in nanoc, please do report it at"
-        puts "<http://nanoc.stoneship.org/trac/newticket> -- thanks!"
-        puts
-        puts 'Message:'
-        puts '  ' + message
-        puts
-        puts 'Page compilation stack:'
-        @base.site.compiler.stack.reverse.each do |item|
-          if item.is_a?(Nanoc::PageRep) # page rep
-            puts "  - [page]   #{item.page.path} (rep #{item.name})"
-          elsif item.is_a?(Nanoc::AssetRep) # asset rep
-            puts "  - [asset]  #{item.asset.path} (rep #{item.name})"
-          else # layout
-            puts "  - [layout] #{item.path}"
-          end
-        end
-        puts
-        puts 'Backtrace:'
-        puts e.backtrace.map { |t| '  - ' + t }.join("\n")
+      rescue Exception => e
+        print_error(e)
       end
+    end
+
+  private
+
+    def setup_notifications
+      Nanoc::NotificationCenter.on(:compilation_started) do |rep|
+        rep_compilation_started(rep)
+      end
+      Nanoc::NotificationCenter.on(:compilation_ended) do |rep|
+        rep_compilation_ended(rep)
+      end
+      Nanoc::NotificationCenter.on(:filtering_started) do |rep, filter_name|
+        rep_filtering_started(rep, filter_name)
+      end
+      Nanoc::NotificationCenter.on(:filtering_ended) do |rep, filter_name|
+        rep_filtering_ended(rep, filter_name)
+      end
+    end
+
+    def print_state_feedback(reps)
+      # Categorise reps
+      rest            = reps
+      created, rest   = *rest.partition { |r| r.created? }
+      modified, rest  = *rest.partition { |r| r.modified? }
+      skipped, rest   = *rest.partition { |r| !r.compiled? }
+      identical       = rest
+
+      # Print
+      puts
+      puts format('  %4d  created',   created.size)
+      puts format('  %4d  modified',  modified.size)
+      puts format('  %4d  skipped',   skipped.size)
+      puts format('  %4d  identical', identical.size)
+    end
+
+    def print_profiling_feedback(reps)
+      # Get max filter length
+      max_filter_name_length = @filter_times.keys.map { |k| k.to_s.size }.max
+      return if max_filter_name_length.nil?
+
+      # Print warning if necessary
+      if reps.any? { |r| !r.compiled? }
+        puts
+        puts "Warning: profiling information may not be accurate because " +
+             "some pages were not compiled."
+      end
+
+      # Print header
+      puts
+      puts ' ' * max_filter_name_length + ' | count    min    avg    max     tot'
+      puts '-' * max_filter_name_length + '-+-----------------------------------'
+
+      @filter_times.each_pair do |filter_name, samples|
+        # Calculate stats
+        count = samples.size
+        min   = samples.min
+        tot   = samples.inject { |memo, i| memo + i}
+        avg   = tot/count
+        max   = samples.max
+
+        # Format stats
+        count = format('%4d',   count)
+        min   = format('%4.2f', min)
+        avg   = format('%4.2f', avg)
+        max   = format('%4.2f', max)
+        tot   = format('%5.2f', tot)
+
+        # Output stats
+        filter_name = format("%#{max_filter_name_length}s", filter_name)
+        puts "#{filter_name} |  #{count}  #{min}s  #{avg}s  #{max}s  #{tot}s"
+      end
+    end
+
+    def print_error(error)
+      # Get page rep
+      page_rep = @base.site.compiler.stack.select { |i| i.is_a?(Nanoc::PageRep) }[-1]
+      page_rep_name = page_rep.nil? ? 'the site' : "#{page_rep.page.path} (rep #{page_rep.name})"
+
+      # Build message
+      case error
+      when Nanoc::Errors::UnknownLayoutError
+        message = "Unknown layout: #{error.message}"
+      when Nanoc::Errors::UnknownFilterError
+        message = "Unknown filter: #{error.message}"
+      when Nanoc::Errors::CannotDetermineFilterError
+        message = "Cannot determine filter for layout: #{error.message}"
+      when Nanoc::Errors::RecursiveCompilationError
+        message = "Recursive call to page content."
+      when Nanoc::Errors::NoLongerSupportedError
+        message = "No longer supported: #{error.message}"
+      else
+        message = "Unknown error: #{error.message}"
+      end
+
+      # Print message
+      puts
+      puts "ERROR: An exception occured while compiling #{page_rep_name}."
+      puts
+      puts "If you think this is a bug in nanoc, please do report it at"
+      puts "<http://nanoc.stoneship.org/trac/newticket> -- thanks!"
+      puts
+      puts 'Message:'
+      puts '  ' + message
+      puts
+      puts 'Page compilation stack:'
+      @base.site.compiler.stack.reverse.each do |item|
+        if item.is_a?(Nanoc::PageRep) # page rep
+          puts "  - [page]   #{item.page.path} (rep #{item.name})"
+        elsif item.is_a?(Nanoc::AssetRep) # asset rep
+          puts "  - [asset]  #{item.asset.path} (rep #{item.name})"
+        else # layout
+          puts "  - [layout] #{item.path}"
+        end
+      end
+      puts
+      puts 'Backtrace:'
+      puts error.backtrace.map { |t| '  - ' + t }.join("\n")
     end
 
     def rep_compilation_started(rep)
