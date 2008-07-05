947e791f2c0c65aafd849793f7b4fecc61b07f3c
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-05 15:25:42 +0200

    added content capturing plugin

---
+++ test/extensions/test_capturing.rb
@@ -0,0 +1,25 @@
+require 'helper'
+
+class Nanoc::Extensions::CapturingTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  include Nanoc::Extensions::Capturing
+
+  def test_content_for
+    # Build content to be evaluated
+    content = "head <% content_for :sidebar do %>\n" +
+              "  <%= 1+2 %>\n" +
+              "<% end %> foot"
+
+    # Evaluate content
+    @page = {}
+    result = ::ERB.new(content).result(binding)
+
+    # Check
+    assert(@page[:content_for_sidebar].strip == '3')
+    assert_match(/^head\s+foot$/, result)
+  end
+
+end
