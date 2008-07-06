078fd17c23706cce1f797429a8a63793d2448863
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-06 11:41:12 +0200

    added missing Time test

---
+++ test/extra/core_ext/test_time.rb
@@ -0,0 +1,16 @@
+require 'helper'
+
+class Nanoc::ExtraCoreExtTimeTest < Test::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_to_iso8601_date
+    assert_equal('2008-05-19', Time.utc(2008, 5, 19, 14, 20, 0, 0).to_iso8601_date)
+  end
+
+  def test_to_iso8601_time
+    assert_equal('2008-05-19T14:20:00Z', Time.utc(2008, 5, 19, 14, 20, 0, 0).to_iso8601_time)
+  end
+
+end
