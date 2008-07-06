729213be21e729e24fbdf94222786d1ceaca1c9a
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-06 14:55:37 +0200

    writing Nanoc::VCS documentation

--- lib/nanoc/extra/vcs.rb
+++ lib/nanoc/extra/vcs.rb
@@ -1,30 +1,46 @@
 module Nanoc::Extra
 
-  # TODO document
+  # Nanoc::Extra::VCS is a very simple representation of a version control
+  # system that abstracts the add, remove and move operations. It does not
+  # commit. This class is primarily used by data sources that store data as
+  # flat files on the disk.
+  #
+  # This is the abstract superclass for all VCSes. Subclasses should implement
+  # the indicated methods. 
   class VCS < Nanoc::Plugin
 
-    # TODO document
-    def self.named(identifier)
+    # Returns the VCS with the given name. +name+ must be a symbol.
+    def self.named(name)
       # Initialize list of VCSes if necessary
       @vcses ||= {}
 
       # Find VCS
-      @vcses[identifier] ||= Nanoc::PluginManager.instance.find(
-        Nanoc::Extra::VCS, :identifiers, identifier
+      @vcses[name] ||= Nanoc::PluginManager.instance.find(
+        Nanoc::Extra::VCS, :identifiers, name
       )
     end
 
-    # TODO document
+    # Adds the file with the given filename to the working copy.
+    #
+    # Subclasses must implement this method.
     def add(filename)
       not_implemented('add')
     end
 
-    # TODO document
+    # Removes the file with the given filename from the working copy. When
+    # this method is executed, the file should no longer be present on the
+    # disk.
+    #
+    # Subclasses must implement this method.
     def remove(filename)
       not_implemented('remove')
     end
 
-    # TODO document
+    # Moves the file with the given filename to a new location. When this
+    # method is executed, the original file should no longer be present on the
+    # disk.
+    #
+    # Subclasses must implement this method.
     def move(src, dst)
       not_implemented('move')
     end
