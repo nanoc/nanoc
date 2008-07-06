43761c1888561dedf1f354d2ffc299a5cae2ad45
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-06 11:49:14 +0200

    implemented Hash#to_split_yaml tests

--- test/extra/core_ext/test_hash.rb
+++ test/extra/core_ext/test_hash.rb
@@ -5,8 +5,36 @@ class Nanoc::ExtraCoreExtHashTest < Test::Unit::TestCase
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
 
-  def test_to_split_yaml
-    # TODO implement
+  def test_to_split_yaml_builtin_only
+    hash = { :layout => 'foo' }
+    assert_equal(
+      "# Built-in \nlayout: foo\n\n# Custom\n",
+      hash.to_split_yaml
+    )
+  end
+
+  def test_to_split_yaml_custom_only
+    hash = { :foo => 'bar' }
+    assert_equal(
+      "# Built-in\n\n# Custom \nfoo: bar\n",
+      hash.to_split_yaml
+    )
+  end
+
+  def test_to_split_yaml_builtin_and_custom
+    hash = { :layout => 'foo', :foo => 'bar' }
+    assert_equal(
+      "# Built-in \nlayout: foo\n\n# Custom \nfoo: bar\n",
+      hash.to_split_yaml
+    )
+  end
+
+  def test_to_split_yaml_arrays
+    hash = { :filters_pre => %w( foo bar baz ), :filters_post => %w( xxx yyy zzz ) }
+    assert_equal(
+      "# Built-in \nfilters_pre: \n- foo\n- bar\n- baz\nfilters_post: \n- xxx\n- yyy\n- zzz\n\n# Custom\n",
+      hash.to_split_yaml
+    )
   end
 
 end
