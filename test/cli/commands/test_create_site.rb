8876c969225289f9645e463a6e931025f20e8e88
Denis Defreyne <denis.defreyne@stoneship.org>
2008-07-14 20:54:45 +0200

    adding first CLI command test EVER!

--- test/cli/commands/test_create_site.rb
+++ test/cli/commands/test_create_site.rb
@@ -5,7 +5,11 @@ class Nanoc::CLI::CreateSiteCommandTest < Test::Unit::TestCase
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
 
-  def test_stub
+  def test_create_site_with_existing_name
+    in_dir %w{ tmp } do
+      assert_nothing_raised()   { Nanoc::CLI::Base.new.run([ 'create_site', 'foo' ]) }
+      assert_raise(SystemExit)  { Nanoc::CLI::Base.new.run([ 'create_site', 'foo' ]) }
+    end
   end
 
 end
