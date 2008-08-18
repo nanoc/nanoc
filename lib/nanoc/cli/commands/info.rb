536994e046ba88c6e94892159aeb9268086038a5
Denis Defreyne <denis.defreyne@stoneship.org>
2008-08-18 10:52:59 +0200

    info command now orders plugins
    --HG--
    branch : 2.1

--- lib/nanoc/cli/commands/info.rb
+++ lib/nanoc/cli/commands/info.rb
@@ -52,7 +52,9 @@ module Nanoc::CLI
       # Find longest name
       max_length = plugins.values.map { |k| k.values }.flatten.map { |k| k.identifiers.join(', ').length }.max + 2
 
-      plugins.each_pair do |superclass, structured_plugins|
+      PLUGIN_CLASS_ORDER.each do |superclass|
+        structured_plugins = plugins[superclass]
+
         # Print kind
         kind = name_for_plugin_class(superclass)
         puts "#{kind}:"
@@ -87,6 +89,14 @@ module Nanoc::CLI
     end
 
   private
+
+    PLUGIN_CLASS_ORDER = [
+      Nanoc::Filter,
+      Nanoc::BinaryFilter,
+      Nanoc::Router,
+      Nanoc::Extra::VCS,
+      Nanoc::DataSource
+    ]
 
     PLUGIN_CLASSES = {
       Nanoc::BinaryFilter => 'Binary Filters',
