1755eb6f713f09a6d0cc897c0885c14fd6f494c3
Denis Defreyne <denis.defreyne@stoneship.org>
2008-08-23 13:05:51 +0200

    dividing rake tasks in namespaces

--- test/base/test_site.rb
+++ test/base/test_site.rb
@@ -103,12 +103,6 @@ class Nanoc::SiteTest < Test::Unit::TestCase
 
   end
 
-  class TestRouter < Nanoc::Router
-
-    identifier :test_router
-
-  end
-
   def test_initialize_custom_router
     assert_nothing_raised do
       Nanoc::Site.new(
@@ -116,58 +110,6 @@ class Nanoc::SiteTest < Test::Unit::TestCase
         :data_source  => 'early_loading_code_data_source',
         :router       => 'early_loading_code_router'
       )
-    end
-  end
-
-  def test_load_data
-    # Create site with newschool data source
-    site = nil
-    assert_nothing_raised do
-      site = Nanoc::Site.new(:data_source => 'test_newschool_data_source')
-      site.load_data
-    end
-
-    # Check classes
-    assert(site.pages.all? { |p| p.is_a?(Nanoc::Page) })
-    assert(site.page_defaults.is_a?(Nanoc::PageDefaults))
-    assert(site.layouts.all? { |l| l.is_a?(Nanoc::Layout) })
-    assert(site.templates.all? { |t| t.is_a?(Nanoc::Template) })
-    assert(site.code.is_a?(Nanoc::Code))
-
-    # Check whether site is set
-    assert(site.pages.all? { |p| p.site == site })
-    assert(site.page_defaults.site == site)
-    assert(site.layouts.all? { |l| l.site == site })
-    assert(site.templates.all? { |t| t.site == site })
-    assert(site.code.site == site)
-  end
-
-  def test_config
-    in_dir [ 'tmp' ] do
-      # Create temporary site
-      create_site('testing')
-
-      in_dir [ 'testing' ] do
-        # Create site
-        site = Nanoc::Site.new(
-          :output_dir   => 'custom_output',
-          :data_source  => 'test_data_source',
-          :router       => 'test_router'
-        )
-
-        # Check
-        assert_equal('custom_output',     site.config[:output_dir])
-        assert_equal('test_data_source',  site.config[:data_source])
-        assert_equal('test_router',       site.config[:router])
-
-        # Create site
-        site = Nanoc::Site.new({})
-
-        # Check
-        assert_equal('output',      site.config[:output_dir])
-        assert_equal('filesystem',  site.config[:data_source])
-        assert_equal('default',     site.config[:router])
-      end
     end
   end
 
