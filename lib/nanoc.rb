40b42caf8ec8517a183d9ae72a270fc376530771
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-01 11:10:44 +0100

    updated version and added changelog entry
    --HG--
    branch : 2.1-ruby1.9

--- lib/nanoc.rb
+++ lib/nanoc.rb
@@ -1,7 +1,7 @@
 module Nanoc
 
   # The current nanoc version.
-  VERSION = '2.1.4'
+  VERSION = '2.1.5'
 
   # Generic error. Superclass for all nanoc-specific errors.
   class Error < RuntimeError ; end
