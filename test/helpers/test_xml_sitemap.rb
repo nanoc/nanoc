f04ee160b569a88b3286032ed53103948677b4ed
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-07 19:09:22 +0100

    XML sitemap no longer included non-outputted pages

--- test/helpers/test_xml_sitemap.rb
+++ test/helpers/test_xml_sitemap.rb
@@ -10,10 +10,11 @@ class Nanoc::Helpers::XMLSitemapTest < MiniTest::Unit::TestCase
   def test_xml_sitemap
     if_have 'builder' do
       # Create pages
-      @pages = [ mock, mock, mock ]
+      @pages = [ mock, mock, mock, mock ]
 
       # Create page 0
       @pages[0].expects(:is_hidden).returns(false)
+      @pages[0].expects(:skip_output).returns(false)
       @pages[0].expects(:path).returns('/foo/')
       @pages[0].expects(:mtime).returns(nil)
       @pages[0].expects(:changefreq).returns(nil)
@@ -24,10 +25,15 @@ class Nanoc::Helpers::XMLSitemapTest < MiniTest::Unit::TestCase
 
       # Create page 2
       @pages[2].expects(:is_hidden).returns(false)
+      @pages[2].expects(:skip_output).returns(false)
       @pages[2].expects(:path).returns('/baz/')
       @pages[2].expects(:mtime).times(2).returns(Time.parse('12/07/2004'))
       @pages[2].expects(:changefreq).times(2).returns('daily')
       @pages[2].expects(:priority).times(2).returns(0.5)
+
+      # Create page 3
+      @pages[3].expects(:is_hidden).returns(false)
+      @pages[3].expects(:skip_output).returns(true)
 
       # Create sitemap page
       @page = mock
