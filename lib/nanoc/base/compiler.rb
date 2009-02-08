332100ee829d0ddffc3b94e26bbfdbd313c16c14
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-08 20:54:21 +0100

    compiler/dsl now supports multiple representations

--- lib/nanoc/base/compiler.rb
+++ lib/nanoc/base/compiler.rb
@@ -7,9 +7,10 @@ module Nanoc
     attr_reader :stack
 
     # Creates a new compiler for the given site.
-    # FIXME compiler doesn't need to know about the site
     def initialize(site)
       @site = site
+
+      @stack = []
 
       @page_rules  = []
       @asset_rules = []
@@ -44,23 +45,47 @@ module Nanoc
       # Create output directory if necessary
       FileUtils.mkdir_p(@site.config[:output_dir])
 
-      # Initialize
-      @stack = []
-
       # Get items to compile
       items ||= @site.pages + @site.assets
 
-      # Get reps to compile
-      reps = items.map do |item|
+      # Compile everything
+      @stack = []
+      items.each { |i| compile_item(i, force) }
+    end
+
+    # Compiles the given item and all its representations. This method should
+    # not be called directly; please use Nanoc::Compiler#run instead, and pass
+    # the items to compile as the first argument.
+    #
+    # +force+:: true if the item rep should be compiled even if it is not
+    #                            outdated, false if not.
+    def compile_item(item, force)
+      # Find matching rules
+      all_rules = (item.type == :page ? @page_rules : @asset_rules)
+      matching_rules = all_rules.inject({}) do |memo, rule|
+        # Skip rule if an existing rule for this rep name already exists
+        next unless memo[rule.rep_name].nil?
+
+        # Skip rule if not applicable to this rep
+        next unless rule.applicable_to?(item)
+
+        # Add rule
+        memo.merge({ rule.rep_name => rule })
+      end
+      raise Nanoc::Errors::NoMatchingRuleFoundError.new if matching_rules.keys.empty?
+
+      # Create reps for each rep name
+      rep_names = matching_rules.keys
+      reps = rep_names.map do |rep_name|
         if item.type == :page
-          PageRep.new(item, :default)
-        elsif item.type == :asset
-          AssetRep.new(item, :default)
+          PageRep.new(item, rep_name)
+        else
+          AssetRep.new(item, rep_name)
         end
       end
 
-      # Compile everything
-      reps.each { |rep| compile_rep(rep, force) }
+      # Compile reps
+      reps.each { |rep| compile_rep(rep, matching_rules[rep.name.to_sym], force) }
     end
 
     # Compiles the given item representation. This method should not be called
@@ -71,7 +96,7 @@ module Nanoc
     #
     # +force+:: true if the item rep should be compiled even if it is not
     #                            outdated, false if not.
-    def compile_rep(rep, force)
+    def compile_rep(rep, rule, force)
       # Reset compilation status
       rep.modified = false
       rep.created  = false
@@ -101,12 +126,10 @@ module Nanoc
       rep.content[:last] ||= rep.content[:raw]
 
       # Check if file will be created
-      old_content = File.file?(rep.disk_path) ? File.read(rep.disk_path) : nil
+      # FIXME temporary
+      old_content = '' # File.file?(rep.disk_path) ? File.read(rep.disk_path) : nil
 
-      # Find and apply matching page rule
-      rules = (rep.type == :page_rep ? @page_rules : @asset_rules)
-      rule = rules.find { |r| r.applicable_to?(rep) }
-      raise Nanoc::Errors::NoMatchingRuleFoundError.new if rule.nil?
+      # Apply matching rule
       rule.apply_to(rep)
 
       # Update status
@@ -121,12 +144,12 @@ module Nanoc
       @stack.pop
     end
 
-    def add_page_rule(path, block)
-      @page_rules << ItemRule.new(path_to_regex(path), self, block)
+    def add_page_rule(path, rep_name, block)
+      @page_rules << ItemRule.new(path_to_regex(path), rep_name, self, block)
     end
 
-    def add_asset_rule(path, block)
-      @asset_rules << ItemRule.new(path_to_regex(path), self, block)
+    def add_asset_rule(path, rep_name, block)
+      @asset_rules << ItemRule.new(path_to_regex(path), rep_name, self, block)
     end
 
     def add_layout_rule(path, block)
@@ -135,7 +158,8 @@ module Nanoc
 
   private
 
-    # TODO document
+    # Converts the given path, which can contain the '*' wildcard, to a regex.
+    # For example, 'foo/*/bar' is transformed into /^foo\/(.*?)\/bar$/.
     def path_to_regex(path)
       if path.is_a? String
         /^#{path.gsub('*', '(.*?)')}$/
--- lib/nanoc/base/item_rule.rb
+++ lib/nanoc/base/item_rule.rb
@@ -7,18 +7,24 @@ module Nanoc
     # rule can be applied to items with a path matching this regex.
     attr_reader :path_regex
 
+    # The name of the representation that will be compiled using this rule.
+    attr_reader :rep_name
+
     # Creates a new item compilation rule with the given path regex, compiler
     # and block. The block will be called during compilation with the item rep
     # as its argument.
-    def initialize(path_regex, compiler, block)
+    def initialize(path_regex, rep_name, compiler, block)
       @path_regex = path_regex
+      @rep_name   = rep_name.to_sym
+
       @compiler   = compiler
+
       @block      = block
     end
 
     # Returns true if this rule can be applied to the given item rep.
-    def applicable_to?(rep)
-      rep.item.path =~ @path_regex
+    def applicable_to?(item)
+      item.path =~ @path_regex
     end
 
     # Applies this rule to the given item rep.
