0bf8b2947fe34bbbf4f15aeb4b17d34475d8b810
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-08 13:37:46 +0100

    removed outdated/incompatible tests and fixtures

--- test/fixtures/site_with_custom_extensions/lib/default.rb
+++
@@ -1,7 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-
-def html_escape(a_string)
-  a_string.gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;').gsub('\'', '&apos;').gsub('"', '&quot;')
-end
-alias h html_escape
--- test/fixtures/site_with_custom_filters/lib/censor_filter.rb
+++
@@ -1,9 +0,0 @@
-class CensorFilter < Nanoc::Filter
-
-  identifier :censor
-
-  def run(content)
-    content.gsub('sucks', 'rocks')
-  end
-
-end
