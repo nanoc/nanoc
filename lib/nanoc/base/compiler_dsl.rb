a36ec62f37617f8e20739958c39a7ca7c645254d
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-08 20:54:21 +0100

    compiler/dsl now supports multiple representations

--- lib/nanoc/base/compiler_dsl.rb
+++ lib/nanoc/base/compiler_dsl.rb
@@ -14,16 +14,22 @@ module Nanoc
       # Require block
       raise ArgumentError.new("#page requires a block") unless block_given?
 
+      # Get rep name
+      rep_name = params[:rep] || :default
+
       # Create rule
-      @compiler.add_page_rule(path, block)
+      @compiler.add_page_rule(path, rep_name, block)
     end
 
     def asset(path, params={}, &block)
       # Require block
       raise ArgumentError.new("#asset requires a block") unless block_given?
 
+      # Get rep name
+      rep_name = params[:rep] || :default
+
       # Create rule
-      @compiler.add_asset_rule(path, block)
+      @compiler.add_asset_rule(path, rep_name, block)
     end
 
     def layout(path, &block)
