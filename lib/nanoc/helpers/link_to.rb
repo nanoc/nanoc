1d5ed17a13f8566872d2877759e59187a1769f25
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-15 20:16:25 +0100

    added #relative_path_to method to LinkTo helper
    This helper method is based on the helper method by Christian Plessl. See <http://groups.google.com/group/nanoc/browse_thread/thread/b03c68d91786007b> for details.

--- lib/nanoc/helpers/link_to.rb
+++ lib/nanoc/helpers/link_to.rb
@@ -68,6 +68,30 @@ module Nanoc::Helpers
       end
     end
 
+    def relative_path_to(path_or_rep)
+      require 'pathname'
+
+      # Get source and destination paths
+      dst_path   = Pathname.new(path_or_rep)
+      src_path   = Pathname.new(@item.path)
+
+      # Calculate elative path (method depends on whether destination is a
+      # directory or not).
+      if src_path.to_s[-1,1] != '/'
+        relative_path = dst_path.relative_path_from(src_path.dirname).to_s
+      else
+        relative_path = dst_path.relative_path_from(src_path).to_s
+      end
+
+      # Add trailing slash if necessary
+      if dst_path.to_s[-1,1] == '/'
+        relative_path += '/'
+      end
+
+      # Done
+      relative_path
+     end
+
   end
 
 end
