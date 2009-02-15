9e7b1ef2a7af2f5e4925a3e81afa6272a29658b7
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-15 20:33:38 +0100

    added relativize_paths filter

---
+++ lib/nanoc/filters/relativize_paths.rb
@@ -0,0 +1,18 @@
+module Nanoc::Filters
+  class RelativizePaths < Nanoc::Filter
+
+    identifier :relativize_paths
+
+    require 'nanoc/helpers/link_to'
+    include Nanoc::Helpers::LinkTo
+
+    # TODO also relativize paths in CSS
+
+    def run(content)
+      content.gsub(/(src|href)=('|")(\/.+?)\2/) do
+        $1 + '=' + $2 + relative_path_to($3) + $2
+      end
+    end
+
+  end
+end
