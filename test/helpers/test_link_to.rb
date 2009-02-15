cf1d1bff2951a25eace4f69ecc33dc8a8ff66bcd
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-15 20:16:25 +0100

    added #relative_path_to method to LinkTo helper
    This helper method is based on the helper method by Christian Plessl. See <http://groups.google.com/group/nanoc/browse_thread/thread/b03c68d91786007b> for details.

--- test/helpers/test_link_to.rb
+++ test/helpers/test_link_to.rb
@@ -69,4 +69,52 @@ class Nanoc::Helpers::LinkToTest < MiniTest::Unit::TestCase
     )
   end
 
+  def test_relative_path_to_with_self
+    # Mock item
+    @item = MiniTest::Mock.new
+    @item.expect(:path, '/foo/bar/baz/')
+
+    # Test
+    assert_equal(
+      './',
+      relative_path_to('/foo/bar/baz/')
+    )
+  end
+
+  def test_relative_path_to_with_root
+    # Mock item
+    @item = MiniTest::Mock.new
+    @item.expect(:path, '/foo/bar/baz/')
+
+    # Test
+    assert_equal(
+      '../../../',
+      relative_path_to('/')
+    )
+  end
+
+  def test_relative_path_to_file
+    # Mock item
+    @item = MiniTest::Mock.new
+    @item.expect(:path, '/foo/bar/baz/')
+
+    # Test
+    assert_equal(
+      '../../quux',
+      relative_path_to('/foo/quux')
+    )
+  end
+
+  def test_relative_path_to_dir
+    # Mock item
+    @item = MiniTest::Mock.new
+    @item.expect(:path, '/foo/bar/baz/')
+
+    # Test
+    assert_equal(
+      '../../quux/',
+      relative_path_to('/foo/quux/')
+    )
+  end
+
 end
