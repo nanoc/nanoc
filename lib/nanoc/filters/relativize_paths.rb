79345c21be20b966f7474001afb319e50b3a52c1
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-15 20:45:43 +0100

    relativize_paths filter no longer requires arguments to be quoted

--- lib/nanoc/filters/relativize_paths.rb
+++ lib/nanoc/filters/relativize_paths.rb
@@ -9,8 +9,8 @@ module Nanoc::Filters
     # TODO also relativize paths in CSS
 
     def run(content)
-      content.gsub(/(src|href)=('|")(\/.+?)\2/) do
-        $1 + '=' + $2 + relative_path_to($3) + $2
+      content.gsub(/(src|href)=(['"]?)(\/.+?)\2([ >])/) do
+        $1 + '=' + $2 + relative_path_to($3) + $2 + $4
       end
     end
 
--- test/filters/test_relativize_paths.rb
+++ test/filters/test_relativize_paths.rb
@@ -43,6 +43,25 @@ class Nanoc::Filters::RelativizePathsTest < MiniTest::Unit::TestCase
     assert_equal(expected_content, actual_content)
   end
 
+  def test_filter_without_quotes
+    # Create filter with mock item
+    filter = Nanoc::Filters::RelativizePaths.new
+
+    # Mock item
+    filter.instance_eval do
+      @item = MiniTest::Mock.new
+      @item.expect(:path, '/foo/bar/baz/')
+    end
+
+    # Set content
+    raw_content      = %[<a href=/foo>foo</a>]
+    expected_content = %[<a href=../..>foo</a>]
+
+    # Test
+    actual_content = filter.run(raw_content)
+    assert_equal(expected_content, actual_content)
+  end
+
   def test_filter_multiple
     # Create filter with mock item
     filter = Nanoc::Filters::RelativizePaths.new
