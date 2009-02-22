8ee0ac3744cd5b28b80303c505512ef2fbfd1944
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-22 17:02:58 +0100

    bringing {Asset,Page,Item} documentation up to date

--- lib/nanoc/base/asset.rb
+++ lib/nanoc/base/asset.rb
@@ -1,16 +1,7 @@
 module Nanoc
 
-  # A Nanoc::Asset represents an asset in a nanoc site. It has content and
-  # attributes, as well as a path. It can also store the modification time to
-  # speed up compilation.
-  #
-  # An asset is observable. The following events will be notified:
-  #
-  # * :visit_started
-  # * :visit_ended
-  #
-  # Each asset has a list of asset representations or reps (Nanoc::AssetRep);
-  # compiling an asset actually compiles all of its assets.
+  # A Nanoc::Asset represents an asset in a nanoc site. It does not have any
+  # functionality of its own; all functionality can be found in Nanoc::Item.
   class Asset < Nanoc::Item
   end
 
--- lib/nanoc/base/page.rb
+++ lib/nanoc/base/page.rb
@@ -1,16 +1,7 @@
 module Nanoc
 
-  # A Nanoc::Page represents a page in a nanoc site. It has content and
-  # attributes, as well as a path. It can also store the modification time to
-  # speed up compilation.
-  #
-  # A page is observable. The following events will be notified:
-  #
-  # * :visit_started
-  # * :visit_ended
-  #
-  # Each page has a list of page representations or reps (Nanoc::PageRep);
-  # compiling a page actually compiles all of its representations.
+  # A Nanoc::Page represents a page in a nanoc site. It does not have any
+  # functionality of its own; all functionality can be found in Nanoc::Item.
   class Page < Nanoc::Item
   end
 
