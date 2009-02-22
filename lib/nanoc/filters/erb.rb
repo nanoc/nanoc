ba03eec4bec68bd856733c1eb014f6d018e7a69f
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-22 19:00:26 +0100

    ERB filter now sets filename

--- lib/nanoc/filters/erb.rb
+++ lib/nanoc/filters/erb.rb
@@ -10,7 +10,13 @@ module Nanoc::Filters
       context = ::Nanoc::Extra::Context.new(assigns)
 
       # Get result
-      ::ERB.new(content).result(context.get_binding)
+      erb = ::ERB.new(content)
+      if assigns[:page]
+        erb.filename = "page #{assigns[:_obj].path} (rep #{assigns[:_obj_rep].name})"
+      elsif assigns[:asset]
+        erb.filename = "asset #{assigns[:_obj].path} (rep #{assigns[:_obj_rep].name})"
+      end
+      erb.result(context.get_binding)
     end
 
   end
--- test/filters/test_erb.rb
+++ test/filters/test_erb.rb
@@ -14,4 +14,56 @@ class Nanoc::Filters::ERBTest < MiniTest::Unit::TestCase
     assert_equal('I was hiding in a cheap motel.', result)
   end
 
+  def test_filter_error_page
+    # Create item and item rep
+    item = MiniTest::Mock.new
+    item.expect(:path, '/foo/bar/baz/')
+    item_rep = MiniTest::Mock.new
+    item_rep.expect(:name, :quux)
+
+    # Create filter
+    filter = ::Nanoc::Filters::ERB.new({
+      :_obj     => item,
+      :_obj_rep => item_rep,
+      :page     => MiniTest::Mock.new,
+      :location => 'a cheap motel'
+    })
+
+    # Run filter
+    raised = false
+    begin
+      filter.run('<%= this isn\'t really ruby so it\'ll break, muahaha %>')
+    rescue SyntaxError => e
+      assert_match 'page /foo/bar/baz/ (rep quux)', e.backtrace[0]
+      raised = true
+    end
+    assert raised
+  end
+
+  def test_filter_error_asset
+    # Create item and item rep
+    item = MiniTest::Mock.new
+    item.expect(:path, '/foo/bar/baz/')
+    item_rep = MiniTest::Mock.new
+    item_rep.expect(:name, :quux)
+
+    # Create filter
+    filter = ::Nanoc::Filters::ERB.new({
+      :_obj     => item,
+      :_obj_rep => item_rep,
+      :asset    => MiniTest::Mock.new,
+      :location => 'a cheap motel'
+    })
+
+    # Run filter
+    raised = false
+    begin
+      filter.run('<%= this isn\'t really ruby so it\'ll break, muahaha %>')
+    rescue SyntaxError => e
+      assert_match 'asset /foo/bar/baz/ (rep quux)', e.backtrace[0]
+      raised = true
+    end
+    assert raised
+  end
+
 end
