a3864f99a467f1b9aa56a32f0e150a67b66f6e7b
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-28 13:27:48 +0100

    adding stubs for missing tests

--- test/base/test_item_rule.rb
+++ test/base/test_item_rule.rb
@@ -8,4 +8,16 @@ class Nanoc::ItemRuleTest < MiniTest::Unit::TestCase
   def test_stub
   end
 
+  def test_initialize
+    # TODO implement
+  end
+
+  def test_applicable_to
+    # TODO implement
+  end
+
+  def test_apply_to
+    # TODO implement
+  end
+
 end
