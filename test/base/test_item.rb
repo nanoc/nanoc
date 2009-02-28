0c5fc3ef530e8807507962d88ee7c4c3cc0f416b
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-28 13:27:58 +0100

    removing unused stub test

--- test/base/test_item.rb
+++ test/base/test_item.rb
@@ -5,9 +5,6 @@ class Nanoc::ItemTest < MiniTest::Unit::TestCase
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
 
-  def test_stub
-  end
-
   def test_to_proxy
     # TODO implement
   end
