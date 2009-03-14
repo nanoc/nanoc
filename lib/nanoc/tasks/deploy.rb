71a9bf64d6b78beab5a8d6e37c5d184fc920a93e
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-14 10:58:29 +0100

    adding deployment task stubs

---
+++ lib/nanoc/tasks/deploy.rb
@@ -0,0 +1,7 @@
+module Nanoc::Tasks::Deploy # :nodoc:
+end
+
+require 'nanoc/tasks/deploy/ftp'
+require 'nanoc/tasks/deploy/rsync'
+require 'nanoc/tasks/deploy/scp'
+require 'nanoc/tasks/deploy/sitecopy'
