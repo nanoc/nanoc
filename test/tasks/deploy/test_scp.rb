60a509e63718f55458c881780385a6471ab82da5
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-14 10:58:29 +0100

    adding deployment task stubs

---
+++ test/tasks/deploy/test_scp.rb
@@ -0,0 +1,12 @@
+require 'test/helper'
+
+class Nanoc::Tasks::Deploy::SCPTest < MiniTest::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_stub
+    # TODO implement
+  end
+
+end
