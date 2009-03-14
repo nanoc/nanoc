10d2f8856bd25d0845063222e100ff956458a60a
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-14 11:16:09 +0100

    test helper now requires nanoc tasks

--- test/helper.rb
+++ test/helper.rb
@@ -23,6 +23,7 @@ end
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
 require 'nanoc'
 require 'nanoc/cli'
+require 'nanoc/tasks'
 
 # Load miscellaneous requirements
 require 'stringio'
