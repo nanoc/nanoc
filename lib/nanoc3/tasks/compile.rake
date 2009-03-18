f6fd5c6767184f0c68ae54ad9bc6ccb3c983efc3
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-18 21:26:12 +0100

    removing (unimplemented) rake tasks which I will never use

--- lib/nanoc3/tasks/compile.rake
+++
@@ -1,9 +0,0 @@
-desc 'Compile the site'
-task :compile do
-  # TODO implement
-end
-
-desc 'Start the autocompiler'
-task :autocompile do
-  # TODO implement
-end
--- lib/nanoc3/tasks/deploy.rb
+++ lib/nanoc3/tasks/deploy.rb
@@ -1,7 +1,4 @@
 module Nanoc3::Tasks::Deploy # :nodoc:
 end
 
-require 'nanoc3/tasks/deploy/ftp'
 require 'nanoc3/tasks/deploy/rsync'
-require 'nanoc3/tasks/deploy/scp'
-require 'nanoc3/tasks/deploy/sitecopy'
