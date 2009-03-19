953f2b7a5d7c219b5910997a242e9b7b05ff67ac
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-19 14:33:24 +0100

    removing .rb extension from requires in base and data sources

--- lib/nanoc3/data_sources.rb
+++ lib/nanoc3/data_sources.rb
@@ -1,5 +1,5 @@
 module Nanoc3::DataSources # :nodoc:
 end
 
-require 'nanoc3/data_sources/filesystem.rb'
-require 'nanoc3/data_sources/filesystem_combined.rb'
+require 'nanoc3/data_sources/filesystem'
+require 'nanoc3/data_sources/filesystem_combined'
