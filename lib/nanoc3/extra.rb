450f5b9b04ae6003bf76fcbe0d79c5afc466ad4c
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-19 15:13:33 +0100

    made VCS filters not autoloaded (otherwise VCS.named breaks)

--- lib/nanoc3/extra.rb
+++ lib/nanoc3/extra.rb
@@ -3,9 +3,9 @@ module Nanoc3::Extra # :nodoc:
   autoload :AutoCompiler, 'nanoc3/extra/auto_compiler'
   autoload :Context,      'nanoc3/extra/context'
   autoload :FileProxy,    'nanoc3/extra/file_proxy'
-  autoload :VCS,          'nanoc3/extra/vcs'
-  autoload :VCSes,        'nanoc3/extra/vcses'
 
 end
 
 require 'nanoc3/extra/core_ext'
+require 'nanoc3/extra/vcs'
+require 'nanoc3/extra/vcses'
