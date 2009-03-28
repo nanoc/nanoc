846114c9a7e1caa7eaa50e97dfb5c0436bd439f8
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-28 11:08:34 +0100

    adding empty dsl test stub

---
+++ test/base/test_compiler_dsl.rb
@@ -0,0 +1,11 @@
+require 'test/helper'
+
+class Nanoc3::CompilerDSLTest < MiniTest::Unit::TestCase
+
+  def setup    ; global_setup    ; end
+  def teardown ; global_teardown ; end
+
+  def test_stub
+  end
+
+end
