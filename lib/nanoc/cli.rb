91dc6a1fd204c219d7a56eecd0d81d4356a7d981
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-28 13:25:31 +0100

    removing erroneous and outdated requires
    --HG--
    branch : 2.2.x

--- lib/nanoc/cli.rb
+++ lib/nanoc/cli.rb
@@ -5,8 +5,5 @@ module Nanoc::CLI # :nodoc:
 end
 
 require 'nanoc/cli/base'
-require 'nanoc/cli/command'
 require 'nanoc/cli/commands'
-require 'nanoc/cli/ext'
 require 'nanoc/cli/logger'
-require 'nanoc/cli/option_parser'
