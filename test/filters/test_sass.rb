8650bbd9df2b61ba07aef8bca1dc67cb9979d35e
Denis Defreyne <denis.defreyne@stoneship.org>
2009-03-30 19:28:14 +0200

    fixed sass test name
    --HG--
    branch : 2.2.x

--- test/filters/test_sass.rb
+++ test/filters/test_sass.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class Nanoc::Filters::HamlTest < Test::Unit::TestCase
+class Nanoc::Filters::SassTest < Test::Unit::TestCase
 
   def setup    ; global_setup    ; end
   def teardown ; global_teardown ; end
