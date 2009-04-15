9346427e6defbd7f15595e7c7123e08d459327d6
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-15 15:43:49 +0200

    added simple clean task

---
+++ lib/nanoc3/tasks/clean.rb
@@ -0,0 +1,31 @@
+module Nanoc3::Tasks
+
+  class Clean
+
+    def initialize(site)
+      @site = site
+    end
+
+    def run
+      # Load site data
+      @site.load_data
+
+      # Delete all compiled item reps
+      filenames.each do |filename|
+        FileUtils.rm_f filename
+      end
+    end
+
+  private
+
+    def filenames
+      @site.items.map do |item|
+        item.reps.map do |rep|
+          rep.raw_path
+        end
+      end.flatten
+    end
+
+  end
+
+end
---
+++ test/tasks/test_clean.rb
@@ -0,0 +1,46 @@
+require 'test/helper'
+
+class Nanoc3::Tasks::CleanTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  def test_simple
+    if_have 'w3c_validators' do
+      # Stub items
+      items = [ mock, mock ]
+      reps  = [ [ mock, mock ], [ mock, mock ] ]
+      items[0].expects(:reps).returns(reps[0])
+      items[1].expects(:reps).returns(reps[1])
+
+      # Create sample files
+      [ 0, 1 ].each do |item_id|
+        [ 0, 1 ].each do |rep_id|
+          filename = "item-#{item_id}-rep-#{rep_id}.txt"
+          reps[item_id][rep_id].expects(:raw_path).returns(filename)
+          File.open(filename, 'w') { |io| io.write('hello') }
+          assert File.file?(filename)
+        end
+      end
+
+      # Stub site
+      site = mock
+      site.expects(:load_data)
+      site.expects(:items).returns(items)
+
+      # Create clean task
+      clean = ::Nanoc3::Tasks::Clean.new(site)
+
+      # Run
+      clean.run
+
+      # Check
+      [ 0, 1 ].each do |item_id|
+        [ 0, 1 ].each do |rep_id|
+          filename = "item-#{item_id}-rep-#{rep_id}.txt"
+          assert !File.file?(filename)
+        end
+      end
+    end
+  end
+
+end
