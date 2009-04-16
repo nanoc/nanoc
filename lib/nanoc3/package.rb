3f6891d9a580420257b67a03ff68095510cccc99
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-16 10:40:30 +0200

    DRYed up package

--- lib/nanoc3/package.rb
+++ lib/nanoc3/package.rb
@@ -8,10 +8,44 @@ module Nanoc3
 
     include Singleton
 
-    # Returns a Gem::Specification used for packaging.
+    # The name of the application.
+    def name
+      'nanoc3'
+    end
+
+    # The files to include in the package. This is also the list of files that
+    # will be included in the documentation (with the exception of the files
+    # in undocumented_files).
+    def files
+      %w( ChangeLog LICENSE NEWS Rakefile README ) +
+      Dir['bin/**/*'] +
+      Dir['lib/**/*'] +
+      Dir['vendor/**/*']
+    end
+
+    # The files that should not be included in the documentation.
+    def undocumented_files
+      Dir['lib/**/*.rake'] +
+      Dir['vendor/**/*']
+    end
+
+    # The files that are not documented by RDoc by default, but should still
+    # be included in the documentation.
+    def extra_rdoc_files
+      files - Dir['lib/**/*']
+    end
+
+    # The name of the file that should be used as entry point for the
+    # documentation.
+    def main_documentation_file
+      # FIXME rename this to README.rdoc or so
+      'README'
+    end
+
+    # The Gem::Specification used for packaging.
     def gem_spec
       @gem_spec ||= Gem::Specification.new do |s|
-        s.name                  = 'nanoc3'
+        s.name                  = self.name
         s.version               = Nanoc3::VERSION
         s.platform              = Gem::Platform::RUBY
         s.summary               = 'a tool that runs on your local computer ' +
@@ -41,18 +75,15 @@ EOS
         s.required_ruby_version = '>= 1.8.5'
 
         s.has_rdoc              = true
-        s.extra_rdoc_files      = [ 'README' ]
-        s.rdoc_options          <<  '--title'   << 'nanoc'                    <<
-                                    '--main'    << 'README'                   <<
-                                    '--charset' << 'utf-8'                    <<
-                                    '--exclude' << 'lib/nanoc3/cli/commands'  <<
-                                    '--exclude' << 'lib/nanoc3/extra/vcses'   <<
-                                    '--exclude' << 'lib/nanoc3/filters'       <<
-                                    '--exclude' << 'test'                     <<
-                                    '--line-numbers'
+        s.extra_rdoc_files      = self.extra_rdoc_files
+        s.rdoc_options          = []
+        s.rdoc_options          += [ '--title', self.name                    ]
+        s.rdoc_options          += [ '--main',  self.main_documentation_file ]
+        self.undocumented_files.each do |undocumented_file|
+          s.rdoc_options        += [ '--exclude', undocumented_file ]
+        end
 
-        s.files                 = %w( README LICENSE ChangeLog Rakefile ) +
-                                  Dir[File.join('{bin,lib,vendor}', '**', '*')]
+        s.files                 = self.files
         s.executables           = [ 'nanoc3' ]
         s.require_path          = 'lib'
         s.bindir                = 'bin'
