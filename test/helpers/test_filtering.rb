df3058786330d815a2a6a510d834f4cf61f646ee
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-19 15:27:03 +0200

    made filtering helper set proper assigns

--- test/helpers/test_filtering.rb
+++ test/helpers/test_filtering.rb
@@ -6,7 +6,7 @@ class Nanoc3::Helpers::FilteringTest < MiniTest::Unit::TestCase
 
   include Nanoc3::Helpers::Filtering
 
-  def test_filter
+  def test_filter_simple
     if_have 'rubypants' do
       # Build content to be evaluated
       content = "<p>Foo...</p>\n" +
@@ -14,13 +14,39 @@ class Nanoc3::Helpers::FilteringTest < MiniTest::Unit::TestCase
                 " <p>Bar...</p>\n" +
                 "<% end %>\n"
 
+      # Mock item and rep
+      @_item_rep = mock
+      @_item_rep.expects(:assigns).returns({})
+
       # Evaluate content
-      @item = {}
       result = ::ERB.new(content).result(binding)
 
       # Check
       assert_match('<p>Foo...</p>',     result)
       assert_match('<p>Bar&#8230;</p>', result)
+    end
+  end
+
+  def test_filter_with_assigns
+    if_have 'rubypants' do
+      # Build content to be evaluated
+      content = "<p>Foo...</p>\n" +
+                "<% filter :erb do %>\n" +
+                " <p><%%= @item.title %></p>\n" +
+                "<% end %>\n"
+
+      # Mock item and rep
+      item = mock
+      item.expects(:title).returns('Bar...')
+      @_item_rep = mock
+      @_item_rep.expects(:assigns).returns({ :item => item })
+
+      # Evaluate content
+      result = ::ERB.new(content).result(binding)
+
+      # Check
+      assert_match('<p>Foo...</p>', result)
+      assert_match('<p>Bar...</p>', result)
     end
   end
 
