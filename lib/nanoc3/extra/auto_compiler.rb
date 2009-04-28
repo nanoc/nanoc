83232a211ca0bb2974078ac2ffac7e2fb5d48a00
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-28 20:52:45 +0200

    made autocompiler not handle exceptions (use Rack::ShowExceptions instead)

--- lib/nanoc3/extra/auto_compiler.rb
+++ lib/nanoc3/extra/auto_compiler.rb
@@ -21,41 +21,6 @@ module Nanoc3::Extra
 </html>
 END
 
-    ERROR_500 = <<END
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-	<head>
-		<title>500 Server Error</title>
-		<style type="text/css">
-			body { padding: 10px; border: 10px solid #f00; margin: 10px; font-family: Helvetica, Arial, sans-serif; }
-		</style>
-	</head>
-	<body>
-		<h1>500 Server Error</h1>
-		<p>An error occurred while compiling the item you requested, <i><%=h path %></i>.</p>
-		<p>If you think this is a bug in nanoc, please do <a href="http://projects.stoneship.org/trac/nanoc/newticket">report it</a>&mdash;thanks!</p>
-		<p>Message:</p>
-		<blockquote><p><%=h message %></p></blockquote>
-		<p>Item compilation stack:</p>
-		<ol>
-<% @site.compiler.stack.reverse.each do |obj| %>
-<% if item.is_a?(Nanoc3::ItemRep) %>
-			<li><strong>Item</strong> <%= obj.item.identifier %> (rep <%= obj.name %>)</li>
-<% else # layout %>
-			<li><strong>Layout</strong> <%= obj.identifier %></li>
-<% end %>
-<% end %>
-		</ol>
-		<p>Backtrace:</p>
-		<ol>
-<% exception.backtrace.each do |line| %>
-			<li><%= line %></li>
-<% end %>
-		</ol>
-	</body>
-</html>
-END
-
     # Creates a new autocompiler for the given site.
     def initialize(site)
       # Set site
@@ -68,24 +33,17 @@ END
     def call(env)
       require 'mime/types'
 
-      handle_request(env['PATH_INFO'])
-    rescue Exception => exception
-      return serve_500(nil, exception)
-    end
-
-  private
-
-    def handle_request(path)
       @mutex.synchronize do
         # Reload site data
         @site.load_data(true)
         build_reps
 
         # Get file path
+        path = env['PATH_INFO']
         file_path = @site.config[:output_dir] + path
 
         # Find rep
-        reps = @site.items.map { |o| o.reps }.flatten
+        reps = @site.items.map { |i| i.reps }.flatten
         rep = reps.find { |r| r.path == path }
 
         if rep.nil?
@@ -108,7 +66,22 @@ END
           serve_rep(rep)
         end
       end
+    rescue StandardError, LoadError, SyntaxError => e
+      # Add compilation stack to env
+      env['nanoc.stack'] = []
+      @site.compiler.stack.reverse.each do |obj|
+        if obj.is_a?(Nanoc3::ItemRep) # item rep
+          env['nanoc.stack'] << "[item] #{obj.item.identifier} (rep #{obj.name})"
+        else # layout
+          env['nanoc.stack'] << "[layout] #{obj.identifier}"
+        end
+      end
+
+      # Re-raise error
+      raise e
     end
+
+  private
 
     def h(s)
       ERB::Util.html_escape(s)
@@ -140,31 +113,6 @@ END
       ]
     end
 
-    def serve_500(path, exception)
-      # Build message
-      case exception
-      when Nanoc3::Errors::UnknownLayoutError
-        message = "Unknown layout: #{exception.message}"
-      when Nanoc3::Errors::UnknownFilterError
-        message = "Unknown filter: #{exception.message}"
-      when Nanoc3::Errors::CannotDetermineFilterError
-        message = "Cannot determine filter for layout: #{exception.message}"
-      when Nanoc3::Errors::RecursiveCompilationError
-        message = "Recursive call to item content. Item stack:"
-      when Nanoc3::Errors::NoLongerSupportedError
-        message = "No longer supported: #{exception.message}"
-      else
-        message = "Unknown error: #{exception.message}"
-      end
-
-      # Build response
-      [
-        500,
-        { 'Content-Type' => 'text/html' },
-        [ ERB.new(ERROR_500).result(binding) ]
-      ]
-    end
-
     def serve_file(path)
       # Build response
       [
@@ -176,11 +124,7 @@ END
 
     def serve_rep(rep)
       # Recompile rep
-      begin
-        @site.compiler.run([ rep.item ], :force => true)
-      rescue Exception => exception
-        return serve_500(rep.path, exception)
-      end
+      @site.compiler.run([ rep.item ], :force => true)
 
       # Build response
       [
