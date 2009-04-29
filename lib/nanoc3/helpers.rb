3d2334029339e24a8ff7cebc571590fbba2a7665
Denis Defreyne <denis.defreyne@stoneship.org>
2009-04-29 15:46:32 +0200

    made autoload use strings instead symbols for consistency

--- lib/nanoc3/helpers.rb
+++ lib/nanoc3/helpers.rb
@@ -1,13 +1,13 @@
 module Nanoc3::Helpers
 
-  autoload :Blogging,   'nanoc3/helpers/blogging'
-  autoload :Capturing,  'nanoc3/helpers/capturing'
-  autoload :Filtering,  'nanoc3/helpers/filtering'
-  autoload :HTMLEscape, 'nanoc3/helpers/html_escape'
-  autoload :LinkTo,     'nanoc3/helpers/link_to'
-  autoload :Render,     'nanoc3/helpers/render'
-  autoload :Tagging,    'nanoc3/helpers/tagging'
-  autoload :Text,       'nanoc3/helpers/text'
-  autoload :XMLSitemap, 'nanoc3/helpers/xml_sitemap'
+  autoload 'Blogging',   'nanoc3/helpers/blogging'
+  autoload 'Capturing',  'nanoc3/helpers/capturing'
+  autoload 'Filtering',  'nanoc3/helpers/filtering'
+  autoload 'HTMLEscape', 'nanoc3/helpers/html_escape'
+  autoload 'LinkTo',     'nanoc3/helpers/link_to'
+  autoload 'Render',     'nanoc3/helpers/render'
+  autoload 'Tagging',    'nanoc3/helpers/tagging'
+  autoload 'Text',       'nanoc3/helpers/text'
+  autoload 'XMLSitemap', 'nanoc3/helpers/xml_sitemap'
 
 end
