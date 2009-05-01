3165858dcf7399ae6396f4ca7ab92168dad204e7
Denis Defreyne <denis.defreyne@stoneship.org>
2009-05-01 19:05:02 +0200

    added breadcrumbs helper

---
+++ lib/nanoc3/helpers/breadcrumbs.rb
@@ -0,0 +1,23 @@
+module Nanoc3::Helpers
+
+  # Nanoc3::Helpers::Breadcrumbs provides support for breadcrumbs, which allow
+  # the user to go up in the page hierarchy.
+  module Breadcrumbs
+
+    # Returns the breadcrumbs trail as an array. Higher items (items that are
+    # closer to the root) come before lower items.
+    def breadcrumbs_trail
+      trail = [] 
+      item = @item 
+
+      begin
+        trail.unshift(item) 
+        item = item.parent 
+      end until item.nil?
+
+      trail
+    end
+
+  end
+
+end
