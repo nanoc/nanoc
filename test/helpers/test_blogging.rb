78541c66f78ff62e669a1eacd2368ca3fb50c033
Denis Defreyne <denis.defreyne@stoneship.org>
2009-05-01 13:26:54 +0200

    made atom_feed tests skipped if builder is not available

--- test/helpers/test_blogging.rb
+++ test/helpers/test_blogging.rb
@@ -110,267 +110,287 @@ class Nanoc3::Helpers::BloggingTest < MiniTest::Unit::TestCase
   end
 
   def test_atom_feed_without_articles
-    # Mock items
-    @items = [ mock, mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('item')
-    @items[2].stubs(:[]).with(:kind).returns('item')
+    if_have 'builder' do
+      # Mock items
+      @items = [ mock, mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('item')
+      @items[2].stubs(:[]).with(:kind).returns('item')
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: no articles',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: no articles',
-      error.message
-    )
   end
 
   def test_atom_feed_without_base_url
-    # Create items
-    @items = [ mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('article')
+    if_have 'builder' do
+      # Create items
+      @items = [ mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('article')
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns(nil)
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns(nil)
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: feed item has no base_url',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: feed item has no base_url',
-      error.message
-    )
   end
 
   def test_atom_feed_without_title
-    # Create items
-    @items = [ mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('article')
+    if_have 'builder' do
+      # Create items
+      @items = [ mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('article')
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns(nil)
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns(nil)
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: feed item has no title',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: feed item has no title',
-      error.message
-    )
   end
 
   def test_atom_feed_without_author_name
-    # Create items
-    @items = [ mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('article')
+    if_have 'builder' do
+      # Create items
+      @items = [ mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('article')
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns(nil)
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns(nil)
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: feed item has no author_name',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: feed item has no author_name',
-      error.message
-    )
   end
 
   def test_atom_feed_without_author_uri
-    # Create items
-    @items = [ mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('article')
+    if_have 'builder' do
+      # Create items
+      @items = [ mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('article')
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns(nil)
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns(nil)
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: feed item has no author_uri',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: feed item has no author_uri',
-      error.message
-    )
   end
 
   def test_atom_feed_without_articles_created_at
-    # Create items
-    @items = [ mock, mock, mock, mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('item')
-    @items[1].stubs(:[]).with(:kind).returns('article')
-    @items[1].stubs(:[]).with(:created_at).returns(Time.now)
-    @items[2].stubs(:[]).with(:kind).returns('article')
-    @items[2].stubs(:[]).with(:created_at).returns(Time.now)
-    @items[3].stubs(:[]).with(:kind).returns('article')
-    @items[3].stubs(:[]).with(:created_at).returns(Time.now)
-    @items[4].stubs(:[]).with(:kind).returns('article')
-    @items[4].stubs(:[]).with(:created_at).returns(nil)
+    if_have 'builder' do
+      # Create items
+      @items = [ mock, mock, mock, mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('item')
+      @items[1].stubs(:[]).with(:kind).returns('article')
+      @items[1].stubs(:[]).with(:created_at).returns(Time.now)
+      @items[2].stubs(:[]).with(:kind).returns('article')
+      @items[2].stubs(:[]).with(:created_at).returns(Time.now)
+      @items[3].stubs(:[]).with(:kind).returns('article')
+      @items[3].stubs(:[]).with(:created_at).returns(Time.now)
+      @items[4].stubs(:[]).with(:kind).returns('article')
+      @items[4].stubs(:[]).with(:created_at).returns(nil)
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
 
-    # Check
-    error = assert_raises(RuntimeError) do
-      atom_feed
+      # Check
+      error = assert_raises(RuntimeError) do
+        atom_feed
+      end
+      assert_equal(
+        'Cannot build Atom feed: one or more articles lack created_at',
+        error.message
+      )
     end
-    assert_equal(
-      'Cannot build Atom feed: one or more articles lack created_at',
-      error.message
-    )
   end
 
   def test_atom_feed_with_articles_param
-    # Mock items
-    @items = [ mock, mock, mock, mock, mock ]
-    @items[0].stubs(:[]).with(:kind).returns('article')
-    @items[1].stubs(:[]).with(:kind).returns('article')
-    @items[2].stubs(:[]).with(:kind).returns('article')
-    @items[3].stubs(:[]).with(:kind).returns('article')
-    @items[4].stubs(:[]).with(:kind).returns('article')
+    if_have 'builder' do
+      # Mock items
+      @items = [ mock, mock, mock, mock, mock ]
+      @items[0].stubs(:[]).with(:kind).returns('article')
+      @items[1].stubs(:[]).with(:kind).returns('article')
+      @items[2].stubs(:[]).with(:kind).returns('article')
+      @items[3].stubs(:[]).with(:kind).returns('article')
+      @items[4].stubs(:[]).with(:kind).returns('article')
 
-    # Mock one article
-    @items[4].stubs(:mtime).returns(Time.now - 500)
-    @items[4].stubs(:content).returns('item 1 content')
-    @items[4].stubs(:[]).with(:created_at).returns(Time.now - 1000)
-    @items[4].stubs(:[]).with(:title).returns('Item One')
-    @items[4].stubs(:[]).with(:custom_path_in_feed).returns(nil)
-    @items[4].stubs(:[]).with(:path).returns('/item1/')
-    @items[4].stubs(:[]).with(:excerpt).returns(nil)
-    item_rep = mock
-    item_rep.stubs(:path).returns('/asdf/fdsa/')
-    @items[4].stubs(:reps).returns([ item_rep ])
+      # Mock one article
+      @items[4].stubs(:mtime).returns(Time.now - 500)
+      @items[4].stubs(:content).returns('item 1 content')
+      @items[4].stubs(:[]).with(:created_at).returns(Time.now - 1000)
+      @items[4].stubs(:[]).with(:title).returns('Item One')
+      @items[4].stubs(:[]).with(:custom_path_in_feed).returns(nil)
+      @items[4].stubs(:[]).with(:path).returns('/item1/')
+      @items[4].stubs(:[]).with(:excerpt).returns(nil)
+      item_rep = mock
+      item_rep.stubs(:path).returns('/asdf/fdsa/')
+      @items[4].stubs(:reps).returns([ item_rep ])
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
-    @item.stubs(:[]).with(:[]).with(:feed_url).returns('http://example.com/feed')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      @item.stubs(:[]).with(:[]).with(:feed_url).returns('http://example.com/feed')
 
-    # Check
-    atom_feed :articles => [ @items[4] ]
+      # Check
+      atom_feed :articles => [ @items[4] ]
+    end
   end
 
   def test_atom_feed_with_limit_param
-    # Mock articles
-    @items = [ mock, mock, mock, mock, mock ]
-    @items.each_with_index do |article, i|
-      article.stubs(:mtime).returns(Time.now - 500)
-      article.stubs(:content).returns("item #{i} content")
-      article.stubs(:[]).with(:kind).returns('article')
-      article.stubs(:[]).with(:created_at).returns(Time.now - 1000*i)
-      article.stubs(:[]).with(:title).returns("Article #{i}")
-      article.stubs(:[]).with(:custom_path_in_feed).returns(nil)
-      article.stubs(:[]).with(:path).returns("/articles/#{i}/")
-      article.stubs(:[]).with(:excerpt).returns(nil)
-      item_rep = mock
-      item_rep.stubs(:path).returns("/articles/#{i}/")
-      article.stubs(:reps).returns([ item_rep ])
+    if_have 'builder' do
+      # Mock articles
+      @items = [ mock, mock, mock, mock, mock ]
+      @items.each_with_index do |article, i|
+        article.stubs(:mtime).returns(Time.now - 500)
+        article.stubs(:content).returns("item #{i} content")
+        article.stubs(:[]).with(:kind).returns('article')
+        article.stubs(:[]).with(:created_at).returns(Time.now - 1000*i)
+        article.stubs(:[]).with(:title).returns("Article #{i}")
+        article.stubs(:[]).with(:custom_path_in_feed).returns(nil)
+        article.stubs(:[]).with(:path).returns("/articles/#{i}/")
+        article.stubs(:[]).with(:excerpt).returns(nil)
+        item_rep = mock
+        item_rep.stubs(:path).returns("/articles/#{i}/")
+        article.stubs(:reps).returns([ item_rep ])
+      end
+
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      @item.stubs(:[]).with(:feed_url).returns('http://example.com/feed')
+
+      # Check
+      result = atom_feed :limit => 3, :articles => @items
+      assert_match(
+        Regexp.new('Article 0.*Article 1.*Article 2', Regexp::MULTILINE),
+        result
+      )
     end
-
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
-    @item.stubs(:[]).with(:feed_url).returns('http://example.com/feed')
-
-    # Check
-    result = atom_feed :limit => 3, :articles => @items
-    assert_match(
-      Regexp.new('Article 0.*Article 1.*Article 2', Regexp::MULTILINE),
-      result
-    )
   end
 
   def test_atom_feed_with_content_proc_param
-    # Mock article
-    @items = [ mock ]
-    @items[0].stubs(:mtime).returns(Time.now - 500)
-    @items[0].stubs(:[]).with(:kind).returns('article')
-    @items[0].stubs(:[]).with(:created_at).returns(Time.now - 1000)
-    @items[0].stubs(:[]).with(:title).returns('Item One')
-    @items[0].stubs(:[]).with(:custom_path_in_feed).returns(nil)
-    @items[0].stubs(:[]).with(:excerpt).returns(nil)
-    item_rep = mock
-    item_rep.stubs(:path).returns('/item1/')
-    @items[0].stubs(:reps).returns([ item_rep ])
+    if_have 'builder' do
+      # Mock article
+      @items = [ mock ]
+      @items[0].stubs(:mtime).returns(Time.now - 500)
+      @items[0].stubs(:[]).with(:kind).returns('article')
+      @items[0].stubs(:[]).with(:created_at).returns(Time.now - 1000)
+      @items[0].stubs(:[]).with(:title).returns('Item One')
+      @items[0].stubs(:[]).with(:custom_path_in_feed).returns(nil)
+      @items[0].stubs(:[]).with(:excerpt).returns(nil)
+      item_rep = mock
+      item_rep.stubs(:path).returns('/item1/')
+      @items[0].stubs(:reps).returns([ item_rep ])
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
-    @item.stubs(:[]).with(:feed_url).returns('http://example.com/feed')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      @item.stubs(:[]).with(:feed_url).returns('http://example.com/feed')
 
-    # Check
-    result = atom_feed :content_proc => lambda { |a| 'foobar!' }
-    assert_match 'foobar!</content>', result
+      # Check
+      result = atom_feed :content_proc => lambda { |a| 'foobar!' }
+      assert_match 'foobar!</content>', result
+    end
   end
 
   def test_atom_feed_with_excerpt_proc_param
-    # Mock article
-    @items = [ mock ]
-    @items[0].stubs(:mtime).returns(Time.now - 500)
-    @items[0].stubs(:[]).with(:kind).returns('article')
-    @items[0].stubs(:[]).with(:created_at).returns(Time.now - 1000)
-    @items[0].stubs(:[]).with(:title).returns('Item One')
-    @items[0].stubs(:[]).with(:custom_path_in_feed).returns(nil)
-    @items[0].stubs(:content).returns('some content')
-    item_rep = mock
-    item_rep.stubs(:path).returns('/item1/')
-    @items[0].stubs(:reps).returns([ item_rep ])
+    if_have 'builder' do
+      # Mock article
+      @items = [ mock ]
+      @items[0].stubs(:mtime).returns(Time.now - 500)
+      @items[0].stubs(:[]).with(:kind).returns('article')
+      @items[0].stubs(:[]).with(:created_at).returns(Time.now - 1000)
+      @items[0].stubs(:[]).with(:title).returns('Item One')
+      @items[0].stubs(:[]).with(:custom_path_in_feed).returns(nil)
+      @items[0].stubs(:content).returns('some content')
+      item_rep = mock
+      item_rep.stubs(:path).returns('/item1/')
+      @items[0].stubs(:reps).returns([ item_rep ])
 
-    # Create feed item
-    @item = mock
-    @item.stubs(:[]).with(:base_url).returns('http://example.com')
-    @item.stubs(:[]).with(:title).returns('My Blog Or Something')
-    @item.stubs(:[]).with(:author_name).returns('J. Doe')
-    @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
-    @item.stubs(:[]).with(:[]).with(:feed_url).returns('http://example.com/feed')
+      # Create feed item
+      @item = mock
+      @item.stubs(:[]).with(:base_url).returns('http://example.com')
+      @item.stubs(:[]).with(:title).returns('My Blog Or Something')
+      @item.stubs(:[]).with(:author_name).returns('J. Doe')
+      @item.stubs(:[]).with(:author_uri).returns('http://example.com/~jdoe')
+      @item.stubs(:[]).with(:[]).with(:feed_url).returns('http://example.com/feed')
 
-    # Check
-    result = atom_feed :excerpt_proc => lambda { |a| 'foobar!' }
-    assert_match 'foobar!</summary>', result
+      # Check
+      result = atom_feed :excerpt_proc => lambda { |a| 'foobar!' }
+      assert_match 'foobar!</summary>', result
+    end
   end
 
   def test_url_for_without_custom_path_in_feed
