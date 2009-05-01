a2f877a4257ee5b1b1038726c43e615dcecbf122
Denis Defreyne <denis.defreyne@stoneship.org>
2009-05-01 19:05:02 +0200

    added breadcrumbs helper

---
+++ test/helpers/test_breadcrumbs.rb
@@ -0,0 +1,60 @@
+require 'test/helper'
+
+class Nanoc3::Helpers::BreadcrumbsTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  include Nanoc3::Helpers::Breadcrumbs
+
+  def test_breadcrumbs_trail_with_0_parents
+    # Mock item
+    @item = mock
+    @item.expects(:parent).returns(nil)
+
+    # Build trail
+    trail = breadcrumbs_trail
+
+    # Check
+    assert_equal(
+      [ @item ],
+      trail
+    )
+  end
+
+  def test_breadcrumbs_trail_with_1_parent
+    # Mock item
+    parent = mock
+    parent.expects(:parent).returns(nil)
+    @item = mock
+    @item.expects(:parent).returns(parent)
+
+    # Build trail
+    trail = breadcrumbs_trail
+
+    # Check
+    assert_equal(
+      [ parent, @item ],
+      trail
+    )
+  end
+
+  def test_breadcrumbs_trail_with_many_parents
+    # Mock item
+    grandparent = mock
+    grandparent.expects(:parent).returns(nil)
+    parent = mock
+    parent.expects(:parent).returns(grandparent)
+    @item = mock
+    @item.expects(:parent).returns(parent)
+
+    # Build trail
+    trail = breadcrumbs_trail
+
+    # Check
+    assert_equal(
+      [ grandparent, parent, @item ],
+      trail
+    )
+  end
+
+end
