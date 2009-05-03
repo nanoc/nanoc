c9768554daa4f8541783a86f966a47facd7b2fd0
Denis Defreyne <denis.defreyne@stoneship.org>
2009-05-03 14:11:11 +0200

    made autocompiler use Rack::File for serving static files

--- test/extra/test_auto_compiler.rb
+++ test/extra/test_auto_compiler.rb
@@ -18,11 +18,11 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
     site = mock
     site.stubs(:load_data).with(true)
     site.stubs(:items).returns(items)
-    site.stubs(:config).returns({ :output_dir => 'output/', :index_filenames => [ 'index.html' ] })
+    site.stubs(:config).returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_rep).with(item_reps[1])
+    autocompiler.expects(:serve).with(item_reps[1])
     autocompiler.stubs(:build_reps)
 
     # Run
@@ -43,127 +43,193 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
     site = mock
     site.expects(:load_data).with(true)
     site.expects(:items).returns(items)
-    site.expects(:config).returns({ :output_dir => 'output/', :index_filenames => [ 'index.html' ] })
+    site.expects(:config).returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = '/foo/2'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
 
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_404).with('/foo/2')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => '/foo/2') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_file
-    # Create items and reps
-    item_reps = [ mock, mock, mock ]
-    item_reps[0].expects(:path).returns('/foo/1/')
-    item_reps[1].expects(:path).returns('/foo/2/')
-    item_reps[2].expects(:path).returns('/bar/')
-    items = [ mock, mock ]
-    items[0].expects(:reps).returns([ item_reps[0], item_reps[1] ])
-    items[1].expects(:reps).returns([ item_reps[2] ])
+    # Create file
+    FileUtils.mkdir_p('out')
+    File.open('out/somefile.txt', 'w') { |io| io.write('hello') }
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'somefile.txt'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
 
     # Create site
     site = mock
     site.expects(:load_data).with(true)
-    site.expects(:items).returns(items)
-    site.expects(:config).at_least_once.returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
-
-    # Create file
-    FileUtils.mkdir_p('out')
-    File.open('out/somefile.txt', 'w') { |io| }
+    site.expects(:items).returns([])
+    site.expects(:config).returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_file).with('out/somefile.txt')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => 'somefile.txt') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_dir_with_slash_with_index_file
+    # Create file
+    FileUtils.mkdir_p('out/foo/bar')
+    File.open('out/foo/bar/index.html', 'w') { |io| io.write('hello') }
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'foo/bar/index.html'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
+
     # Create site
     site = mock
     site.expects(:load_data).with(true)
     site.expects(:items).returns([])
     site.expects(:config).at_least_once.returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
-    # Create file
-    FileUtils.mkdir_p('out/foo/bar')
-    File.open('out/foo/bar/index.html', 'w') { |io| }
-
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_file).with('out/foo/bar/index.html')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => 'foo/bar/') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_dir_with_slash_without_index_file
+    # Create file
+    FileUtils.mkdir_p('out/foo/bar')
+    File.open('out/foo/bar/someotherfile.txt', 'w') { |io| io.write('hello') }
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'foo/bar/'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
+
     # Create site
     site = mock
     site.expects(:load_data).with(true)
     site.expects(:items).returns([])
     site.expects(:config).at_least_once.returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
-    # Create file
-    FileUtils.mkdir_p('out/foo/bar')
-    File.open('out/foo/bar/someotherfile.txt', 'w') { |io| }
-
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_404).with('foo/bar/')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => 'foo/bar/') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_dir_without_slash_with_index_file
+    # Create file
+    FileUtils.mkdir_p('out/foo/bar')
+    File.open('out/foo/bar/index.html', 'w') { |io| io.write('hello') }
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'foo/bar'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
+
     # Create site
     site = mock
     site.expects(:load_data).with(true)
     site.expects(:items).returns([])
     site.expects(:config).at_least_once.returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
-    # Create file
-    FileUtils.mkdir_p('out/foo/bar')
-    File.open('out/foo/bar/index.html', 'w') { |io| }
-
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_404).with('foo/bar')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => 'foo/bar') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_dir_without_slash_without_index_file
+    # Create file
+    FileUtils.mkdir_p('out/foo/bar')
+    File.open('out/foo/bar/someotherfile.txt', 'w') { |io| io.write('hello') }
+
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'foo/bar'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
+
     # Create site
     site = mock
     site.expects(:load_data).with(true)
     site.expects(:items).returns([])
     site.expects(:config).at_least_once.returns({ :output_dir => 'out/', :index_filenames => [ 'index.html' ] })
 
-    # Create file
-    FileUtils.mkdir_p('out/foo/bar')
-    File.open('out/foo/bar/someotherfile.txt', 'w') { |io| }
-
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_404).with('foo/bar')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
     autocompiler.instance_eval { call('PATH_INFO' => 'foo/bar') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_handle_request_with_404
+    # Create file server
+    file_server = mock
+    def file_server.call(env)
+      @expected_path_info = 'four-oh-four.txt'
+      @actual_path_info   = env['PATH_INFO']
+    end
+    def file_server.expected_path_info ; @expected_path_info ; end
+    def file_server.actual_path_info   ; @actual_path_info   ; end
+
     # Create site
     site = mock
     site.expects(:load_data).with(true)
@@ -172,11 +238,13 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
 
     # Create autocompiler
     autocompiler = Nanoc3::Extra::AutoCompiler.new(site)
-    autocompiler.expects(:serve_404).with('someotherfile.txt')
-    autocompiler.stubs(:build_reps)
+    autocompiler.expects(:file_server).returns(file_server)
 
     # Run
-    autocompiler.instance_eval { call('PATH_INFO' => 'someotherfile.txt') }
+    autocompiler.instance_eval { call('PATH_INFO' => 'four-oh-four.txt') }
+
+    # Check
+    assert_equal(file_server.expected_path_info, file_server.actual_path_info)
   end
 
   def test_h
@@ -196,14 +264,14 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
       autocompiler = Nanoc3::Extra::AutoCompiler.new(nil)
 
       # Create known test file
-      File.open('foo.html', 'w') { |io| }
+      File.open('foo.html', 'w') { |io| io.write('hello') }
       assert_equal(
         'text/html',
         autocompiler.instance_eval { mime_type_of('foo.html', 'huh') }
       )
 
       # Create unknown test file
-      File.open('foo', 'w') { |io| }
+      File.open('foo', 'w') { |io| io.write('hello') }
       assert_equal(
         'huh',
         autocompiler.instance_eval { mime_type_of('foo', 'huh') }
@@ -211,20 +279,7 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
     end
   end
 
-  def test_serve_400
-    # Create autocompiler
-    autocompiler = Nanoc3::Extra::AutoCompiler.new(nil)
-
-    # Fill response for 404
-    response = autocompiler.instance_eval { serve_404('/foo/bar/baz/') }
-
-    # Check response
-    assert_equal(404,                   response[0])
-    assert_equal('text/html',           response[1]['Content-Type'])
-    assert_match(/404 File Not Found/,  response[2][0])
-  end
-
-  def test_serve_rep_with_working_item
+  def test_serve_with_working_item
     if_have('mime/types') do
       # Create item and item rep
       item = mock
@@ -234,7 +289,7 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
       item_rep.expects(:content_at_snapshot).with(:post).returns('compiled item content')
 
       # Create file
-      File.open(item_rep.raw_path, 'w') { |io| }
+      File.open(item_rep.raw_path, 'w') { |io| io.write('hello') }
 
       # Create compiler
       compiler = Object.new
@@ -251,7 +306,7 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
 
       begin
         # Serve
-        response = autocompiler.instance_eval { serve_rep(item_rep) }
+        response = autocompiler.instance_eval { serve(item_rep) }
 
         # Check response
         assert_equal(200,                     response[0])
@@ -264,7 +319,7 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
     end
   end
 
-  def test_serve_rep_with_broken_item
+  def test_serve_with_broken_item
     if_have('mime/types') do
       # Create item and item rep
       item = mock
@@ -283,35 +338,8 @@ class Nanoc3::Extra::AutoCompilerTest < MiniTest::Unit::TestCase
 
       # Serve
       assert_raises(RuntimeError) do
-        autocompiler.instance_eval { serve_rep(item_rep) }
+        autocompiler.instance_eval { serve(item_rep) }
       end
-    end
-  end
-
-  def test_serve_file
-    if_have('mime/types') do
-      # Create test files
-      File.open('test.css', 'w') { |io| io.write("body { color: blue; }")  }
-      File.open('test',     'w') { |io| io.write("random blah blah stuff") }
-
-      # Create autocompiler
-      autocompiler = Nanoc3::Extra::AutoCompiler.new(self)
-
-      # Serve file 1
-      response = autocompiler.instance_eval { serve_file('test.css') }
-
-      # Check response for file 1
-      assert_equal(200,         response[0])
-      assert_equal('text/css',  response[1]['Content-Type'])
-      assert(response[2][0].include?('body { color: blue; }'))
-
-      # Serve file 2
-      response = autocompiler.instance_eval { serve_file('test') }
-
-      # Check response for file 2
-      assert_equal(200,                         response[0])
-      assert_equal('application/octet-stream',  response[1]['Content-Type'])
-      assert(response[2][0].include?('random blah blah stuff'))
     end
   end
 
