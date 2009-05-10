84b9e5508e9e8cd68aa5e0e4e4bd403029eeae74
Denis Defreyne <denis.defreyne@stoneship.org>
2009-05-10 12:37:43 +0200

    renamed Nanoc3::Errors::GenericError to Nanoc3::Errors::Generic for consistency

--- lib/nanoc3/base/errors.rb
+++ lib/nanoc3/base/errors.rb
@@ -3,12 +3,12 @@ module Nanoc3
   module Errors
 
     # Generic error. Superclass for all nanoc-specific errors.
-    class GenericError < ::StandardError
+    class Generic < ::StandardError
     end
 
     # Error that is raised when a site is loaded that uses a data source with
     # an unknown identifier.
-    class UnknownDataSource < GenericError
+    class UnknownDataSource < Generic
       def initialize(data_source_name)
         super("The data source specified in the site's configuration file, #{data_source_name}, does not exist.")
       end
@@ -16,7 +16,7 @@ module Nanoc3
 
     # Error that is raised when a site is loaded that uses a data source with
     # an unknown identifier.
-    class UnknownRouter < GenericError
+    class UnknownRouter < Generic
       def initialize(router_name)
         super("The router specified in the site's configuration file, #{router_name}, does not exist.")
       end
@@ -24,7 +24,7 @@ module Nanoc3
 
     # Error that is raised during site compilation when an item uses a layout
     # that is not present in the site.
-    class UnknownLayout < GenericError
+    class UnknownLayout < Generic
       def initialize(layout_identifier)
         super("The site does not have a layout with identifier '#{layout_identifier}'.")
       end
@@ -32,7 +32,7 @@ module Nanoc3
 
     # Error that is raised during site compilation when an item uses a filter
     # that is not known.
-    class UnknownFilter < GenericError
+    class UnknownFilter < Generic
       def initialize(filter_name)
         super("The requested filter, #{filter_name}, does not exist.")
       end
@@ -41,7 +41,7 @@ module Nanoc3
     # Error that is raised during site compilation when a layout is compiled
     # for which the filter cannot be determined. This is similar to the
     # UnknownFilterError, but specific for filters for layouts.
-    class CannotDetermineFilter < GenericError
+    class CannotDetermineFilter < Generic
       def initialize(layout_identifier)
         super("The filter to be used for the '#{layout_identifier}' could not be determined. Make sure the layout does have a filter.")
       end
@@ -49,7 +49,7 @@ module Nanoc3
 
     # Error that is raised when data is requested when the data is not yet
     # available (possibly due to a missing Nanoc::Site#load_data).
-    class DataNotYetAvailable < GenericError
+    class DataNotYetAvailable < Generic
       def initialize(type, plural)
         super("#{type} #{plural ? 'are' : 'is'} not available yet. You may be missing a Nanoc::Site#load_data call.")
       end
@@ -57,7 +57,7 @@ module Nanoc3
 
     # Error that is raised during site compilation when an item (directly or
     # indirectly) includes its own item content, leading to endless recursion.
-    class RecursiveCompilation < GenericError
+    class RecursiveCompilation < Generic
       def initialize
         super("A recursive call to item content was detected. Items cannot (directly or indirectly) contain their own content.")
       end
@@ -65,7 +65,7 @@ module Nanoc3
 
     # Error that is raised when no rules file can be found in the current
     # working directory.
-    class NoRulesFileFound < GenericError
+    class NoRulesFileFound < Generic
       def initialize
         super("This site does not have a rules file, which is required for nanoc sites.")
       end
@@ -73,14 +73,14 @@ module Nanoc3
 
     # Error that is raised when no compilation rule that can be applied to the
     # current item can be found.
-    class NoMatchingCompilationRuleFound < GenericError
+    class NoMatchingCompilationRuleFound < Generic
       def initialize(item)
         super("No compilation rules were found for the '#{item.identifier}' item.")
       end
     end
 
     # Error that is raised when an rep cannot be compiled because it depends on other representations.
-    class UnmetDependency < GenericError
+    class UnmetDependency < Generic
       attr_reader :rep
       def initialize(rep)
         @rep = rep
