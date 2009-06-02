dc05b995e0e640ce51d222901d74a009d5d33ab3
Denis Defreyne <denis.defreyne@stoneship.org>
2009-06-02 20:06:09 +0200

    removed unused mock classes from site test

--- test/base/test_site.rb
+++ test/base/test_site.rb
@@ -6,68 +6,6 @@ class Nanoc3::SiteTest < MiniTest::Unit::TestCase
 
   include Nanoc3::TestHelpers
 
-  class TestDataSource < Nanoc3::DataSource
-
-    identifier :test_data_source
-
-    def code ; Nanoc3::Code.new('') ; end
-
-  end
-
-  class TestNewschoolDataSource < Nanoc3::DataSource
-
-    identifier :test_newschool_data_source
-
-    def items
-      [
-        Nanoc3::Item.new("Hi!",          {}, '/'),
-        Nanoc3::Item.new("Hello there.", {}, '/about/')
-      ]
-    end
-
-    def layouts
-      [
-        Nanoc3::Layout.new(
-          'HEADER <%= yield %> FOOTER',
-          { :filter => 'erb' },
-          '/quux/'
-        )
-      ]
-    end
-
-    def code
-      Nanoc3::Code.new("def something_random ; 'something random, yah' ; end")
-    end
-
-  end
-
-  class TestEarlyLoadingCodeDataSource < Nanoc3::DataSource
-
-    identifier :early_loading_code_data_source
-
-    def items
-      [
-        Nanoc3::Item.new("Hi!",          {}, '/'),
-        Nanoc3::Item.new("Hello there.", {}, '/about/')
-      ]
-    end
-
-    def layouts
-      [
-        Nanoc3::Layout.new(
-          'HEADER <%= yield %> FOOTER',
-          { :filter => 'erb' },
-          '/quux/'
-        )
-      ]
-    end
-
-    def code
-      Nanoc3::Code.new('')
-    end
-
-  end
-
   def test_load_rules_with_existing_rules_file
     # Mock DSL
     dsl = mock
