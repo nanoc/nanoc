68eaf4a559c4e4638ecdc2d9f6b1b444deff092c
Denis Defreyne <denis.defreyne@stoneship.org>
2009-06-16 13:24:14 +0200

    moved shared filesystem and filesystem_combined code into a new filesystem_common module

---
+++ lib/nanoc3/data_sources/filesystem_common.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+module Nanoc3::DataSources
+
+  # The Nanoc3::DataSources::FilesystemCommon module provides code- and
+  # rule-loading methods that are used by both the filesystem and the
+  # filesystem_combined data sources.
+  module FilesystemCommon
+
+    def code
+      # Get files
+      filenames = Dir['lib/**/*.rb'].sort
+
+      # Read snippets
+      snippets = filenames.map do |fn|
+        { :filename => fn, :code => File.read(fn) }
+      end
+
+      # Get modification time
+      mtimes = filenames.map { |filename| File.stat(filename).mtime }
+      mtime = mtimes.inject { |memo, mtime| memo > mtime ? mtime : memo }
+
+      # Build code
+      Nanoc3::Code.new(snippets, mtime)
+    end
+
+    def rules
+      # Find rules file
+      rules_filename = [ 'Rules', 'rules', 'Rules.rb', 'rules.rb' ].find { |f| File.file?(f) }
+      raise Nanoc3::Errors::NoRulesFileFound.new if rules_filename.nil?
+
+      # Get contents
+      File.read(rules_filename)
+    end
+
+  end
+
+end
--- test/data_sources/test_filesystem.rb
+++ test/data_sources/test_filesystem.rb
@@ -94,60 +94,6 @@ class Nanoc3::DataSources::FilesystemTest < MiniTest::Unit::TestCase
     assert_equal('erb', layouts[0][:filter])
   end
 
-  def test_code
-    # Create data source
-    data_source = Nanoc3::DataSources::Filesystem.new(nil)
-
-    # Create code
-    FileUtils.mkdir_p('lib')
-    File.open('lib/foo.rb', 'w') do |io|
-      io.write("# This is a bit of code right here...\n")
-    end
-
-    # Load code
-    code = data_source.code
-
-    # Check code
-    assert_equal(
-      [ { :code => "# This is a bit of code right here...\n", :filename => 'lib/foo.rb' } ],
-      code.snippets
-    )
-  end
-
-  def test_rules_with_valid_rules_file_names
-    # Create data source
-    data_source = Nanoc3::DataSources::Filesystem.new(nil)
-
-    [ 'Rules', 'rules', 'Rules.rb', 'rules.rb' ].each do |filename|
-      begin
-        # Create a sample rules file
-        File.open(filename, 'w') { |io| io.write("This is #{filename}.") }
-        
-        # Attempt to read it
-        assert_equal "This is #{filename}.", data_source.rules
-      ensure
-        FileUtils.rm(filename)
-      end
-    end
-  end
-
-  def test_rules_with_invalid_rules_file_names
-    # Create data source
-    data_source = Nanoc3::DataSources::Filesystem.new(nil)
-
-    begin
-      # Create a sample rules file
-      File.open('ZeRules', 'w') { |io| io.write("This is a rules file with an invalid name.") }
-
-      # Attempt to read it
-      assert_raises(Nanoc3::Errors::NoRulesFileFound) do
-        data_source.rules
-      end
-    ensure
-      FileUtils.rm('ZeRules')
-    end
-  end
-
   # Test creating data
 
   def test_create_item_at_root
