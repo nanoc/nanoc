b032b390bbf5349c5a12ea96985e42e2cd4cf630
Denis Defreyne <denis.defreyne@stoneship.org>
2009-06-16 13:09:36 +0200

    implemented several missing Nanoc3::Item tests

--- test/base/test_item.rb
+++ test/base/test_item.rb
@@ -6,12 +6,21 @@ class Nanoc3::ItemTest < MiniTest::Unit::TestCase
 
   include Nanoc3::TestHelpers
 
-  def test_content
-    # TODO implement
+  def test_initialize_with_attributes_with_string_keys
+    item = Nanoc3::Item.new("foo", { 'abc' => 'xyz' }, '/foo/')
+
+    assert_equal nil,   item.attributes['abc']
+    assert_equal 'xyz', item.attributes[:abc]
+  end
+
+  def test_initialize_with_unclean_identifier
+    item = Nanoc3::Item.new("foo", {}, '/foo')
+
+    assert_equal '/foo/', item.identifier
   end
 
   def test_lookup
-    # Create item and rep
+    # Create item
     item = Nanoc3::Item.new(
       "content",
       { :one => 'one in item' },
@@ -23,6 +32,14 @@ class Nanoc3::ItemTest < MiniTest::Unit::TestCase
 
     # Test finding two
     assert_equal(nil, item[:two])
+  end
+
+  def test_set_attribute
+    item = Nanoc3::Item.new("foo", {}, '/foo')
+    assert_equal nil, item[:motto]
+
+    item[:motto] = 'More human than human'
+    assert_equal 'More human than human', item[:motto]
   end
 
 end
