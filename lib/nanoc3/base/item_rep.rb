2e1dd6cd825bd572015e7cb32bec17f7962be8d2
Denis Defreyne <denis.defreyne@stoneship.org>
2009-07-03 15:30:29 +0200

    made ItemRep#outdated? not explode when config has no mtime

--- lib/nanoc3/base/item_rep.rb
+++ lib/nanoc3/base/item_rep.rb
@@ -119,6 +119,7 @@ module Nanoc3
       return true if !@item[:skip_output] && @item.site.code.mtime > compiled_mtime
 
       # Outdated if config outdated
+      return true if @item.site.config.mtime.nil?
       return true if @item.site.config.mtime > compiled_mtime
 
       return false
