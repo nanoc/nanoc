f5a90e5de34c9b67d50fca388d35baefb50f9624
Denis Defreyne <denis.defreyne@stoneship.org>
2009-07-18 11:55:35 +0200

    added filesystem_compact data source

--- lib/nanoc3/data_sources.rb
+++ lib/nanoc3/data_sources.rb
@@ -6,11 +6,13 @@ module Nanoc3::DataSources
   autoload 'Filesystem',         'nanoc3/data_sources/filesystem'
   autoload 'FilesystemCombined', 'nanoc3/data_sources/filesystem_combined'
   autoload 'FilesystemCommon',   'nanoc3/data_sources/filesystem_common'
+  autoload 'FilesystemCompact',  'nanoc3/data_sources/filesystem_compact'
   autoload 'Twitter',            'nanoc3/data_sources/twitter'
 
   Nanoc3::DataSource.register '::Nanoc3::DataSources::Delicious',          :delicious
   Nanoc3::DataSource.register '::Nanoc3::DataSources::Filesystem',         :filesystem
   Nanoc3::DataSource.register '::Nanoc3::DataSources::FilesystemCombined', :filesystem_combined
+  Nanoc3::DataSource.register '::Nanoc3::DataSources::FilesystemCompact',  :filesystem_compact
   Nanoc3::DataSource.register '::Nanoc3::DataSources::Twitter',            :twitter
 
 end
