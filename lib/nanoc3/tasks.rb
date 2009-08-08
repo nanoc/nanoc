ccd1c840d9d45524a46a10ce83616bd94c2dcec7
Denis Defreyne <denis.defreyne@stoneship.org>
2009-08-08 21:20:01 +0200

    made tasks.rb load tasks from subdirectories as well

--- lib/nanoc3/tasks.rb
+++ lib/nanoc3/tasks.rb
@@ -5,5 +5,5 @@ require 'nanoc3'
 module Nanoc3::Tasks
 end
 
-Dir[File.dirname(__FILE__) + '/tasks/*.rb'].each { |f| load f }
-Dir[File.dirname(__FILE__) + '/tasks/*.rake'].each { |f| load f }
+Dir[File.dirname(__FILE__) + '/tasks/**/*.rb'].each   { |f| load f }
+Dir[File.dirname(__FILE__) + '/tasks/**/*.rake'].each { |f| load f }
