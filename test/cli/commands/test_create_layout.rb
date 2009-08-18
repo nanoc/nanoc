5220b492b9b779d48a059f9ead4fec9b169f6e96
Denis Defreyne <denis.defreyne@stoneship.org>
2009-08-18 20:12:40 +0200

    fixed bug which would cause new layouts to output the title the wrong way
    --HG--
    branch : 3.0.x

--- test/cli/commands/test_create_layout.rb
+++ test/cli/commands/test_create_layout.rb
@@ -6,7 +6,23 @@ class Nanoc3::CLI::Commands::CreateLayoutTest < MiniTest::Unit::TestCase
 
   include Nanoc3::TestHelpers
 
-  def test_stub
+  def test_can_compile_new_layout
+    Nanoc3::CLI::Base.new.run([ 'create_site', 'foo' ])
+
+    FileUtils.cd('foo') do
+      # Create new layout
+      Nanoc3::CLI::Base.new.run([ 'create_layout', 'moo' ])
+
+      # Makes rules use new layout
+      rules_raw = File.read('Rules')
+      File.open('Rules', 'w') do |io|
+        io.write rules_raw.sub("layout 'default'", "layout 'moo'")
+      end
+
+      site = Nanoc3::Site.new('.')
+      site.load_data
+      site.compiler.run
+    end
   end
 
 end
