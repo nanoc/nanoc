b16b6e4775a8e50ce01b946805c657ba774cc944
Denis Defreyne <denis.defreyne@stoneship.org>
2009-12-22 21:57:37 +0100

    added options to rainpress filter

--- test/filters/test_rainpress.rb
+++ test/filters/test_rainpress.rb
@@ -17,4 +17,15 @@ class Nanoc3::Filters::RainpressTest < MiniTest::Unit::TestCase
     end
   end
 
+  def test_filter_with_options
+    if_have 'rainpress' do
+      # Create filter
+      filter = ::Nanoc3::Filters::Rainpress.new
+
+      # Run filter
+      result = filter.run("body { color: #aabbcc; }", :colors => false)
+      assert_equal("body{color:#aabbcc}", result)
+    end
+  end
+
 end
