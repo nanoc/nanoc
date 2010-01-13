6851c0ce31409629272231325fb5db03e5727271
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-13 17:08:43 +0100

    added some helpful tips to the default Rules file

--- lib/nanoc3/cli/commands/create_site.rb
+++ lib/nanoc3/cli/commands/create_site.rb
@@ -7,6 +7,16 @@ module Nanoc3::CLI::Commands
     DEFAULT_RULES = <<EOS
 #!/usr/bin/env ruby
 
+# A few helpful tips about the Rules file:
+#
+# * The order of rules is important: for each item, only the first matching
+#   rule is applied.
+#
+# * Item identifiers start and end with a slash (e.g. “/about/” for the file
+#   “content/about.html”). To select all children, grandchildren, … of an
+#   item, use the pattern “/about/*/”; “/about/*” will also select the parent,
+#   because “*” matches zero or more characters.
+
 compile '/stylesheet/' do
   # don’t filter or layout
 end
