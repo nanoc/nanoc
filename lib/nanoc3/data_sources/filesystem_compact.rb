620221022a9d6a1e4e57c5ff9308f74ec4c94cd1
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-15 16:13:16 +0100

    cleaned up filesystem compact

--- lib/nanoc3/data_sources/filesystem_compact.rb
+++ lib/nanoc3/data_sources/filesystem_compact.rb
@@ -2,12 +2,6 @@
 
 module Nanoc3::DataSources
 
-  # The filesystem_combined data source is the default data source for a new
-  # nanoc site. It stores all data as files on the hard disk.
-  #
-  # None of the methods are documented in this file. See Nanoc3::DataSource
-  # for documentation on the overridden methods instead.
-  #
   # = Items
   #
   # Items are stored as pairs of two files: a content file, containing the
@@ -18,16 +12,26 @@ module Nanoc3::DataSources
   #
   # Items are stored in the "content" directory of the nanoc site.
   #
-  # The home page item, located at /, is represented by an index.yaml meta
-  # file, along with its corresponding content file.
+  # The identifier of a item is determined as follows. A file with an
+  # 'index.*' content filename, such as 'index.txt', will have the filesystem
+  # path with the 'index.*' part stripped as a identifier. For example:
   #
-  # Subitems of other pages can be achieved in two ways: they can either be
-  # nested in directories and named "index" such as the home page item, or
-  # they can simply be given a non-"index" name.
+  #    foo/bar/index.html → /foo/bar/
   #
-  # The identifier is calculated by stripping the extension; if there is more
-  # than one extension, only the last extension is stripped and the previous
-  # extensions will be part of the identifier.
+  # In other cases, the identifier is calculated by stripping the extension.
+  # If the `allow_periods_in_identifiers` attribute in the configuration is
+  # true, only the last extension will be stripped if the file has multiple
+  # extensions; if it is false or unset, all extensions will be stripped.
+  # For example:
+  #
+  #   (allow_periods_in_identifiers set to true)
+  #   foo.entry.html → /foo.entry/
+  #   
+  #   (allow_periods_in_identifiers set to false)
+  #   foo.html.erb → /foo/
+  #
+  # Note that it is possible for two different, separate files to have the
+  # same identifier. It is recommended to avoid such situations.
   #
   # For example, this directory structure:
   #
@@ -60,7 +64,7 @@ module Nanoc3::DataSources
   #   /journal/2005/
   #   /journal/2005/a-very-old-post/
   #   /journal/2005/another-very-old-post/
-  #   /journal/2005/foo.entry/
+  #   /journal/2005/foo.entry/ OR /journal/2005/foo/
   #   /myst/
   #
   # = Layouts
@@ -70,80 +74,80 @@ module Nanoc3::DataSources
   #
   # The identifier for layouts is generated the same way as identifiers for
   # items (see above for details).
-  #
-  # = Code Snippets
-  #
-  # Code snippets are stored in '.rb' files in the 'lib' directory. Code
-  # snippets can reside in sub-directories.
-  class FilesystemCompact < Nanoc3::DataSource
-
-    ########## VCSes ##########
-
-    attr_accessor :vcs
-
-    def vcs
-      @vcs ||= Nanoc3::Extra::VCSes::Dummy.new
-    end
-
-    ########## Preparation ##########
-
-    def setup
-      # Create directories
-      %w( content layouts lib ).each do |dir|
-        FileUtils.mkdir_p(dir)
-        vcs.add(dir)
-      end
-    end
-
-    ########## Loading data ##########
-
-    def items
-      create_from_files_in('content', Nanoc3::Item)
-    end
-
-    def layouts
-      create_from_files_in('layouts', Nanoc3::Layout)
-    end
-
-    ########## Creating data ##########
-
-    # Creates a new item with the given content, attributes and identifier.
-    def create_item(content, attributes, identifier)
-      # Get filenames
-      base_path = 'content' + (identifier == '/' ? '/index' : identifier[0..-2])
-      meta_filename    = base_path + '.yaml'
-      content_filename = base_path + '.html'
-
-      # Notify
-      Nanoc3::NotificationCenter.post(:file_created, meta_filename)
-      Nanoc3::NotificationCenter.post(:file_created, content_filename)
-
-      # Create files
-      FileUtils.mkdir_p(File.dirname(meta_filename))
-      File.open(meta_filename,    'w') { |io| io.write(YAML.dump(attributes.stringify_keys)) }
-      File.open(content_filename, 'w') { |io| io.write(content) }
-    end
-
-    # Creates a new layout with the given content, attributes and identifier.
-    def create_layout(content, attributes, identifier)
-      # Get filenames
-      base_path = 'layouts' + identifier[0..-2]
-      meta_filename    = base_path + '.yaml'
-      content_filename = base_path + '.html'
-
-      # Notify
-      Nanoc3::NotificationCenter.post(:file_created, meta_filename)
-      Nanoc3::NotificationCenter.post(:file_created, content_filename)
-
-      # Create files
-      FileUtils.mkdir_p(File.dirname(meta_filename))
-      File.open(meta_filename,    'w') { |io| io.write(YAML.dump(attributes.stringify_keys)) }
-      File.open(content_filename, 'w') { |io| io.write(content) }
-    end
+  class FilesystemCompact < Nanoc3::DataSources::Filesystem
 
   private
 
-    ########## Custom functions ##########
+    # See superclass for documentation.
+    def create_object(dir_name, content, attributes, identifier)
+      # Check for periods
+      if (@config.nil? || !@config[:allow_periods_in_identifiers]) && identifier.include?('.')
+        raise RuntimeError,
+          "Attempted to create an object in #{dir_name} with identifier #{identifier} containing a period, but allow_periods_in_identifiers is not enabled in the site configuration. (Enabling allow_periods_in_identifiers may cause the site to break, though.)"
+      end
+
+      # Get filenames
+      base_path = dir_name + (identifier == '/' ? '/index' : identifier[0..-2])
+      meta_filename    = base_path + '.yaml'
+      content_filename = base_path + '.html'
+
+      # Notify
+      Nanoc3::NotificationCenter.post(:file_created, meta_filename)
+      Nanoc3::NotificationCenter.post(:file_created, content_filename)
+
+      # Create files
+      FileUtils.mkdir_p(File.dirname(meta_filename))
+      File.open(meta_filename,    'w') { |io| io.write(YAML.dump(attributes.stringify_keys)) }
+      File.open(content_filename, 'w') { |io| io.write(content) }
+    end
+
+    # See superclass for documentation.
+    def load_objects(dir_name, kind, klass)
+      all_files_in(dir_name).map do |base_filename, (meta_ext, content_ext)|
+        # Get filenames
+        meta_filename    = meta_ext    ? base_filename + '.' + meta_ext    : nil
+        content_filename = content_ext ? base_filename + '.' + content_ext : nil
+
+        # Get meta and content
+        meta    = (meta_filename    ? YAML.load_file(meta_filename) : nil) || {}
+        content = (content_filename ? File.read(content_filename)   : nil) || ''
+
+        # Get attributes
+        attributes = {
+          :content_filename => content_filename,
+          :meta_filename    => meta_filename,
+          :extension        => content_filename ? ext_of(content_filename)[1..-1] : nil,
+          # WARNING :file is deprecated; please create a File object manually
+          # using the :content_filename or :meta_filename attributes.
+          :file             => content_filename ? Nanoc3::Extra::FileProxy.new(content_filename) : nil
+        }.merge(meta)
+
+        # Get identifier
+        if meta_filename
+          identifier = identifier_for_filename(meta_filename[(dir_name.length+1)..-1])
+        elsif content_filename
+          identifier = identifier_for_filename(content_filename[(dir_name.length+1)..-1])
+        else
+          raise RuntimeError, "meta_filename and content_filename are both nil"
+        end
+
+        # Get modification times
+        meta_mtime    = meta_filename    ? File.stat(meta_filename).mtime    : nil
+        content_mtime = content_filename ? File.stat(content_filename).mtime : nil
+        if meta_mtime && content_mtime
+          mtime = meta_mtime > content_mtime ? meta_mtime : content_mtime
+        elsif meta_mtime
+          mtime = meta_mtime
+        elsif content_mtime
+          mtime = content_mtime
+        else
+          raise RuntimeError, "meta_mtime and content_mtime are both nil"
+        end
+
+        # Create layout object
+        klass.new(content, attributes, identifier, mtime)
+      end
+    end
 
     # Finds all items/layouts/... in the given base directory. Returns a hash
     # in which the keys are the file's dirname + basenames, and the values a
@@ -156,12 +160,12 @@ module Nanoc3::DataSources
     #     'content/bar' => [ 'yaml', nil    ],
     #     'content/qux' => [ nil,    'html' ]
     #   }
-    def all_files_in(base)
+    def all_files_in(dir_name)
       # Get all good file names
-      filenames = Dir[base + '/**/*'].select { |i| File.file?(i) }
+      filenames = Dir[dir_name + '/**/*'].select { |i| File.file?(i) }
       filenames.reject! { |fn| fn =~ /(~|\.orig|\.rej|\.bak)$/ }
 
-      # Group by dirname+basename
+      # Group by identifier
       grouped_filenames = filenames.group_by { |fn| basename_of(fn) }
 
       # Convert values into metafile/content file extension tuple
@@ -187,58 +191,6 @@ module Nanoc3::DataSources
       grouped_filenames
     end
 
-    # Creates Item or Layout objects based on all files in the given directory.
-    #
-    # +base+:: The base directory where to search for files.
-    #
-    # +klass+:: The class (Nanoc3::Item or Nanoc3::Layout) of the objects to generate.
-    def create_from_files_in(base, klass)
-      all_files_in(base).map do |base_filename, (meta_ext, content_ext)|
-        # Get filenames
-        meta_filename    = meta_ext    ? base_filename + '.' + meta_ext    : nil
-        content_filename = content_ext ? base_filename + '.' + content_ext : nil
-
-        # Get meta and content
-        meta    = (meta_filename    ? YAML.load_file(meta_filename) : nil) || {}
-        content = (content_filename ? File.read(content_filename)   : nil) || ''
-
-        # Get attributes
-        attributes = {
-          :content_filename => content_filename,
-          :meta_filename    => meta_filename,
-          :extension        => content_filename ? File.extname(content_filename)[1..-1] : nil,
-          # WARNING :file is deprecated; please create a File object manually
-          # using the :content_filename or :meta_filename attributes.
-          :file             => content_filename ? Nanoc3::Extra::FileProxy.new(content_filename) : nil
-        }.merge(meta)
-
-        # Get identifier
-        if meta_filename
-          identifier = identifier_for_filename(meta_filename[(base.length+1)..-1])
-        elsif content_filename
-          identifier = identifier_for_filename(content_filename[(base.length+1)..-1])
-        else
-          raise RuntimeError, "meta_filename and content_filename are both nil"
-        end
-
-        # Get modification times
-        meta_mtime    = meta_filename    ? File.stat(meta_filename).mtime    : nil
-        content_mtime = content_filename ? File.stat(content_filename).mtime : nil
-        if meta_mtime && content_mtime
-          mtime = meta_mtime > content_mtime ? meta_mtime : content_mtime
-        elsif meta_mtime
-          mtime = meta_mtime
-        elsif content_mtime
-          mtime = content_mtime
-        else
-          raise RuntimeError, "meta_mtime and content_mtime are both nil"
-        end
-
-        # Create layout object
-        klass.new(content, attributes, identifier, mtime)
-      end
-    end
-
     # Returns the identifier for the given filename. This method assumes that
     # the base is already stripped.
     #
@@ -250,9 +202,11 @@ module Nanoc3::DataSources
     #   /foo/foo.yaml       -> /foo/foo/
     #   /foo/bar.html       -> /foo/bar/
     #   /foo/bar.entry.yaml -> /foo/bar.entry/
-    def identifier_for_filename(meta_filename)
+    def identifier_for_filename(filename)
+      return basename_of(filename).sub(/index$/, '').cleaned_identifier
+
       # Split into components
-      components = meta_filename.gsub(%r{(^/|/$)}, '').split('/')
+      components = filename.gsub(%r{(^/|/$)}, '').split('/')
       components[-1].sub!(/\.[a-z0-9]+$/, '')
 
       if components[-1] == 'index'
@@ -262,16 +216,29 @@ module Nanoc3::DataSources
       end
     end
 
-    # Returns the base name of filename, i.e. filename with all extensions
-    # stripped off. Supports multiple extensions.
+    # Returns the base name of filename, i.e. filename with the first or all
+    # extensions stripped off. By default, all extensions are stripped off,
+    # but when allow_periods_in_identifiers is set to true in the site
+    # configuration, only the last extension will be stripped .
     def basename_of(filename)
-      filename.sub(/\.[a-z0-9]+$/, '')
+      filename.sub(extension_regex, '')
     end
 
     # Returns the extension(s) of filename. Supports multiple extensions.
     # Includes the leading period.
     def ext_of(filename)
-      filename =~ /(\.[a-z0-9]+)$/ ? $1 : ''
+      filename =~ extension_regex ? $1 : ''
+    end
+
+    # Returns a regex that is used for determining the extension of a file
+    # name. The first match group will be the entire extension, including the
+    # leading period.
+    def extension_regex
+      if @config && @config[:allow_periods_in_identifiers]
+        /(\.[^\/\.]+$)/
+      else
+        /(\.[^\/]+$)/
+      end
     end
 
   end
