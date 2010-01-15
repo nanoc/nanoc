a33cf496c61159af13a2dc72807b2bcec7f8170c
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-15 14:52:43 +0100

    moved common filesystem tests into a new filesystem test suite

---
+++ test/data_sources/test_filesystem.rb
@@ -0,0 +1,73 @@
+# encoding: utf-8
+
+require 'test/helper'
+
+class Nanoc3::DataSources::FilesystemTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  def test_setup
+    # Create data source
+    data_source = Nanoc3::DataSources::FilesystemCombined.new(nil, nil, nil, nil)
+
+    # Remove files to make sure they are recreated
+    FileUtils.rm_rf('content')
+    FileUtils.rm_rf('layouts/default')
+    FileUtils.rm_rf('lib/default.rb')
+
+    # Mock VCS
+    vcs = mock
+    vcs.expects(:add).times(3) # One time for each directory
+    data_source.vcs = vcs
+
+    # Recreate files
+    data_source.setup
+
+    # Ensure essential files have been recreated
+    assert(File.directory?('content/'))
+    assert(File.directory?('layouts/'))
+    assert(File.directory?('lib/'))
+
+    # Ensure no non-essential files have been recreated
+    assert(!File.file?('content/index.html'))
+    assert(!File.file?('layouts/default.html'))
+    assert(!File.file?('lib/default.rb'))
+  end
+
+  def test_items
+    # Create data source
+    data_source = Nanoc3::DataSources::FilesystemCombined.new(nil, nil, nil, nil)
+
+    # Check
+    data_source.expects(:load_objects).with('content', 'item', Nanoc3::Item)
+    data_source.items
+  end
+
+  def test_layouts
+    # Create data source
+    data_source = Nanoc3::DataSources::FilesystemCombined.new(nil, nil, nil, nil)
+
+    # Check
+    data_source.expects(:load_objects).with('layouts', 'layout', Nanoc3::Layout)
+    data_source.layouts
+  end
+
+  def test_create_item
+    # Create data source
+    data_source = Nanoc3::DataSources::FilesystemCombined.new(nil, nil, nil, nil)
+
+    # Check
+    data_source.expects(:create_object).with('content', 'the content', 'the attributes', 'the identifier')
+    data_source.create_item('the content', 'the attributes', 'the identifier')
+  end
+
+  def test_create_layout
+    # Create data source
+    data_source = Nanoc3::DataSources::FilesystemCombined.new(nil, nil, nil, nil)
+
+    # Check
+    data_source.expects(:create_object).with('layouts', 'the content', 'the attributes', 'the identifier')
+    data_source.create_layout('the content', 'the attributes', 'the identifier')
+  end
+
+end
