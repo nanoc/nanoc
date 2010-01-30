0efacbf850b7246dd893acdb7090f53eb27265ab
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-30 12:27:01 +0100

    made assigns accessible without @

--- lib/nanoc3/extra/context.rb
+++ lib/nanoc3/extra/context.rb
@@ -7,11 +7,17 @@ module Nanoc3::Extra
   class Context
 
     # Creates a new context based off the contents of the hash. Each pair in
-    # the hash will be converted to an instance variable. For example, passing
-    # the hash { :foo => 'bar' } will cause @foo to have the value "bar".
+    # the hash will be converted to an instance variable and an instance
+    # method. For example, passing the hash { :foo => 'bar' } will cause @foo
+    # to have the value "bar", and the instance method #foo to return "bar".
     def initialize(hash)
       hash.each_pair do |key, value|
+        # Build instance variable
         instance_variable_set('@' + key.to_s, value)
+
+        # Define method
+        metaclass = (class << self ; self ; end)
+        metaclass.send(:define_method, key) { value }
       end
     end
 
