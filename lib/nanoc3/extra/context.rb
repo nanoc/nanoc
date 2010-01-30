3a662b90f2b6bd1cd7599338cc13a1fea711c7c3
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-30 12:43:54 +0100

    improved Context documentation

--- lib/nanoc3/extra/context.rb
+++ lib/nanoc3/extra/context.rb
@@ -2,14 +2,29 @@
 
 module Nanoc3::Extra
 
-  # Nanoc3::Extra::Context provides a context and a Binding for use in various
-  # filters, such as the ERB and Haml one.
+  # Provides a context and a binding for use in filters such as the ERB and
+  # Haml ones.
   class Context
 
-    # Creates a new context based off the contents of the hash. Each pair in
-    # the hash will be converted to an instance variable and an instance
-    # method. For example, passing the hash { :foo => 'bar' } will cause @foo
-    # to have the value "bar", and the instance method #foo to return "bar".
+    # Creates a new context based off the contents of the hash.
+    #
+    # Each pair in the hash will be converted to an instance variable and an
+    # instance method. For example, passing the hash `{ :foo => 'bar' }` will
+    # cause `@foo` to have the value `"bar"`, and the instance method `#foo`
+    # to return the same value `"bar"`.
+    #
+    # @param [Hash] hash A list of key-value pairs to make available
+    #
+    # @example Defining a context and accessing values
+    #
+    #     context = Nanoc3::Extra::Context.new(
+    #       :name     => 'Max Payne',
+    #       :location => 'in a cheap motel'
+    #     )
+    #     context.instance_eval do
+    #       "I am #{name} and I am hiding #{@location}."
+    #     end
+    #     # => "I am Max Payne and I am hiding in a cheap motel."
     def initialize(hash)
       hash.each_pair do |key, value|
         # Build instance variable
@@ -21,7 +36,9 @@ module Nanoc3::Extra
       end
     end
 
-    # Returns a binding for this context.
+    # Returns a binding for this instance.
+    #
+    # @return [Binding] A binding for this instance
     def get_binding
       binding
     end
