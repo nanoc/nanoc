4441872ecd9575b6c0bd34a0ba965729e5c5a844
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-31 19:09:17 +0100

    cleaned up Rule documentation

--- lib/nanoc3/base/rule.rb
+++ lib/nanoc3/base/rule.rb
@@ -2,14 +2,16 @@
 
 module Nanoc3
 
-  # Nanoc3::Rule contains the processing information for a item.
+  # Contains the processing information for a item.
   class Rule
 
-    # The regex that determines which items this rule can be applied to. This
-    # rule can be applied to items with a identifier matching this regex.
+    # @return [Regexp] The regex that determines which items this rule can be
+    #   applied to. This rule can be applied to items with a identifier
+    #   matching this regex.
     attr_reader :identifier_regex
 
-    # The name of the representation that will be compiled using this rule.
+    # @return [Symbol] The name of the representation that will be compiled
+    #   using this rule
     attr_reader :rep_name
 
     # Creates a new item compilation rule with the given identifier regex,
@@ -17,13 +19,13 @@ module Nanoc3
     # item rep as its argument.
     #
     # @param [Regexp] identifier_regex A regular expression that will be used
-    #   to determine whether this rule is applicable to certain items. 
+    #   to determine whether this rule is applicable to certain items.
     #
     # @param [String, Symbol] rep_name The name of the item representation
-    #   where this rule can be applied to.
+    #   where this rule can be applied to
     #
     # @param [Proc] block A block that will be called when matching items are
-    #   compiled.
+    #   compiled
     def initialize(identifier_regex, rep_name, block)
       @identifier_regex = identifier_regex
       @rep_name         = rep_name.to_sym
@@ -31,18 +33,20 @@ module Nanoc3
       @block = block
     end
 
-    # @param [Nanoc3::Item] item The item to check.
+    # @param [Nanoc3::Item] item The item to check
     #
     # @return [Boolean] true if this rule can be applied to the given item
-    #   rep, false otherwise.
+    #   rep, false otherwise
     def applicable_to?(item)
       item.identifier =~ @identifier_regex
     end
 
     # Applies this rule to the given item rep.
     #
-    # @param [Nanoc3::ItemRep] The item representation where this rule should
-    #   be applied ot.
+    # @param [Nanoc3::ItemRep] rep The item representation where this rule
+    #   should be applied to
+    #
+    # @return [void]
     def apply_to(rep)
       Nanoc3::RuleContext.new(rep).instance_eval &@block
     end
