a1f6595208b56b28ef983250a112bfe86c7f29ce
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-31 19:05:01 +0100

    cleaned up RuleContext documentation

--- lib/nanoc3/base/rule_context.rb
+++ lib/nanoc3/base/rule_context.rb
@@ -35,7 +35,7 @@ module Nanoc3
       site.config
     end
 
-    # @return [Array<Nanoc::Item>] The items in the site of the item
+    # @return [Array<Nanoc3::Item>] The items in the site of the item
     #   representation that is currently being processed in this context.
     def items
       site.items
@@ -47,20 +47,43 @@ module Nanoc3
       site.layouts
     end
 
-    # Filters the current representation (calls #filter with the given
-    # arguments on the rep).
+    # Filters the current representation (calls {Nanoc3::ItemRep#filter} with
+    # the given arguments on the rep).
+    #
+    # @see Nanoc3::ItemRep#filter
+    #
+    # @param [Symbol] filter_name The name of the filter to run the item
+    #   representations' content through
+    #
+    # @param [Hash] filter_args The filter arguments that should be passed to
+    #   the filter's #run method
+    #
+    # @return [void]
     def filter(filter_name, filter_args={})
       rep.filter(filter_name, filter_args)
     end
 
-    # Layouts the current representation (calls #layout with the given
-    # arguments on the rep).
+    # Layouts the current representation (calls {Nanoc3::ItemRep#layout} with
+    # the given arguments on the rep).
+    #
+    # @see Nanoc3::ItemRep#layout
+    #
+    # @param [String] layout_identifier The identifier of the layout the ite
+    #   should be laid out with
+    #
+    # @return [void]
     def layout(layout_identifier)
       rep.layout(layout_identifier)
     end
 
-    # Creates a snapshot of the current representation (calls #snapshot with
-    # the given arguments on the rep).
+    # Creates a snapshot of the current compiled item content. Calls
+    # {Nanoc3::ItemRep#snapshot} with the given arguments on the rep.
+    #
+    # @see Nanoc3::ItemRep#snapshot
+    #
+    # @param [Symbol] snapshot_name The name of the snapshot to create
+    #
+    # @return [void]
     def snapshot(snapshot_name)
       rep.snapshot(snapshot_name)
     end
