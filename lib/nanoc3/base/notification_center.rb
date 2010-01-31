ea563e324042725dc13f7481b557d0b0ff883f70
Denis Defreyne <denis.defreyne@stoneship.org>
2010-01-31 18:45:59 +0100

    cleaned up NotificationCenter documentation

--- lib/nanoc3/base/notification_center.rb
+++ lib/nanoc3/base/notification_center.rb
@@ -2,10 +2,9 @@
 
 module Nanoc3
 
-  # Nanoc3::NotificationCenter provides a way to send notifications between
-  # objects. It allows blocks associated with a certain notification name to
-  # be registered; these blocks will be called when the notification with the
-  # given name is posted.
+  # Provides a way to send notifications between objects. It allows blocks
+  # associated with a certain notification name to be registered; these blocks
+  # will be called when the notification with the given name is posted.
   #
   # It is a slightly different implementation of the Observer pattern; the
   # table of subscribers is not stored in the observable object itself, but in
@@ -24,6 +23,10 @@ module Nanoc3
       #   only used to be able to remove the block (using the remove method)
       #   later. Can be nil, but this is not recommended because it prevents
       #   the given notification block from being unregistered.
+      #
+      # @yield [*args] Will be executed with the arguments passed to {#post}
+      #
+      # @return [void]
       def on(name, id=nil, &block)
         initialize_if_necessary(name)
 
@@ -38,6 +41,8 @@ module Nanoc3
       #
       # @param args Arguments that wil be passed to the blocks handling the
       #   notification.
+      #
+      # @return [void]
       def post(name, *args)
         initialize_if_necessary(name)
 
@@ -56,6 +61,8 @@ module Nanoc3
       #
       # @param [String, Symbol] id The identifier of the block that should be
       #   removed.
+      #
+      # @return [void]
       def remove(name, id)
         initialize_if_necessary(name)
 
