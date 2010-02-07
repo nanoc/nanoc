e371079b363102e0027a4d2450a832c5d266b702
Denis Defreyne <denis.defreyne@stoneship.org>
2010-02-07 17:10:43 +0100

    removed delicious, twitter and last.fm data sources

--- lib/nanoc3/data_sources.rb
+++ lib/nanoc3/data_sources.rb
@@ -2,18 +2,12 @@
 
 module Nanoc3::DataSources
 
-  autoload 'Delicious',          'nanoc3/data_sources/delicious'
   autoload 'Filesystem',         'nanoc3/data_sources/filesystem'
   autoload 'FilesystemUnified',  'nanoc3/data_sources/filesystem_unified'
   autoload 'FilesystemVerbose',  'nanoc3/data_sources/filesystem_verbose'
-  autoload 'LastFM',             'nanoc3/data_sources/last_fm'
-  autoload 'Twitter',            'nanoc3/data_sources/twitter'
 
-  Nanoc3::DataSource.register '::Nanoc3::DataSources::Delicious',          :delicious
   Nanoc3::DataSource.register '::Nanoc3::DataSources::FilesystemVerbose',  :filesystem_verbose
   Nanoc3::DataSource.register '::Nanoc3::DataSources::FilesystemUnified',  :filesystem_unified
-  Nanoc3::DataSource.register '::Nanoc3::DataSources::LastFM',             :last_fm
-  Nanoc3::DataSource.register '::Nanoc3::DataSources::Twitter',            :twitter
 
   # Deprecated; use filesystem_verbose instead
   # TODO [in nanoc 4.0] remove me
