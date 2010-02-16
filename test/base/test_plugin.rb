42e6204a644ced95f712e54edf73da22716ed39c
Denis Defreyne <denis.defreyne@stoneship.org>
2010-02-16 10:25:45 +0100

    made #register call inside plugin subclass work

--- test/base/test_plugin.rb
+++ test/base/test_plugin.rb
@@ -6,6 +6,10 @@ class Nanoc3::PluginTest < MiniTest::Unit::TestCase
 
   include Nanoc3::TestHelpers
 
+  class SampleFilter < Nanoc3::Filter
+    identifier :_plugin_test_sample_filter
+  end
+
   def test_named
     # Find existant filter
     filter = Nanoc3::Filter.named(:erb)
@@ -14,6 +18,15 @@ class Nanoc3::PluginTest < MiniTest::Unit::TestCase
     # Find non-existant filter
     filter = Nanoc3::Filter.named(:lksdaffhdlkashlgkskahf)
     assert(filter.nil?)
+  end
+
+  def test_register
+    SampleFilter.send(:identifier, :_plugin_test_sample_filter)
+
+    registry = Nanoc3::PluginRegistry.instance
+    filter = registry.find(Nanoc3::Filter, :_plugin_test_sample_filter)
+
+    refute_nil filter
   end
 
 end
