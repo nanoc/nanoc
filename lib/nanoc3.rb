016cd244ef83ac8206fe98512c9f4aee60f43661
Denis Defreyne <denis.defreyne@stoneship.org>
2010-02-28 18:46:55 +0100

    updated version to 3.1.0a3

--- lib/nanoc3.rb
+++ lib/nanoc3.rb
@@ -3,7 +3,7 @@
 module Nanoc3
 
   # The current nanoc version.
-  VERSION = '3.1.0a2'
+  VERSION = '3.1.0a3'
 
   # Loads all nanoc3 plugins, i.e. requires all ruby gems whose name start
   # with `nanoc3-`.
