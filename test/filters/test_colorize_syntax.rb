f7a2e5e8bd85f3ce654ddceffb26681d3823220d
Denis Defreyne <denis.defreyne@stoneship.org>
2010-03-05 18:41:49 +0100

    added syntax coloring filter test case

---
+++ test/filters/test_colorize_syntax.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+require 'test/helper'
+
+class Nanoc3::Filters::ColorizeSyntaxTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  def test_coderay_simple
+    if_have 'coderay' do
+      # Create filter
+      filter = ::Nanoc3::Filters::ColorizeSyntax.new
+
+      # Get input and expected output
+      input = '<pre title="moo"><code class="language-ruby"># comment</code></pre>'
+      expected_output = '<pre title="moo"><code class="language-ruby"><span class="c"># comment</span></code></pre>'
+
+      # Run filter
+      actual_output = filter.run(input)
+      assert_equal(expected_output, actual_output)
+    end
+  end
+
+  def test_coderay_with_more_classes
+    if_have 'coderay' do
+      # Create filter
+      filter = ::Nanoc3::Filters::ColorizeSyntax.new
+
+      # Get input and expected output
+      input = '<pre title="moo"><code class="abc language-ruby xyz"># comment</code></pre>'
+      expected_output = '<pre title="moo"><code class="abc language-ruby xyz"><span class="c"># comment</span></code></pre>'
+
+      # Run filter
+      actual_output = filter.run(input)
+      assert_equal(expected_output, actual_output)
+    end
+  end
+
+end
