ac57cb3ebffe3100c607d373ca057908ca0a16e3
Denis Defreyne <denis.defreyne@stoneship.org>
2010-03-05 12:53:07 +0100

    removed support for gem plugins

--- lib/nanoc3.rb
+++ lib/nanoc3.rb
@@ -5,26 +5,6 @@ module Nanoc3
   # The current nanoc version.
   VERSION = '3.1.0a3'
 
-  # Loads all nanoc3 plugins, i.e. requires all ruby gems whose name start
-  # with `nanoc3-`.
-  #
-  # @return [Boolean] true if all plugins were loaded successfully, false if
-  # rubygems isn’t loaded.
-  def self.load_plugins
-    # Don’t load if there’s no rubygems
-    return false if !defined?(Gem)
-
-    Gem.source_index.find_name('').each do |gem|
-      # Skip irrelevant ones
-      next if gem.name !~ /^nanoc3-/
-
-      # Load plugin
-      require gem.name
-    end
-
-    true
-  end
-
 end
 
 # Load requirements
@@ -37,6 +17,3 @@ require 'nanoc3/extra'
 require 'nanoc3/data_sources'
 require 'nanoc3/filters'
 require 'nanoc3/helpers'
-
-# Load plugins
-Nanoc3.load_plugins
