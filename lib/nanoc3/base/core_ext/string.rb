540563fa1482a6ac6ad8800a42b2f808a14eb0ba
Denis Defreyne <denis.defreyne@stoneship.org>
2010-03-22 12:20:12 +0100

    made unicode-only chars be decomposed to ascii when necessary

--- lib/nanoc3/base/core_ext/string.rb
+++ lib/nanoc3/base/core_ext/string.rb
@@ -9,6 +9,24 @@ module Nanoc3::StringExtensions
     "/#{self}/".gsub(/^\/+|\/+$/, '/')
   end
 
+  # Replaces Unicode characters with their ASCII decompositions if the
+  # environment does not support Unicode.
+  #
+  # This method is not suited for general usage. If you need similar
+  # functionality, consider using the Iconv library instead.
+  #
+  # @return [String] The decomposed string
+  def make_compatible_with_env
+    # Check whether environment supports Unicode
+    # TODO this is ugly, and there most likely are better ways to do this
+    is_unicode_supported = %w( LC_ALL LC_CTYPE LANG ).any? { |e| ENV[e] =~ /UTF/ }
+    return self if is_unicode_supported
+
+    # Decompose if necessary
+    # TODO this decomposition is not generally usable
+    self.gsub(/“|”/, '"').gsub(/‘|’/, '\'').gsub('…', '...')
+  end
+
 end
 
 class String
