b60c4098a0986656f1117d873a348a852dd83ca3
Denis Defreyne <denis.defreyne@stoneship.org>
2010-04-15 12:46:55 +0200

    added mustache filter (experimental)

---
+++ test/filters/test_mustache.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+require 'test/helper'
+
+class Nanoc3::Filters::MustacheTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  def test_filte
+    if_have 'mustache' do
+      # Create item
+      item = Nanoc3::Item.new(
+        'content',
+        { :title => 'Max Payne', :protagonist => 'Max Payne' },
+        '/games/max-payne/'
+      )
+
+      # Create filter
+      filter = ::Nanoc3::Filters::Mustache.new({ :item => item })
+
+      # Run filter
+      result = filter.run('The protagonist of {{title}} is {{protagonist}}.')
+      assert_equal('The protagonist of Max Payne is Max Payne.', result)
+    end
+  end
+
+end
