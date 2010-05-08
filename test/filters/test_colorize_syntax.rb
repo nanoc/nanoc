30539f0a5e3b94cc87eb4786594a808ee8b5b326
Denis Defreyne <denis.defreyne@stoneship.org>
2010-05-08 12:19:21 +0200

    made pygmentize not output extra divs and pres
    --HG--
    branch : 3.1.x

--- test/filters/test_colorize_syntax.rb
+++ test/filters/test_colorize_syntax.rb
@@ -36,4 +36,21 @@ class Nanoc3::Filters::ColorizeSyntaxTest < MiniTest::Unit::TestCase
     end
   end
 
+  def test_pygmentize
+    if `which pygmentize`.strip.empty?
+      skip "could not find pygmentize"
+    end
+
+    # Create filter
+    filter = ::Nanoc3::Filters::ColorizeSyntax.new
+
+    # Get input and expected output
+    input = '<pre title="moo"><code class="language-ruby"># comment</code></pre>'
+    expected_output = '<pre title="moo"><code class="language-ruby"><span class="c1"># comment</span></code></pre>'
+
+    # Run filter
+    actual_output = filter.run(input, :colorizers => { 'ruby' => :pygmentize })
+    assert_equal(expected_output, actual_output)
+  end
+
 end
