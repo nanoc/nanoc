3422921c21c9bfb787de7b46285a59035c525200
Denis Defreyne <denis.defreyne@stoneship.org>
2010-06-06 10:54:26 +0200

    made less filter allow relative paths

--- lib/nanoc3/filters/less.rb
+++ lib/nanoc3/filters/less.rb
@@ -12,7 +12,13 @@ module Nanoc3::Filters
     def run(content, params={})
       require 'less'
 
+      # Add filename to load path
+      $LESS_LOAD_PATH << File.dirname(@item[:content_filename])
+
       ::Less::Engine.new(content).to_css
+    ensure
+      # Restore load path
+      $LESS_LOAD_PATH.delete_at(-1)
     end
 
   end
--- test/filters/test_less.rb
+++ test/filters/test_less.rb
@@ -8,12 +8,51 @@ class Nanoc3::Filters::LessTest < MiniTest::Unit::TestCase
 
   def test_filter
     if_have 'less' do
+      # Create item
+      @item = Nanoc3::Item.new("blah", { :content_filename => 'content/foo/bar.txt' }, '/foo/bar/')
+
       # Create filter
-      filter = ::Nanoc3::Filters::Less.new
+      filter = ::Nanoc3::Filters::Less.new(:item => @item)
 
       # Run filter
       result = filter.run('.foo { bar: 1 + 1 }')
       assert_match /\.foo\s*\{\s*bar:\s*2;?\s*\}/, result
+    end
+  end
+
+  def test_filter_with_paths_relative_to_site_directory
+    if_have 'less' do
+      # Create file to import
+      FileUtils.mkdir_p('content/foo/bar')
+      File.open('content/foo/bar/imported_file.less', 'w') { |io| io.write('p { color: red; }') }
+
+      # Create item
+      @item = Nanoc3::Item.new("blah", { :content_filename => 'content/foo/bar.txt' }, '/foo/bar/')
+
+      # Create filter
+      filter = ::Nanoc3::Filters::Less.new(:item => @item)
+
+      # Run filter
+      result = filter.run('@import "content/foo/bar/imported_file.less";')
+      assert_match /p\s*\{\s*color:\s*red;?\s*\}/, result
+    end
+  end
+
+  def test_filter_with_paths_relative_to_current_file
+    if_have 'less' do
+      # Create file to import
+      FileUtils.mkdir_p('content/foo/bar')
+      File.open('content/foo/bar/imported_file.less', 'w') { |io| io.write('p { color: red; }') }
+
+      # Create item
+      @item = Nanoc3::Item.new("blah", { :content_filename => 'content/foo/bar.txt' }, '/foo/bar/')
+
+      # Create filter
+      filter = ::Nanoc3::Filters::Less.new(:item => @item)
+
+      # Run filter
+      result = filter.run('@import "bar/imported_file.less";')
+      assert_match /p\s*\{\s*color:\s*red;?\s*\}/, result
     end
   end
 
