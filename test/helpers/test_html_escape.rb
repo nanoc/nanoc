3a86073b6403be06efe47e78516e24953849990a
Denis Defreyne <denis.defreyne@stoneship.org>
2010-06-12 11:54:46 +0200

    allowed html_escape to escape blocks

--- test/helpers/test_html_escape.rb
+++ test/helpers/test_html_escape.rb
@@ -8,11 +8,21 @@ class Nanoc3::Helpers::HTMLEscapeTest < MiniTest::Unit::TestCase
 
   include Nanoc3::Helpers::HTMLEscape
 
-  def test_html_escape
+  def test_html_escape_with_string
     assert_equal('&lt;',    html_escape('<'))
     assert_equal('&gt;',    html_escape('>'))
     assert_equal('&amp;',   html_escape('&'))
     assert_equal('&quot;',  html_escape('"'))
+  end
+
+  def test_html_escape_with_block
+    _erbout = 'moo'
+
+    html_escape do
+      _erbout << '<h1>Looks like a header</h1>'
+    end
+
+    assert_equal 'moo&lt;h1&gt;Looks like a header&lt;/h1&gt;', _erbout
   end
 
 end
