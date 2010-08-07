f3ea4c77724f98b237fbdeca57853cfcdccfc152
Vincent Driessen <vincent@datafox.nl>
2010-08-07 10:29:24 +0200

    Expose RedCloth params to the nanoc filter.
    This way, we can easily influence how RedCloth behaves.  In particular
    it's useful to be able to turn off hard line breaks, like so:
    
        compile '/' do
            filter :redcloth, :hard_breaks => false
            layout 'default'
        end

--- lib/nanoc3/filters/redcloth.rb
+++ lib/nanoc3/filters/redcloth.rb
@@ -3,8 +3,22 @@
 module Nanoc3::Filters
   class RedCloth < Nanoc3::Filter
 
-    # Runs the content through [RedCloth](http://redcloth.org/).
-    # This method takes no options.
+    # Runs the content through [RedCloth](http://redcloth.org/). This method
+    # takes the following options:
+    #
+    # * `:filter_class`
+    # * `:filter_html`
+    # * `:filter_ids`
+    # * `:filter_style`
+    # * `:hard_breaks`
+    # * `:lite_mode`
+    # * `:no_span_caps`
+    # * `:sanitize_htm`
+    #
+    # Each of these options sets the corresponding attribute on the `RedCloth`
+    # instance. For example, when the `:hard_breaks => false` option is passed
+    # to this filter, the filter will call `r.hard_breaks = false` (with `r`
+    # being the `RedCloth` instance).
     #
     # @param [String] content The content to filter
     #
@@ -12,8 +26,21 @@ module Nanoc3::Filters
     def run(content, params={})
       require 'redcloth'
 
+      # Create formatter
+      r = ::RedCloth.new(content)
+
+      # Set options
+      r.filter_classes = params[:filter_classes] if params.has_key?(:filter_classes)
+      r.filter_html    = params[:filter_html]    if params.has_key?(:filter_html)
+      r.filter_ids     = params[:filter_ids]     if params.has_key?(:filter_ids)
+      r.filter_styles  = params[:filter_styles]  if params.has_key?(:filter_styles)
+      r.hard_breaks    = params[:hard_breaks]    if params.has_key?(:hard_breaks)
+      r.lite_mode      = params[:lite_mode]      if params.has_key?(:lite_mode)
+      r.no_span_caps   = params[:no_span_caps]   if params.has_key?(:no_span_caps)
+      r.sanitize_html  = params[:sanitize_html]  if params.has_key?(:sanitize_html)
+
       # Get result
-      ::RedCloth.new(content).to_html
+      r.to_html
     end
 
   end
