d5452f12ea431301d2bb8715f2d9746795b5eb86
Denis Defreyne <denis.defreyne@stoneship.org>
2010-11-21 17:43:03 +0100

    made colorize_syntax output proper xml

--- lib/nanoc3/filters/colorize_syntax.rb
+++ lib/nanoc3/filters/colorize_syntax.rb
@@ -81,7 +81,8 @@ module Nanoc3::Filters
         element.inner_html = highlighted_code.strip
       end
 
-      doc.to_html(:encoding => 'UTF-8')
+      method = "to_#{syntax}".to_sym
+      doc.send(method, :encoding => 'UTF-8')
     end
 
   private
