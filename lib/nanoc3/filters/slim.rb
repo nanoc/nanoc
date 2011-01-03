073dfab647735168880c985508fbd48b36d95cd6
zaiste <oh@zaiste.net>
2011-01-03 13:21:37 +0100

    added slim filter

---
+++ lib/nanoc3/filters/slim.rb
@@ -0,0 +1,19 @@
+class Slim < Nanoc3::Filter
+  identifier :slim
+  type :text
+
+  # Runs the content through [Slim](http://slim-lang.com/)
+  # This method takes no options.
+  #
+  # @param [String] content The content to filter
+  #
+  # @return [String] The filtered content
+  def run(content, params={})
+    require 'slim'
+
+    # Create context
+    context = ::Nanoc3::Context.new(assigns)
+
+    Slim::Template.new({}) { content }.render(context) 
+  end
+end
