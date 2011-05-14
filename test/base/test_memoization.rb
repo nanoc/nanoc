d47e203647392c32c376c0954ddbee8c3abcc770
Denis Defreyne <denis.defreyne@stoneship.org>
2011-05-14 17:21:35 +0200

    added memoization test
    --HG--
    branch : 3.2.x-rule-recorder

---
+++ test/base/test_memoization.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+require 'test/helper'
+
+class Nanoc3::MemoizationTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  class Sample1
+
+    def initialize(value)
+      @value = value
+    end
+
+    def run(n)
+      @value*10 + n
+    end
+
+  end
+
+  class Sample2
+
+    def initialize(value)
+      @value = value
+    end
+
+    def run(n)
+      @value*100 + n
+    end
+
+  end
+
+  def test
+    sample1a = Sample1.new(10)
+    sample1b = Sample1.new(15)
+    sample2a = Sample2.new(20)
+    sample2b = Sample2.new(25)
+
+    3.times do
+      assert_equal (10*10+5),  sample1a.run(5)
+      assert_equal (10*15+7),  sample1b.run(7)
+      assert_equal (100*20+5), sample2a.run(5)
+      assert_equal (100*25+7), sample2b.run(7)
+    end
+  end
+
+end
