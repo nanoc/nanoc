9cacdcb477154395be8b729fdaa7702481bdcd13
Peter Aronoff <telemachus@arpinum.org>
2011-05-20 10:46:44 -0400

    Add support and tests for Redcarpet Markdown
    This is based entirely on the existing Rdiscount filter. But I did add
    two tests to confirm that (1) Redcarpet outputs HTML style tags by default
    and (2) can output strict XHTML style on request.

---
+++ test/filters/test_redcarpet.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+require 'test/helper'
+
+class Nanoc3::Filters::RedcarpetTest < MiniTest::Unit::TestCase
+
+  include Nanoc3::TestHelpers
+
+  def test_filter
+    if_have 'redcarpet' do
+      # Create filter
+      filter = ::Nanoc3::Filters::Redcarpet.new
+
+      # Run filter
+      result = filter.run("> Quote")
+      assert_match(/<blockquote>\s*<p>Quote<\/p>\s*<\/blockquote>/, result)
+    end
+  end
+
+  def test_with_extensions
+    if_have 'redcarpet' do
+      # Create filter
+      filter = ::Nanoc3::Filters::Redcarpet.new
+
+      # Run filter
+      input           = "The quotation 'marks' sure make this look sarcastic!"
+      output_expected = /The quotation &lsquo;marks&rsquo; sure make this look sarcastic!/
+      output_actual   = filter.run(input, :options => [ :smart ])
+      assert_match(output_expected, output_actual)
+    end
+  end
+
+  def test_html_by_default
+    if_have 'redcarpet' do
+      # Create filter
+      filter = ::Nanoc3::Filters::Redcarpet.new
+
+      # Run filter
+      input           = "![Alt](/path/to/img 'Title')"
+      output_expected = %r{<img src="/path/to/img" alt="Alt" title="Title">}
+      output_actual   = filter.run(input)
+      assert_match(output_expected, output_actual)
+    end
+  end
+
+  def test_xhtml_if_requested
+    if_have 'redcarpet' do
+      # Create filter
+      filter = ::Nanoc3::Filters::Redcarpet.new
+
+      # Run filter
+      input           = "![Alt](/path/to/img 'Title')"
+      output_expected = %r{<img src="/path/to/img" alt="Alt" title="Title"/>}
+      output_actual   = filter.run(input, :options => [ :xhtml ])
+      assert_match(output_expected, output_actual)
+    end
+  end
+
+end
