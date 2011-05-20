a97c922e74b0073910842ec5a41f59dc7d0656b8
Denis Defreyne <denis.defreyne@stoneship.org>
2011-05-20 18:47:39 +0200

    added documentation for rdiscount params
    --HG--
    branch : 3.1.x

--- lib/nanoc3/filters/rdiscount.rb
+++ lib/nanoc3/filters/rdiscount.rb
@@ -4,7 +4,8 @@ module Nanoc3::Filters
   class RDiscount < Nanoc3::Filter
 
     # Runs the content through [RDiscount](http://github.com/rtomayko/rdiscount).
-    # This method takes no options.
+    #
+    # @option params [Array] symbol ([]) A list of RDiscount extensions
     #
     # @param [String] content The content to filter
     #
