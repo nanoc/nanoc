1d654c0d3ee43c9eaed4141f217bb1e82d8eccf7
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-05 09:41:08 +0200

    made data sources handle _index suffix in filenames correctly
    --HG--
    branch : 3.1.x

--- lib/nanoc3/data_sources/filesystem_unified.rb
+++ lib/nanoc3/data_sources/filesystem_unified.rb
@@ -103,10 +103,10 @@ module Nanoc3::DataSources
     # Returns the identifier derived from the given filename, first stripping
     # the given directory name off the filename.
     def identifier_for_filename(filename)
-      if filename =~ /index\.[^\/]+$/
-        regex = ((@config && @config[:allow_periods_in_identifiers]) ? /index\.[^\/\.]+$/ : /index\.[^\/]+$/)
+      if filename =~ /(^|\/)index\.[^\/]+$/
+        regex = ((@config && @config[:allow_periods_in_identifiers]) ? /\/?index\.[^\/\.]+$/ : /\/?index\.[^\/]+$/)
       else
-        regex = ((@config && @config[:allow_periods_in_identifiers]) ? /\.[^\/\.]+$/      : /\.[^\/]+$/)
+        regex = ((@config && @config[:allow_periods_in_identifiers]) ? /\.[^\/\.]+$/         : /\.[^\/]+$/)
       end
       filename.sub(regex, '').cleaned_identifier
     end
