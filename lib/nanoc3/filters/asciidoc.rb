fb1c6473fd4f38fb431709c08a983660870af44c
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-05 19:12:48 +0200

    made asciidoc filter use systemu

--- lib/nanoc3/filters/asciidoc.rb
+++ lib/nanoc3/filters/asciidoc.rb
@@ -12,45 +12,25 @@ module Nanoc3::Filters
     #
     # @return [String] The filtered content
     def run(content, params={})
-      require 'escape'
-      require 'tempfile'
+      require 'systemu'
 
-      # Run filter
-      output = ''
-      errors = ''
-      success = true
-      Tempfile.open('nanoc-asciidoc-in') do |cmd_in|
-        cmd_out = Tempfile.open('nanoc-asciidoc-out')
-        cmd_err = Tempfile.open('nanoc-asciidoc-err')
-        cmd_out.close
-        cmd_err.close
-
-        # Write input
-        cmd_in.write(content)
-        cmd_in.close
-
-        # Run
-        # TODO allow customizable options
-        fns = {
-          :in  => Escape.shell_single_word(cmd_in.path),
-          :out => Escape.shell_single_word(cmd_out.path),
-          :err => Escape.shell_single_word(cmd_err.path),
-        }
-        command = "asciidoc -o #{fns[:out]} #{fns[:in]} 2>#{fns[:err]}"
-        system(command)
-        success = $?.success?
-
-        # Done
-        output = File.read(cmd_out.path)
-        errors = File.read(cmd_err.path)
-      end
+      # Run command
+      stdout = ''
+      stderr = ''
+      status = systemu(
+        [ 'asciidoc', '-o', '-', '-' ],
+        'stdin'  => content,
+        'stdout' => stdout,
+        'stderr' => stderr)
 
       # Show errors
-      puts errors
-      raise RuntimeError, errors if !success
+      unless status.success?
+        $stderr.puts stderr
+        raise RuntimeError, "AsciiDoc filter failed with status #{status}"
+      end
 
-      # Done
-      output
+      # Get result
+      stdout
     end
 
   end
