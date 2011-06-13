62b358b8573c24e848e41e4dc289fbb024fabe88
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-13 16:38:16 +0200

    made watch command watch all sorts of rules files

--- lib/nanoc3/cli/commands/watch.rb
+++ lib/nanoc3/cli/commands/watch.rb
@@ -89,7 +89,7 @@ module Nanoc3::CLI::Commands
       # Get directories to watch
       watcher_config = @base.site.config[:watcher] || {}
       dirs_to_watch  = watcher_config[:dirs_to_watch]  || %w( content layouts lib )
-      files_to_watch = watcher_config[:files_to_watch] || %w( config.yaml Rules )
+      files_to_watch = watcher_config[:files_to_watch] || %w( config.yaml Rules rules Rules.rb rules.rb' )
 
       # Watch
       puts "Watching for changes…".make_compatible_with_env
