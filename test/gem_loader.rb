220347023e88832b926706921c1d40f5fcd3d546
Denis Defreyne <denis.defreyne@stoneship.org>
2011-06-24 20:45:29 +0200

    added gem loader
    --HG--
    branch : 3.1.x

---
+++ test/gem_loader.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+begin
+  require 'rubygems'
+
+  gemspec = File.expand_path("nanoc3.gemspec", Dir.pwd)
+  Gem::Specification.load(gemspec).dependencies.each do |dep|
+    gem dep.name, *dep.requirement.as_list
+  end
+rescue LoadError => e
+end
