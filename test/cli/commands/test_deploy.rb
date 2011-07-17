718affd17831a50a299cf167dd8175e22a93ae17
Denis Defreyne <denis.defreyne@stoneship.org>
2011-07-17 10:47:40 +0200

    skipped fog/rsync tests if dependencies are not available

--- test/cli/commands/test_deploy.rb
+++ test/cli/commands/test_deploy.rb
@@ -5,40 +5,44 @@ class Nanoc::CLI::Commands::DeployTest < MiniTest::Unit::TestCase
   include Nanoc::TestHelpers
 
   def test_deploy
-    with_site do |site|
-      File.open('config.yaml', 'w') do |io|
-        io.write "deploy:\n"
-        io.write "  public:\n"
-        io.write "    kind: rsync\n"
-        io.write "    dst: mydestination"
+    if_have 'systemu' do
+      with_site do |site|
+        File.open('config.yaml', 'w') do |io|
+          io.write "deploy:\n"
+          io.write "  public:\n"
+          io.write "    kind: rsync\n"
+          io.write "    dst: mydestination"
+        end
+
+        FileUtils.mkdir_p('output')
+        File.open('output/blah.html', 'w') { |io| io.write 'moo' }
+
+        Nanoc::CLI.run %w( deploy -t public )
+
+        assert File.directory?('mydestination')
+        assert File.file?('mydestination/blah.html')
       end
-
-      FileUtils.mkdir_p('output')
-      File.open('output/blah.html', 'w') { |io| io.write 'moo' }
-
-      Nanoc::CLI.run %w( deploy -t public )
-
-      assert File.directory?('mydestination')
-      assert File.file?('mydestination/blah.html')
     end
   end
 
   def test_deploy_with_dry_run
-    with_site do |site|
-      File.open('config.yaml', 'w') do |io|
-        io.write "deploy:\n"
-        io.write "  public:\n"
-        io.write "    kind: rsync\n"
-        io.write "    dst: mydestination"
+    if_have 'systemu' do
+      with_site do |site|
+        File.open('config.yaml', 'w') do |io|
+          io.write "deploy:\n"
+          io.write "  public:\n"
+          io.write "    kind: rsync\n"
+          io.write "    dst: mydestination"
+        end
+
+        FileUtils.mkdir_p('output')
+        File.open('output/blah.html', 'w') { |io| io.write 'moo' }
+
+        Nanoc::CLI.run %w( deploy -t public -n )
+
+        refute File.directory?('mydestination')
+        refute File.file?('mydestination/blah.html')
       end
-
-      FileUtils.mkdir_p('output')
-      File.open('output/blah.html', 'w') { |io| io.write 'moo' }
-
-      Nanoc::CLI.run %w( deploy -t public -n )
-
-      refute File.directory?('mydestination')
-      refute File.file?('mydestination/blah.html')
     end
   end
 
--- test/extra/deployers/test_rsync.rb
+++ test/extra/deployers/test_rsync.rb
@@ -5,88 +5,96 @@ class Nanoc::Extra::Deployers::RsyncTest < MiniTest::Unit::TestCase
   include Nanoc::TestHelpers
 
   def test_run_without_dst
-    # Create deployer
-    rsync = Nanoc::Extra::Deployers::Rsync.new(
-      'output/',
-      {})
+    if_have 'systemu' do
+      # Create deployer
+      rsync = Nanoc::Extra::Deployers::Rsync.new(
+        'output/',
+        {})
 
-    # Mock run_shell_cmd
-    def rsync.run_shell_cmd(args)
-      @shell_cms_args = args
+      # Mock run_shell_cmd
+      def rsync.run_shell_cmd(args)
+        @shell_cms_args = args
+      end
+
+      # Try running
+      error = assert_raises(RuntimeError) do
+        rsync.run
+      end
+
+      # Check error message
+      assert_equal 'No dst found in deployment configuration', error.message
     end
-
-    # Try running
-    error = assert_raises(RuntimeError) do
-      rsync.run
-    end
-
-    # Check error message
-    assert_equal 'No dst found in deployment configuration', error.message
   end
 
   def test_run_with_erroneous_dst
-    # Create deployer
-    rsync = Nanoc::Extra::Deployers::Rsync.new(
-      'output/',
-      { :dst => 'asdf/' })
+    if_have 'systemu' do
+      # Create deployer
+      rsync = Nanoc::Extra::Deployers::Rsync.new(
+        'output/',
+        { :dst => 'asdf/' })
 
-    # Mock run_shell_cmd
-    def rsync.run_shell_cmd(args)
-      @shell_cms_args = args
+      # Mock run_shell_cmd
+      def rsync.run_shell_cmd(args)
+        @shell_cms_args = args
+      end
+
+      # Try running
+      error = assert_raises(RuntimeError) do
+        rsync.run
+      end
+
+      # Check error message
+      assert_equal 'dst requires no trailing slash', error.message
     end
-
-    # Try running
-    error = assert_raises(RuntimeError) do
-      rsync.run
-    end
-
-    # Check error message
-    assert_equal 'dst requires no trailing slash', error.message
   end
 
   def test_run_everything_okay
-    # Create deployer
-    rsync = Nanoc::Extra::Deployers::Rsync.new(
-      'output/',
-      { :dst => 'asdf' })
+    if_have 'systemu' do
+      # Create deployer
+      rsync = Nanoc::Extra::Deployers::Rsync.new(
+        'output/',
+        { :dst => 'asdf' })
 
-    # Mock run_shell_cmd
-    def rsync.run_shell_cmd(args)
-      @shell_cms_args = args
+      # Mock run_shell_cmd
+      def rsync.run_shell_cmd(args)
+        @shell_cms_args = args
+      end
+
+      # Run
+      rsync.run
+
+      # Check args
+      opts = Nanoc::Extra::Deployers::Rsync::DEFAULT_OPTIONS
+      assert_equal(
+        [ 'rsync', opts, File.expand_path('output') + '/', 'asdf' ].flatten,
+        rsync.instance_eval { @shell_cms_args }
+      )
     end
-
-    # Run
-    rsync.run
-
-    # Check args
-    opts = Nanoc::Extra::Deployers::Rsync::DEFAULT_OPTIONS
-    assert_equal(
-      [ 'rsync', opts, File.expand_path('output') + '/', 'asdf' ].flatten,
-      rsync.instance_eval { @shell_cms_args }
-    )
   end
 
   def test_run_everything_okay_dry
-    # Create deployer
-    rsync = Nanoc::Extra::Deployers::Rsync.new(
-      'output/',
-      { :dst => 'asdf' },
-      :dry_run => true)
+    if_have 'systemu' do
+      # Create deployer
+      rsync = Nanoc::Extra::Deployers::Rsync.new(
+        'output/',
+        { :dst => 'asdf' },
+        :dry_run => true)
 
-    # Mock run_shell_cmd
-    def rsync.run_shell_cmd(args)
-      @shell_cms_args = args
+      # Mock run_shell_cmd
+      def rsync.run_shell_cmd(args)
+        @shell_cms_args = args
+      end
+
+      # Run
+      rsync.run
+
+      # Check args
+      opts = Nanoc::Extra::Deployers::Rsync::DEFAULT_OPTIONS
+      assert_equal(
+        [ 'echo', 'rsync', opts, File.expand_path('output') + '/', 'asdf' ].flatten,
+        rsync.instance_eval { @shell_cms_args }
+      )
     end
-
-    # Run
-    rsync.run
-
-    # Check args
-    opts = Nanoc::Extra::Deployers::Rsync::DEFAULT_OPTIONS
-    assert_equal(
-      [ 'echo', 'rsync', opts, File.expand_path('output') + '/', 'asdf' ].flatten,
-      rsync.instance_eval { @shell_cms_args }
-    )
   end
 
 end
