fee445dfb3fa289c9375f7fa09cf15d5fb49e390
Denis Defreyne <denis.defreyne@stoneship.org>
2011-07-24 15:33:09 +0200

    made rdoc filter test be run when rdoc exists

--- test/filters/test_rdoc.rb
+++ test/filters/test_rdoc.rb
@@ -5,12 +5,14 @@ class Nanoc3::Filters::RDocTest < MiniTest::Unit::TestCase
   include Nanoc3::TestHelpers
 
   def test_filter
-    # Get filter
-    filter = ::Nanoc3::Filters::RDoc.new
+    if_have 'rdoc' do
+      # Get filter
+      filter = ::Nanoc3::Filters::RDoc.new
 
-    # Run filter
-    result = filter.run("= Foo")
-    assert_match(%r{<h1>Foo</h1>\Z}, result)
+      # Run filter
+      result = filter.run("= Foo")
+      assert_match(%r{<h1>Foo</h1>\Z}, result)
+    end
   end
 
 end
