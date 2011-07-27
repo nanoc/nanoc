7fa0bdf8adeea1f36624ed43269ddb14a87f0ce0
Arnau Siches <asiches@gmail.com>
2011-07-27 16:48:37 -0400

    Add test with params for xsl filter

--- test/filters/test_xsl.rb
+++ test/filters/test_xsl.rb
@@ -35,10 +35,11 @@ EOS
       # Run filter
       result = filter.run(xml_content, { :template => xsl_file.path })
       assert_equal(xml_content, result)
+      xsl_file.close
     end
   end
 
-  def test_filter
+  def test_filter_with_param
     if_have 'nokogiri' do
       require 'tempfile'
       # Create a simple XSL file
@@ -47,29 +48,30 @@ EOS
 <?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
   <xsl:output method="xml" version="1.0" encoding="utf-8" indent="yes" omit-xml-declaration="yes"/>
-
-  <xsl:template match="/ | node() | @* | comment() | processing-instruction()">
-    <xsl:copy>
-      <xsl:apply-templates select="@* | node()"/>
-    </xsl:copy>
+  <xsl:param name="foo"/>
+  <xsl:template match="/">
+    <result><xsl:value-of select="$foo"/></result>
   </xsl:template>
 </xsl:stylesheet>
 EOS
       
       xml_content = <<-EOS
 <?xml version="1.0" encoding="utf-8"?>
-<test foo="bar">
-  <shouldbe>identical</shouldbe>
-</test>
+<test with="param"/>
 EOS
+      xml_expected = <<-EOS
+<?xml version="1.0" encoding="utf-8"?>
+<result>bar</result>
+EOS
+
       xsl_file.open
       # Create filter
       filter = ::Nanoc::Filters::XSL.new
 
       # Run filter
-      result = filter.run(xml_content, { :template => xsl_file.path })
-      assert_equal(xml_content, result)
-      xsl_file.open
+      result = filter.run(xml_content, { :template => xsl_file.path, :foo => 'bar' })
+      assert_equal(xml_expected, result)
+      xsl_file.close
     end
   end
 
