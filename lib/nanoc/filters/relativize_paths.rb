be4cf0133a7d53920cd88855d42ee9e2a7bb88a9
Arnau Siches <asiches@gmail.com>
2011-08-03 19:28:04 -0400

    Add :xml and :xhtml types to relativize_path filters

--- lib/nanoc/filters/relativize_paths.rb
+++ lib/nanoc/filters/relativize_paths.rb
@@ -6,8 +6,8 @@ module Nanoc::Filters
     require 'nanoc/helpers/link_to'
     include Nanoc::Helpers::LinkTo
 
-    # Relativizes all paths in the given content, which can be either HTML or
-    # CSS. This filter is quite useful if a site needs to be hosted in a
+    # Relativizes all paths in the given content, which can be HTML, XHTML, XML
+    # or CSS. This filter is quite useful if a site needs to be hosted in a
     # subdirectory instead of a subdomain. In HTML, all `href` and `src`
     # attributes will be relativized. In CSS, all `url()` references will be
     # relativized.
@@ -15,7 +15,15 @@ module Nanoc::Filters
     # @param [String] content The content to filter
     #
     # @option params [Symbol] :type The type of content to filter; can be
-    #   either `:html` or `:css`.
+    #   `:html`, `:xhtml`, `:xml` or `:css`.
+    #
+    # @option params [Array] :select The XPath expressions that matches the
+    #   nodes to modify. This param is useful only for the `:xml` and `:xhtml`
+    #   types.
+    #
+    # @option params [Hash] :namespaces The pairs `prefix => uri` to define
+    #   any namespace you want to use in the XPath expressions. This param 
+    #   is useful only for the `:xml` and `:xhtml` types.
     #
     # @return [String] The filtered content
     def run(content, params={})
@@ -26,20 +34,51 @@ module Nanoc::Filters
       # FIXME use nokogiri or csspool instead of regular expressions
       case params[:type]
       when :html
-        content.gsub(/(<[^>]+\s+(src|href))=(['"]?)(\/.*?)\3([ >])/) do
+        content.gsub(/(<[^>]+\s+(src|href))=(['"]?)(\/.*?)\3([\s\/>])/) do
           $1 + '=' + $3 + relative_path_to($4) + $3 + $5
         end
       when :css
         content.gsub(/url\((['"]?)(\/.*?)\1\)/) do
           'url(' + $1 + relative_path_to($2) + $1 + ')'
         end
+      when :xml
+        selectors = params[:select] || ['//a/@href | a/@href', '//img/@src | img/@src', '//script/@src | script/@src', '//link/@href | link/@href']
+        namespaces = params[:namespaces] || {}
+        nokogiri_process(content, selectors, namespaces, params[:type])
+      when :xhtml
+        selectors = params[:select] || ['//a/@href | a/@href', '//img/@src | img/@src', '//script/@src | script/@src', '//link/@href | link/@href']
+        namespaces = params[:namespaces] || {}
+        nokogiri_process(content, selectors, namespaces, params[:type])
       else
         raise RuntimeError.new(
           "The relativize_paths needs to know the type of content to " +
-          "process. Pass :type => :html for HTML or :type => :css for CSS."
+          "process. Pass :type => :xml for XML, :type => :html for HTML or :type => :css for CSS."
         )
       end
     end
-
+    
+    private
+    
+    def nokogiri_process(content, selectors, namespaces, type)
+      require 'nokogiri'
+      # Because it's a DocumentFragment all XPath expressions that begins 
+      # with '//' should be changed to '*//'.
+      # Consider converting `//([^\s\[]+(\[[^\]]+])?)` in
+      # something like `*//\1 | \1` to make some magic with partial content.
+      selectors.map! { |s| s.sub(%r{^(//)}, '*\1') }
+      # Ensure that all prefixes are String
+      namespaces = namespaces.inject({}) { |new, (prefix, uri)| new.merge(prefix.to_s => uri) }
+      doc = ::Nokogiri::XML.fragment(content)
+      selectors.each do |selector|
+        doc.xpath(selector, namespaces).each do |node|
+          node.content = relative_path_to(node.content)
+        end
+      end
+      # Because using the `Nokogiri::XML::DocumentFragment` class DOCTYPE 
+      # pseudonodes becomes even more creepy than usual.
+      doc.method("to_#{type}").call.sub(/(!DOCTYPE.+?)(&gt;)/, '<\1>')
+    end
+    
+    
   end
 end
--- test/filters/test_relativize_paths.rb
+++ test/filters/test_relativize_paths.rb
@@ -4,6 +4,7 @@ class Nanoc::Filters::RelativizePathsTest < MiniTest::Unit::TestCase
 
   include Nanoc::TestHelpers
 
+
   def test_filter_html_with_double_quotes
     # Create filter with mock item
     filter = Nanoc::Filters::RelativizePaths.new
@@ -280,5 +281,169 @@ class Nanoc::Filters::RelativizePathsTest < MiniTest::Unit::TestCase
     actual_content = filter.run(raw_content, :type => :css)
     assert_equal(expected_content, actual_content)
   end
+
+
+  def test_filter_xml
+    if_have 'nokogiri' do
+      # Create filter with mock item
+      filter = Nanoc::Filters::RelativizePaths.new
+
+      # Mock item
+      filter.instance_eval do
+        @item_rep = Nanoc::ItemRep.new(
+          Nanoc::Item.new(
+            'content',
+            {},
+            '/foo/bar/baz/'),
+          :blah)
+        @item_rep.path = '/foo/bar/baz/'
+      end
+
+      # Set content
+      raw_content = <<-XML
+<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <bar boo="/foo">baz</bar>
+</foo>
+XML
+
+      expected_content = <<-XML
+<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <bar boo="../..">baz</bar>
+</foo>
+XML
+
+      # Test
+      actual_content = filter.run(raw_content, :type => :xml, :select => ['//@boo'])
+      assert_equal(expected_content, actual_content)
+    end
+  end
+
+  def test_filter_fragment_xml
+    if_have 'nokogiri' do
+      # Create filter with mock item
+      filter = Nanoc::Filters::RelativizePaths.new
+
+      # Mock item
+      filter.instance_eval do
+        @item_rep = Nanoc::ItemRep.new(
+          Nanoc::Item.new(
+            'content',
+            {},
+            '/foo/bar/baz/'),
+          :blah)
+        @item_rep.path = '/foo/bar/baz/'
+      end
+
+      # Set content
+      raw_content = <<-XML
+<foo>
+  <bar><far href="/foo">baz</far></bar>
+</foo>
+XML
+
+      expected_content = <<-XML
+<foo>
+  <bar><far href="../..">baz</far></bar>
+</foo>
+XML
+
+      # Test
+      actual_content = filter.run(raw_content, :type => :xml, :select => ['//far/@href'])
+      assert_equal(expected_content, actual_content)
+    end
+  end
+
+  def test_filter_xml_with_namespaces
+    if_have 'nokogiri' do
+      # Create filter with mock item
+      filter = Nanoc::Filters::RelativizePaths.new
+
+      # Mock item
+      filter.instance_eval do
+        @item_rep = Nanoc::ItemRep.new(
+          Nanoc::Item.new(
+            'content',
+            {},
+            '/foo/bar/baz/'),
+          :blah)
+        @item_rep.path = '/foo/bar/baz/'
+      end
+
+      # Set content
+      raw_content = <<-XML
+<foo xmlns="http://example.org">
+  <bar><a href="/foo">baz</a></bar>
+</foo>
+XML
+
+      expected_content = <<-XML
+<foo xmlns="http://example.org">
+  <bar><a href="../..">baz</a></bar>
+</foo>
+XML
+
+      # Test
+      actual_content = filter.run(raw_content, {
+        :type => :xml, 
+        :namespaces => {:ex => 'http://example.org'}, 
+        :select => ['//ex:a/@href']
+      })
+      assert_equal(expected_content, actual_content)
+    end
+  end
+
+  def test_filter_xml_with_doctype
+    if_have 'nokogiri' do
+      # Create filter with mock item
+      filter = Nanoc::Filters::RelativizePaths.new
+
+      # Mock item
+      filter.instance_eval do
+        @item_rep = Nanoc::ItemRep.new(
+          Nanoc::Item.new(
+            'content',
+            {},
+            '/foo/bar/baz/'),
+          :blah)
+        @item_rep.path = '/foo/bar/baz/'
+      end
+
+      # Set content
+      raw_content = <<-XML
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <link rel="stylesheet" href="/css"/>
+    <script src="/js"></script>
+  </head>
+  <body>
+    <a href="/foo">bar</a>
+    <img src="/img"/>
+  </body>
+</html>
+XML
+
+      expected_content = <<-XML
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <link rel="stylesheet" href="../../../css" />
+    <script src="../../../js"></script>
+  </head>
+  <body>
+    <a href="../..">bar</a>
+    <img src="../../../img" />
+  </body>
+</html>
+XML
+
+      # Test
+      actual_content = filter.run(raw_content, :type => :xhtml)
+      assert_equal(expected_content, actual_content)
+    end
+  end
+
 
 end
