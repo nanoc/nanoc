09d43c00f9eff5809a3184ae81762480f78ed196
Denis Defreyne <denis.defreyne@stoneship.org>
2011-10-16 21:57:27 +0200

    cleaned up and documented coffeescript filter

--- lib/nanoc/filters/coffeescript.rb
+++ lib/nanoc/filters/coffeescript.rb
@@ -2,10 +2,16 @@ require 'coffee-script'
 
 module Nanoc::Filters
   class CoffeeScript < Nanoc::Filter
-    identifier :coffeescript
 
+    # Runs the content through [CoffeeScript](http://coffeescript.org/).
+    # This method takes no options.
+    #
+    # @param [String] content The CoffeeScript content to turn into JavaScript
+    #
+    # @return [String] The resulting JavaScript
     def run(content, params = {})
       ::CoffeeScript.compile(content)
     end
+
   end
 end
