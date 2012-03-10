e87a1cf54c8433c347a302325e09787e6c87df5a
Denis Defreyne <denis.defreyne@stoneship.org>
2012-03-10 11:14:12 +0100

    fixed encoding of exception message

--- lib/nanoc/cli/error_handler.rb
+++ lib/nanoc/cli/error_handler.rb
@@ -66,7 +66,7 @@ module Nanoc::CLI
     def print_error(error)
       # Header
       $stderr.puts
-      $stderr.puts "Captain! We’ve been hit!"
+      $stderr.puts "Captain! We’ve been hit!".make_compatible_with_env
 
       # Exception and resolution (if any)
       $stderr.puts
