19a9a2d7baa831c2a32c03d8f711ffe222afa51c
Denis Defreyne <denis.defreyne@stoneship.org>
2012-05-27 10:28:29 +0200

    fixed failing error handler test

--- test/cli/test_cli.rb
+++ test/cli/test_cli.rb
@@ -87,6 +87,11 @@ EOS
 
       # Run command
       position_before = $stderr.tell
+      Nanoc::CLI::ErrorHandler.disable_error_handling
+      assert_raises RuntimeError do
+        Nanoc::CLI.run %w( _test )
+      end
+      Nanoc::CLI::ErrorHandler.enable_error_handling
       assert_raises SystemExit do
         Nanoc::CLI.run %w( _test )
       end
