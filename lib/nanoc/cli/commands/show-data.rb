6324bf5fa8c59860c340d176853d012c8ad99556
Denis Defreyne <denis.defreyne@stoneship.org>
2012-09-24 07:42:50 +0200

    refactored show-data command more

--- lib/nanoc/cli/commands/show-data.rb
+++ lib/nanoc/cli/commands/show-data.rb
@@ -12,92 +12,6 @@ module Nanoc::CLI::Commands
 
   class ShowData < ::Nanoc::CLI::CommandRunner
 
-    def print_header(title)
-      header = '=' * 78
-      header[3..(title.length+5)] = " #{title} "
-
-      puts
-      puts header
-      puts
-    end
-
-    def print_item_dependencies(items, dependency_tracker)
-      self.print_header('Item dependencies')
-
-      is_first = true
-      items.sort_by { |i| i.identifier }.each do |item|
-        puts unless is_first
-        is_first = false
-        puts "item #{item.identifier} depends on:"
-        predecessors = dependency_tracker.objects_causing_outdatedness_of(item).sort_by { |i| i ? i.identifier : '' }
-        predecessors.each do |pred|
-          if pred
-            puts "  [ #{format '%6s', pred.type} ] #{pred.identifier}"
-          else
-            puts "  ( removed item )"
-          end
-        end
-        puts "  (nothing)" if predecessors.empty?
-      end
-    end
-
-    def print_item_rep_paths(items)
-      self.print_header('Item representation paths')
-
-      is_first = true
-      items.sort_by { |i| i.identifier }.each do |item|
-        puts unless is_first
-        is_first = false
-        item.reps.sort_by { |r| r.name.to_s }.each do |rep|
-          puts "item #{item.identifier}, rep #{rep.name}:"
-          if rep.raw_paths.empty?
-            puts "  (not written)"
-          end
-          length = rep.raw_paths.keys.map { |s| s.to_s.length }.max
-          rep.raw_paths.each do |snapshot_name, raw_path|
-            puts "  [ %-#{length}s ] %s" % [ snapshot_name, raw_path ]
-          end
-        end
-      end
-    end
-
-    def print_item_rep_outdatedness(items, compiler)
-      self.print_header('Item representation outdatedness')
-
-      is_first = true
-      items.sort_by { |i| i.identifier }.each do |item|
-        puts unless is_first
-        is_first = false
-        item.reps.sort_by { |r| r.name.to_s }.each do |rep|
-          puts "item #{item.identifier}, rep #{rep.name}:"
-          outdatedness_reason = compiler.outdatedness_checker.outdatedness_reason_for(rep)
-          if outdatedness_reason
-            puts "  is outdated: #{outdatedness_reason.message}"
-          else
-            puts "  is not outdated"
-          end
-        end
-      end
-    end
-
-    def print_layouts(layouts, compiler)
-      self.print_header('Layouts')
-
-      is_first = true
-      layouts.sort_by { |l| l.identifier }.each do |layout|
-        puts unless is_first
-        is_first = false
-        puts "layout #{layout.identifier}:"
-        outdatedness_reason = compiler.outdatedness_checker.outdatedness_reason_for(layout)
-        if outdatedness_reason
-          puts "  is outdated: #{outdatedness_reason.message}"
-        else
-          puts "  is not outdated"
-        end
-        puts
-      end
-    end
-
     def run
       # Make sure we are in a nanoc site directory
       print "Loading site data... "
@@ -119,6 +33,100 @@ module Nanoc::CLI::Commands
       self.print_item_rep_paths(items)
       self.print_item_rep_outdatedness(items, compiler)
       self.print_layouts(layouts, compiler)
+    end
+
+  protected
+
+    def sorted_with_prev(objects)
+      prev = nil
+      objects.sort_by { |o| o.identifier }.each do |object|
+        yield(object, prev)
+        prev = object
+      end
+    end
+
+    def sorted_reps_with_prev(items)
+      prev = nil
+      items.sort_by { |i| i.identifier }.each do |item|
+        item.reps.sort_by { |r| r.name.to_s }.each do |rep|
+          yield(rep, prev)
+          prev = rep
+        end
+      end
+    end
+
+    def print_header(title)
+      header = '=' * 78
+      header[3..(title.length+5)] = " #{title} "
+
+      puts
+      puts header
+      puts
+    end
+
+    def print_item_dependencies(items, dependency_tracker)
+      self.print_header('Item dependencies')
+
+      self.sorted_with_prev(items) do |item, prev|
+        puts if prev
+        puts "item #{item.identifier} depends on:"
+        predecessors = dependency_tracker.objects_causing_outdatedness_of(item).sort_by { |i| i ? i.identifier : '' }
+        predecessors.each do |pred|
+          if pred
+            puts "  [ #{format '%6s', pred.type} ] #{pred.identifier}"
+          else
+            puts "  ( removed item )"
+          end
+        end
+        puts "  (nothing)" if predecessors.empty?
+      end
+    end
+
+    def print_item_rep_paths(items)
+      self.print_header('Item representation paths')
+
+      self.sorted_reps_with_prev(items) do |rep, prev|
+        puts if prev
+        puts "item #{rep.item.identifier}, rep #{rep.name}:"
+        if rep.raw_paths.empty?
+          puts "  (not written)"
+        end
+        length = rep.raw_paths.keys.map { |s| s.to_s.length }.max
+        rep.raw_paths.each do |snapshot_name, raw_path|
+          puts "  [ %-#{length}s ] %s" % [ snapshot_name, raw_path ]
+        end
+      end
+    end
+
+    def print_item_rep_outdatedness(items, compiler)
+      self.print_header('Item representation outdatedness')
+
+      self.sorted_reps_with_prev(items) do |rep, prev|
+        puts if prev
+        puts "item #{rep.item.identifier}, rep #{rep.name}:"
+        outdatedness_reason = compiler.outdatedness_checker.outdatedness_reason_for(rep)
+        if outdatedness_reason
+          puts "  is outdated: #{outdatedness_reason.message}"
+        else
+          puts "  is not outdated"
+        end
+      end
+    end
+
+    def print_layouts(layouts, compiler)
+      self.print_header('Layouts')
+
+      self.sorted_with_prev(layouts) do |layout, prev|
+        puts if prev
+        puts "layout #{layout.identifier}:"
+        outdatedness_reason = compiler.outdatedness_checker.outdatedness_reason_for(layout)
+        if outdatedness_reason
+          puts "  is outdated: #{outdatedness_reason.message}"
+        else
+          puts "  is not outdated"
+        end
+        puts
+      end
     end
 
   end
