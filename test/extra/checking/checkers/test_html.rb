f8c0d5a9eee97f688f66313ffb38bb84ea9df202
Denis Defreyne <denis.defreyne@stoneship.org>
2012-10-28 20:19:59 +0100

    fixed code to adhere to style guide

--- test/extra/checking/checkers/test_html.rb
+++ test/extra/checking/checkers/test_html.rb
@@ -8,8 +8,8 @@ class Nanoc::Extra::Checking::Checkers::HTMLTest < MiniTest::Unit::TestCase
     with_site do |site|
       # Create files
       FileUtils.mkdir_p('output')
-      File.open('output/blah.html', 'w') { | io| io.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hi!</h1></body>') }
-      File.open('output/style.css', 'w') { | io| io.write('h1 { coxlor: rxed; }') }
+      File.open('output/blah.html', 'w') { |io| io.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hi!</h1></body>') }
+      File.open('output/style.css', 'w') { |io| io.write('h1 { coxlor: rxed; }') }
 
       # Run checker
       checker = Nanoc::Extra::Checking::Checkers::HTML.new(site)
@@ -24,8 +24,8 @@ class Nanoc::Extra::Checking::Checkers::HTMLTest < MiniTest::Unit::TestCase
     with_site do |site|
       # Create files
       FileUtils.mkdir_p('output')
-      File.open('output/blah.html', 'w') { | io| io.write('<h2>Hi!</h1>') }
-      File.open('output/style.css', 'w') { | io| io.write('h1 { coxlor: rxed; }') }
+      File.open('output/blah.html', 'w') { |io| io.write('<h2>Hi!</h1>') }
+      File.open('output/style.css', 'w') { |io| io.write('h1 { coxlor: rxed; }') }
 
       # Run checker
       checker = Nanoc::Extra::Checking::Checkers::HTML.new(site)
--- test/extra/checking/checkers/test_stale.rb
+++ test/extra/checking/checkers/test_stale.rb
@@ -25,8 +25,8 @@ class Nanoc::Extra::Checking::Checkers::StaleTest < MiniTest::Unit::TestCase
       assert self.calc_issues.empty?
 
       # One OK file
-      File.open('content/index.html', 'w') { | io| io.write('stuff') }
-      File.open('output/index.html', 'w') { | io| io.write('stuff') }
+      File.open('content/index.html', 'w') { |io| io.write('stuff') }
+      File.open('output/index.html', 'w') { |io| io.write('stuff') }
       assert self.calc_issues.empty?
     end
   end
@@ -36,8 +36,8 @@ class Nanoc::Extra::Checking::Checkers::StaleTest < MiniTest::Unit::TestCase
       assert Dir['content/*'].empty?
       assert Dir['output/*'].empty?
 
-      File.open('content/index.html', 'w') { | io| io.write('stuff') }
-      File.open('output/WRONG.html', 'w') { | io| io.write('stuff') }
+      File.open('content/index.html', 'w') { |io| io.write('stuff') }
+      File.open('output/WRONG.html', 'w') { |io| io.write('stuff') }
       assert_equal 1, self.calc_issues.count
       issue = self.calc_issues.to_a[0]
       assert_equal "file without matching item", issue.description
