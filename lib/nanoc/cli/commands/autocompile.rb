6eda877c86a60759099b68549c00d1bcc1a296c9
Stuart Montgomery <smontgomery@apple.com>
2012-12-30 00:22:15 -0800

    Adding ability to specify host and/or port options for the autocompile command in config.yaml

--- lib/nanoc/cli/commands/autocompile.rb
+++ lib/nanoc/cli/commands/autocompile.rb
@@ -4,9 +4,16 @@ usage       'autocompile [options]'
 summary     'start the autocompiler'
 aliases     :aco
 description <<-EOS
-Start the autocompiler web server. Unless specified, the web server will run
-on port 3000 and listen on all IP addresses. Running the autocompiler requires
-the 'mime/types' and 'rack' gems.
+Start the autocompiler web server. Unless overridden with commandline options
+or configuration entries, the web server will run on port 3000 and listen on all
+IP addresses. Running the autocompiler requires the 'mime/types' and 'rack' gems.
+
+To specify the host and/or port options in config.yaml, you can add the following:
+
+  autocompile:
+    host: '10.0.2.0'  # override the default host
+    port: 4000        # override the default port
+
 EOS
 
 required :H, :handler, 'specify the handler to use (webrick/mongrel/…)'
@@ -22,11 +29,12 @@ module Nanoc::CLI::Commands
 
       # Make sure we are in a nanoc site directory
       self.require_site
+      autocompile_config = self.site.config[:autocompile] || {}
 
       # Set options
       options_for_rack = {
-        :Port      => (options[:port] || 3000).to_i,
-        :Host      => (options[:host] || '0.0.0.0')
+        :Port      => (options[:port] || autocompile_config[:port] || 3000).to_i,
+        :Host      => (options[:host] || autocompile_config[:host] || '0.0.0.0')
       }
 
       # Guess which handler we should use
