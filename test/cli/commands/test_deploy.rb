3b6192f1413157921ad4dabf564b9c705db9bb39
Denis Defreyne <denis.defreyne@stoneship.org>
2013-02-27 22:01:17 +0100

    Fix failing missing default deploy configuration test

--- test/cli/commands/test_deploy.rb
+++ test/cli/commands/test_deploy.rb
@@ -154,7 +154,7 @@ class Nanoc::CLI::Commands::DeployTest < MiniTest::Unit::TestCase
           err = assert_raises Nanoc::Errors::GenericTrivial do
             Nanoc::CLI.run %w( deploy )
           end
-          assert_equal 'The site configuration has no deploy configuration for default.', err.message
+          assert_equal 'The site has no deployment configuration for default.', err.message
         end
       end
     end
