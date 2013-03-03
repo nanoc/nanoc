82b2f5e9e8a74f714e92a257c8e53c60672d47d8
Denis Defreyne <denis.defreyne@stoneship.org>
2013-03-03 18:11:41 +0100

    Refactor watcher

--- lib/nanoc/extra/watcher.rb
+++ lib/nanoc/extra/watcher.rb
@@ -13,59 +13,14 @@ module Nanoc::Extra
       require 'pathname'
 
       require_site
-      watcher_config = self.site.config[:watcher] || {}
-
-      @notifier = Nanoc::Extra::UserNotifier.new
-
-      # Define rebuilder
-      rebuilder = lambda do |file_path|
-        # Determine filename
-        if file_path.nil?
-          filename = nil
-        else
-          filename = ::Pathname.new(file_path).relative_path_from(::Pathname.new(Dir.getwd)).to_s
-        end
-
-        # Notify
-        if filename
-          print "Change detected to #{filename}; recompiling… "
-        else
-          print "Watcher started; compiling the entire site… "
-        end
-
-        # Recompile
-        start = Time.now
-        site = Nanoc::Site.new('.')
-        begin
-          site.compile
-
-          # TODO include icon (--image misc/success-icon.png)
-          notify_on_compilation_success = watcher_config.fetch(:notify_on_compilation_success) { true }
-          if notify_on_compilation_success
-            @notifier.notify('Compilation complete')
-          end
-
-          time_spent = ((Time.now - start)*1000.0).round
-          puts "done in #{format '%is %ims', *(time_spent.divmod(1000))}"
-        rescue Exception => e
-          # TODO include icon (--image misc/error-icon.png)
-          notify_on_compilation_failure = watcher_config.fetch(:notify_on_compilation_failure) { true }
-          if notify_on_compilation_failure
-            @notifier.notify('Compilation failed')
-          end
-
-          puts
-          Nanoc::CLI::ErrorHandler.print_error(e)
-          puts
-        end
-      end
 
       # Rebuild once
-      rebuilder.call(nil)
+      print "Watcher started; compiling the entire site… "
+      self.recompile
 
       # Get directories to watch
-      dirs_to_watch  = watcher_config[:dirs_to_watch]  || ['content', 'layouts', 'lib']
-      files_to_watch = watcher_config[:files_to_watch] || ['nanoc.yaml', 'config.yaml', 'Rules', 'rules', 'Rules.rb', 'rules.rb']
+      dirs_to_watch  = self.watcher_config[:dirs_to_watch]  || ['content', 'layouts', 'lib']
+      files_to_watch = self.watcher_config[:files_to_watch] || ['nanoc.yaml', 'config.yaml', 'Rules', 'rules', 'Rules.rb', 'rules.rb']
       files_to_watch = Regexp.new(files_to_watch.map { |name| "#{Regexp.quote(name)}$"}.join("|"))
       ignore_dir = Regexp.new(Dir.glob("*").map{|dir| dir if File::ftype(dir) == "directory" }.compact.join("|"))
 
@@ -73,6 +28,7 @@ module Nanoc::Extra
       puts "Watching for changes…"
 
       callback = Proc.new do |modified, added, removed|
+        rebuilder = self.rebuilder_proc
         rebuilder.call(modified[0]) if modified[0]
         rebuilder.call(added[0]) if added[0]
         rebuilder.call(removed[0]) if removed[0]
@@ -87,6 +43,57 @@ module Nanoc::Extra
       rescue Interrupt
         listener.stop
         listener_root.stop
+      end
+    end
+
+  protected
+
+    def watcher_config
+      @_watcher_config ||= self.site.config[:watcher] || {}
+    end
+
+    def notify(message)
+      @_notifier ||= Nanoc::Extra::UserNotifier.new
+      @_notifier.notify(message)
+    end
+
+    def recompile
+      begin
+        start = Time.now
+        site = Nanoc::Site.new('.')
+        site.compile
+
+        # Notify done
+        notify_on_compilation_success = self.watcher_config.fetch(:notify_on_compilation_success) { true }
+        if notify_on_compilation_success
+          self.notify('Compilation complete')
+        end
+
+        # Show time spent
+        time_spent = ((Time.now - start)*1000.0).round
+        puts "done in #{format '%is %ims', *(time_spent.divmod(1000))}"
+      rescue Exception => e
+        # Notify done
+        notify_on_compilation_failure = self.watcher_config.fetch(:notify_on_compilation_failure) { true }
+        if notify_on_compilation_failure
+          self.notify('Compilation failed')
+        end
+
+        # Show error
+        puts
+        Nanoc::CLI::ErrorHandler.print_error(e)
+        puts
+      end
+    end
+
+    def rebuilder_proc
+      rebuilder = lambda do |file_path|
+        # Notify
+        filename = ::Pathname.new(file_path).relative_path_from(::Pathname.new(Dir.getwd)).to_s
+        print "Change detected to #{filename}; recompiling… "
+
+        # Compile start
+        self.recompile
       end
     end
 
