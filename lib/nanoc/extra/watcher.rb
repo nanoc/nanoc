acf22ed90d282b00744223d36be2397c9148069e
Denis Defreyne <denis.defreyne@stoneship.org>
2013-03-25 22:51:59 +0100

    Document watcher classes

--- lib/nanoc/extra/watcher.rb
+++ lib/nanoc/extra/watcher.rb
@@ -5,40 +5,50 @@ module Nanoc::Extra
   # Watches the working directory for changes and recompiles if necessary.
   class Watcher
 
+    # @return [Hash] The watcher configuration
     attr_reader :config
 
-    # TODO document
+    # @option params [Hash] :config The watcher configuration
     def initialize(params={})
       @config          = params.fetch(:config)
       @change_detector = params.fetch(:change_detector) { self.new_change_detector }
       @recompiler      = params.fetch(:recompiler)      { self.new_recompiler }
     end
 
-    # TODO document
+    # Starts the watcher asynchronously. Use {#stop} to stop the watcher.
+    #
+    # @return [void]
     def start
-      @recompiler.recompile
+      self.compile
       @change_detector.start
     end
 
-    # TODO document
+    # Stops the watcher. Each call to {#start} should be balanced with a call
+    # to this method.
+    #
+    # @return [void]
     def stop
       @change_detector.stop
     end
 
-    # TODO document
+  protected
+
+    def compile
+      puts 'Compiling.'
+      @recompiler.recompile
+    end
+
     def recompile
       puts 'Change detected; recompiling.'
       @recompiler.recompile
     end
 
-    # TODO document
     def new_change_detector
       change_detector = ChangeDetector.new(self.config)
       change_detector.on_change { self.recompile }
       change_detector
     end
 
-    # TODO document
     def new_recompiler
       Recompiler.new(:watcher_config => self.config)
     end
--- lib/nanoc/extra/watcher/recompiler.rb
+++ lib/nanoc/extra/watcher/recompiler.rb
@@ -9,13 +9,15 @@ class Nanoc::Extra::Watcher
 
     attr_accessor :user_notifier
 
-    # TODO document
+    # @option params [Hash] :watcher_config The watcher configuration to use
     def initialize(params={})
       @watcher_config = params.fetch(:watcher_config)
       @user_notifier  = params.fetch(:user_notifier) { Nanoc::Extra::UserNotifier.new }
     end
 
-    # TODO document
+    # Recompiles the site and notifies on success or failure.
+    #
+    # @return [void]
     def recompile
       begin
         site = Nanoc::Site.new('.')
@@ -28,27 +30,24 @@ class Nanoc::Extra::Watcher
       end
     end
 
-    # TODO document
+  protected
+
     def should_notify_success?
       @_should_notify_success ||= @watcher_config.fetch(:notify_on_compilation_success, true)
     end
 
-    # TODO document
     def should_notify_failure?
       @_should_notify_failure ||= @watcher_config.fetch(:notify_on_compilation_failure, true)
     end
 
-    # TODO document
     def notify_success
       self.notify(NOTIFICATION_MESSAGE_SUCCESS)
     end
 
-    # TODO document
     def notify_failure
       self.notify(NOTIFICATION_MESSAGE_FAILURE)
     end
 
-    # TODO document
     def notify(message)
       self.user_notifier.notify(message)
     end
