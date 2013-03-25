aa9e830d9427985b32b3ded3cb5ccd3bfb895d03
Denis Defreyne <denis.defreyne@stoneship.org>
2013-03-25 23:04:13 +0100

    Document change detector

--- lib/nanoc/extra/watcher/change_detector.rb
+++ lib/nanoc/extra/watcher/change_detector.rb
@@ -7,24 +7,22 @@ class Nanoc::Extra::Watcher
     DEFAULT_DIRS_TO_WATCH  = %w( content layouts lib )
     DEFAULT_FILES_TO_WATCH = %w( nanoc.yaml config.yaml Rules rules Rules.rb rules.rb )
 
+    # @param [Hash] watcher_config The watcher configuration
     def initialize(watcher_config={})
       @watcher_config = watcher_config
     end
 
+    # Defines a block to execute when one or more changes are detected.
+    #
+    # @return [void]
     def on_change(&block)
       @on_change_block = block
     end
 
-    def dirs_to_watch
-      @watcher_config.fetch(:dirs_to_watch, DEFAULT_DIRS_TO_WATCH)
-    end
-
-    def files_to_watch
-      files = @watcher_config.fetch(:files_to_watch, DEFAULT_FILES_TO_WATCH)
-      regex_string = files.map { |fn| '\A' + Regexp.quote(fn) + '\Z' }.join('|')
-      Regexp.new(regex_string)
-    end
-
+    # Starts the change detector asynchronously. Use {#stop} to stop the
+    # change detector.
+    #
+    # @return [void]
     def start
       require 'listen'
 
@@ -39,9 +37,25 @@ class Nanoc::Extra::Watcher
       @listener.start(false)
     end
 
+    # Stops the change detector. Each call to {#start} should be balanced with
+    # a call to this method.
+    #
+    # @return [void]
     def stop
       @listener_root.stop
       @listener.stop
+    end
+
+  protected
+
+    def dirs_to_watch
+      @watcher_config.fetch(:dirs_to_watch, DEFAULT_DIRS_TO_WATCH)
+    end
+
+    def files_to_watch
+      files = @watcher_config.fetch(:files_to_watch, DEFAULT_FILES_TO_WATCH)
+      regex_string = files.map { |fn| '\A' + Regexp.quote(fn) + '\Z' }.join('|')
+      Regexp.new(regex_string)
     end
 
   end
