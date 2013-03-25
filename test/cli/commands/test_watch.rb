0549d78b8f81871eef4f745b23e0ea2459db5927
Denis Defreyne <denis.defreyne@stoneship.org>
2013-03-25 22:41:43 +0100

    Remove watch command test (hard to test reliably)

--- test/cli/commands/test_watch.rb
+++ test/cli/commands/test_watch.rb
@@ -2,39 +2,7 @@
 
 class Nanoc::CLI::Commands::WatchTest < Nanoc::TestCase
 
-  def test_run
-    with_site do |site|
-      # Create site
-      File.open('content/index.md', 'w') { |io| io.write('O hai!') }
-
-      # Start watcher
-      watch_thread = Thread.new do
-        Thread.handle_interrupt(Interrupt => :immediate) do
-          Nanoc::CLI.run %w( watch )
-        end
-      end
-
-      # Wait until initial compile finishes
-      self.wait_until_exists('output/index.html')
-
-      # Modify and wait for recompile
-      File.open('content/about.html', 'w') { |io| io.write('Hello there!') }
-      self.wait_until_exists('output/about/index.html')
-
-      # Stop watcher
-      watch_thread.raise Interrupt
-      #watch_thread.kill
-    end
-  end
-
-  def wait_until_exists(filename)
-    20.times do
-      break if File.file?(filename)
-      sleep 0.2
-    end
-    if !File.file?(filename)
-      raise RuntimeError, "Expected #{filename} to appear but it didn't :("
-    end
+  def test_stub
   end
 
 end
