403efaa1bc8271be67693de082533b7079433222
Denis Defreyne <denis.defreyne@stoneship.org>
2013-04-01 08:54:07 +0200

    Add necessary charset to HTML check test

--- test/extra/checking/checks/test_html.rb
+++ test/extra/checking/checks/test_html.rb
@@ -6,7 +6,7 @@ class Nanoc::Extra::Checking::Checks::HTMLTest < Nanoc::TestCase
     with_site do |site|
       # Create files
       FileUtils.mkdir_p('output')
-      File.open('output/blah.html', 'w') { |io| io.write('<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hi!</h1></body>') }
+      File.open('output/blah.html', 'w') { |io| io.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Hello</title></head><body><h1>Hi!</h1></body>') }
       File.open('output/style.css', 'w') { |io| io.write('h1 { coxlor: rxed; }') }
 
       # Run check
