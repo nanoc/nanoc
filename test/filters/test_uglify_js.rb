c8d96ce0b8bfc4cb9fbf36565a5ab7ed666ee092
Denis Defreyne <denis.defreyne@stoneship.org>
2013-04-11 21:35:51 +0200

    Update Uglifier test cases to match UglifyJS2 behavior

--- test/filters/test_uglify_js.rb
+++ test/filters/test_uglify_js.rb
@@ -8,20 +8,22 @@ class Nanoc::Filters::UglifyJSTest < Nanoc::TestCase
       filter = ::Nanoc::Filters::UglifyJS.new
 
       # Run filter
-      result = filter.setup_and_run("foo = 1; (function(bar) { if (true) alert(bar); })(foo)")
+      input = "foo = 1; (function(bar) { if (true) alert(bar); })(foo)"
+      result = filter.setup_and_run(input)
       assert_match(/foo=1,function\((.)\)\{alert\(\1\)\}\(foo\);/, result)
     end
   end
 
   def test_filter_with_options
-
     if_have 'uglifier' do
-      # Create filter
       filter = ::Nanoc::Filters::UglifyJS.new
+      input = "if(donkey) alert('It is a donkey!');"
 
-      # Run filter
-      result = filter.setup_and_run("foo = 1; (function(bar) { if (true) alert(bar); })(foo)", :toplevel => true)
-      assert_match(/foo=1,function\((.)\)\{alert\(\1\)\}\(foo\);/, result)
+      result = filter.setup_and_run(input, output: { beautify: false })
+      assert_equal 'donkey&&alert("It is a donkey!");', result
+
+      result = filter.setup_and_run(input, output: { beautify: true })
+      assert_equal 'donkey && alert("It is a donkey!");', result
     end
   end
 
