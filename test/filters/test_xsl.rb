a3bb37a6cbc595d489f14eae1efa19ead1b1836c
Denis Defreyne <denis.defreyne@stoneship.org>
2014-04-14 09:06:27 +0200

    Make XSL tests less strict

--- test/filters/test_xsl.rb
+++ test/filters/test_xsl.rb
@@ -28,17 +28,7 @@ EOS
 </report>
 EOS
 
-  SAMPLE_XML_OUT = <<-EOS
-<?xml version="1.0" encoding="utf-8"?>
-<html>
-  <head>
-    <title>My Report</title>
-  </head>
-  <body>
-    <h1>My Report</h1>
-  </body>
-</html>
-EOS
+  SAMPLE_XML_OUT = %r{\A<?xml version="1.0" encoding="utf-8"?>\s*<html>\s*<head>\s*<title>My Report</title>\s*</head>\s*<body>\s*<h1>My Report</h1>\s*</body>\s*</html>\s*\Z}m
 
   SAMPLE_XSL_WITH_PARAMS = <<-EOS
 <?xml version="1.0" encoding="utf-8"?>
@@ -64,17 +54,7 @@ EOS
 </report>
 EOS
 
-  SAMPLE_XML_OUT_WITH_PARAMS = <<-EOS
-<?xml version="1.0" encoding="utf-8"?>
-<html>
-  <head>
-    <title>My Report</title>
-  </head>
-  <body>
-    <h1>bar</h1>
-  </body>
-</html>
-EOS
+  SAMPLE_XML_OUT_WITH_PARAMS = %r{\A<?xml version="1.0" encoding="utf-8"?>\s*<html>\s*<head>\s*<title>My Report</title>\s*</head>\s*<body>\s*<h1>bar</h1>\s*</body>\s*</html>\s*\Z}m
 
   SAMPLE_XSL_WITH_OMIT_XML_DECL = <<-EOS
 <?xml version="1.0" encoding="utf-8"?>
@@ -101,16 +81,7 @@ EOS
 </report>
 EOS
 
-  SAMPLE_XML_OUT_WITH_OMIT_XML_DECL = <<-EOS
-<html>
-  <head>
-    <title>My Report</title>
-  </head>
-  <body>
-    <h1>My Report</h1>
-  </body>
-</html>
-EOS
+  SAMPLE_XML_OUT_WITH_OMIT_XML_DECL = %r{\A<html>\s*<head>\s*<title>My Report</title>\s*</head>\s*<body>\s*<h1>My Report</h1>\s*</body>\s*</html>\s*\Z}m
 
   def test_filter_as_layout
     if_have 'nokogiri' do
@@ -132,7 +103,7 @@ EOS
 
       # Run the filter and validate the results
       result = filter.setup_and_run(layout.raw_content)
-      assert_equal SAMPLE_XML_OUT, result
+      assert_match SAMPLE_XML_OUT, result
     end
   end
 
@@ -157,7 +128,7 @@ EOS
       # Run the filter and validate the results
       result = filter.setup_and_run(layout.raw_content,
                                     :foo => 'bar')
-      assert_equal SAMPLE_XML_OUT_WITH_PARAMS, result
+      assert_match SAMPLE_XML_OUT_WITH_PARAMS, result
     end
   end
 
@@ -181,7 +152,7 @@ EOS
 
       # Run the filter and validate the results
       result = filter.setup_and_run(layout.raw_content)
-      assert_equal SAMPLE_XML_OUT_WITH_OMIT_XML_DECL, result
+      assert_match SAMPLE_XML_OUT_WITH_OMIT_XML_DECL, result
     end
   end
 
