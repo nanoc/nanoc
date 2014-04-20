007c00f8113e3a51d36c084f5a2de1ea8a32e18b
Denis Defreyne <denis.defreyne@stoneship.org>
2014-04-20 14:12:29 +0200

    Skip pruner test that checks behavior for output dir symlink
    JRuby 1.7.11 has buggy File.find behavior. Also see
    https://github.com/jruby/jruby/issues/1647

--- test/cli/commands/test_prune.rb
+++ test/cli/commands/test_prune.rb
@@ -94,6 +94,10 @@ class Nanoc::CLI::Commands::PruneTest < Nanoc::TestCase
 
   def test_run_with_symlink_to_output_dir
     skip_unless_have_symlink
+    if defined?(JRUBY_VERSION) && JRUBY_VERSION == '1.7.11'
+      skip "JRuby 1.7.11 has buggy File.find behavior (see https://github.com/jruby/jruby/issues/1647)"
+    end
+
     with_site do |site|
       # Set output dir
       FileUtils.rm_rf('output')
