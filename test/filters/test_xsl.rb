13bf41c7558931be0c17930c5fb0120dbc6c127c
Denis Defreyne <denis.defreyne@stoneship.org>
2014-04-21 19:32:24 +0200

    Fix broken XSLT stylesheet
    XSLT stylesheets that use variable names must define the variables.
    libxml appears to be too permissive and allow broken stylesheets, while
    JRuby-Nokogiri correctly fails to parse the stylesheet.
    
    For details, see the following analysis of this issue by @headius:
    https://github.com/sparklemotion/nokogiri/issues/1078#issuecomment-40936310
    
    This test still fails due to a missing <?xml> header. For details, see
    https://github.com/sparklemotion/nokogiri/issues/1079

--- test/filters/test_xsl.rb
+++ test/filters/test_xsl.rb
@@ -34,6 +34,7 @@ EOS
 <?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
   <xsl:output method="xml" version="1.0" encoding="utf-8" indent="yes"/>
+  <xsl:variable name="foo"/>
   <xsl:template match="/">
     <html>
       <head>
