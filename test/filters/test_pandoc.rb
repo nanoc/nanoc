3c161bb7c99ef00e05fa5c1d11ccfc19aaf00bb8
Denis Defreyne <denis.defreyne@stoneship.org>
2014-05-05 08:42:33 +0200

    Allow all pandoc options to be passed

--- test/filters/test_pandoc.rb
+++ test/filters/test_pandoc.rb
@@ -15,4 +15,19 @@ class Nanoc::Filters::PandocTest < Nanoc::TestCase
     end
   end
 
+  def test_params
+    if_have 'pandoc-ruby' do
+      skip_unless_have_command "pandoc"
+
+      # Create filter
+      filter = ::Nanoc::Filters::Pandoc.new
+
+      # Run filter
+      opts = [ :s, {:f => :markdown, :to => :html}, 'no-wrap', :toc ]
+      result = filter.setup_and_run("# Heading\n", *opts)
+      assert_match '<div id="TOC">', result
+      assert_match(%r{<h1 id=\"heading\">Heading</h1>\s*}, result)
+    end
+  end
+
 end
