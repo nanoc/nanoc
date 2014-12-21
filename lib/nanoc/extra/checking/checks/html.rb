0fb0f83fca44e7228dac8bab472627d2a560e6ba
Denis Defreyne <denis.defreyne@stoneship.org>
2014-12-21 11:14:00 +0100

    Improve HTML validator messages

--- lib/nanoc/extra/checking/checks/html.rb
+++ lib/nanoc/extra/checking/checks/html.rb
@@ -9,8 +9,12 @@ module ::Nanoc::Extra::Checking::Checks
 
       Dir[site.config[:output_dir] + '/**/*.{htm,html}'].each do |filename|
         results = ::W3CValidators::MarkupValidator.new.validate_file(filename)
+        lines = File.readlines(filename)
         results.errors.each do |e|
-          desc = e.message.gsub(%r{\s+}, ' ').strip
+          line_num = e.line.to_i - 1
+          line = lines[line_num]
+          message = e.message.gsub(%r{\s+}, ' ').strip.sub(/\s+:$/, '')
+          desc = "line #{line_num + 1}: #{message}: #{line}"
           add_issue(desc, subject: filename)
         end
       end
