4e37d7d4ea0da139c05fa1a0ca00ea94fcb107da
Michal Papis <mpapis@gmail.com>
2014-12-27 16:44:11 +0100

    formating split lines

--- lib/nanoc/data_sources/filesystem.rb
+++ lib/nanoc/data_sources/filesystem.rb
@@ -153,8 +153,8 @@ module Nanoc::DataSources
     def all_split_files_in(dir_name)
       grouped_filenames =
         all_files_in(dir_name).
-        reject   { |fn| fn =~ /(~|\.orig|\.rej|\.bak)$/ }.
-        group_by { |fn| basename_of(fn) }
+          reject   { |fn| fn =~ /(~|\.orig|\.rej|\.bak)$/ }.
+          group_by { |fn| basename_of(fn) }
 
       grouped_filenames.each_pair do |key, filenames|
         # Divide
