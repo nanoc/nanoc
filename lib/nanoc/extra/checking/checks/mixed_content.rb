e2d4feb54cfee11bbb576abcd6edfe0c546892fd
Mike Pennisi <mike@mikepennisi.com>
2015-04-18 15:41:37 -0400

    Implement "Mixed Content" check
    This check ensures that resources embedded within HTML documents are not
    requested using a protocol that degrades the security of the original
    connection. This avoids "mixed content" warnings in browsers that
    enforce consistent protocols, and it precludes the leaking of private
    information in browsers that do not.

---
+++ lib/nanoc/extra/checking/checks/mixed_content.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+module Nanoc::Extra::Checking::Checks
+  # A check that verifies HTML files do not reference external resources with
+  # URLs that would trigger "mixed content" warnings.
+  class MixedContent < ::Nanoc::Extra::Checking::Check
+    PROTOCOL_PATTERN = /^(\w+):\/\//
+
+    def run
+      filenames = output_filenames.select { |f| File.extname(f) == '.html' }
+      resource_uris_with_filenames = ::Nanoc::Extra::LinkCollector.new(filenames).filenames_per_resource_uri
+
+      resource_uris_with_filenames.each_pair do |uri, fns|
+        next unless guaranteed_insecure?(uri)
+        fns.each do |filename|
+          add_issue(
+            "mixed content include: #{uri}",
+            subject: filename)
+        end
+      end
+    end
+
+    private
+
+    def guaranteed_insecure?(href)
+      protocol = PROTOCOL_PATTERN.match(href)
+
+      protocol && protocol[1].downcase == 'http'
+    end
+  end
+end
