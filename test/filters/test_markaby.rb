97204636e5d4fa90fd22d6d87f96edf1e92e1664
Denis Defreyne <denis.defreyne@stoneship.org>
2015-05-13 09:41:54 +0200

    Re-enable Markaby test
    It apparently works again!

--- test/filters/test_markaby.rb
+++ test/filters/test_markaby.rb
@@ -2,12 +2,6 @@
 
 class Nanoc::Filters::MarkabyTest < Nanoc::TestCase
   def test_filter
-    # Don’t run this test on 1.9.x, because it breaks and it annoys me
-    if RUBY_VERSION >= '1.9'
-      skip 'Markaby is not compatible with 1.9.x'
-      return
-    end
-
     if_have 'markaby' do
       # Create filter
       filter = ::Nanoc::Filters::Markaby.new
