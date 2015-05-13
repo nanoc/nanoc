ee3e4c262ffd4d2bc706ad7dfe44caf4630822ec
Denis Defreyne <denis.defreyne@stoneship.org>
2015-05-13 11:30:47 +0200

    Add LayoutCollectionView#[]

--- lib/nanoc/base/views/layout_collection.rb
+++ lib/nanoc/base/views/layout_collection.rb
@@ -28,5 +28,39 @@ module Nanoc
       @layouts.each { |l| yield view_class.new(l) }
       self
     end
+
+    # @overload [](string)
+    #
+    #   Finds the item whose identifier matches the given string.
+    #
+    #   If the glob syntax is enabled, the string can be a glob, in which case
+    #   this method finds the first item that matches the given glob.
+    #
+    #   @param [String] string
+    #
+    #   @return [nil] if no item matches the string
+    #
+    #   @return [Nanoc::ItemView] if an item was found
+    #
+    # @overload [](regex)
+    #
+    #   Finds the item whose identifier matches the given regular expression.
+    #
+    #   @param [Regex] regex
+    #
+    #   @return [nil] if no item matches the regex
+    #
+    #   @return [Nanoc::ItemView] if an item was found
+    def [](arg)
+      layout = @layouts.find { |l| l.identifier == arg }
+      return view_class.new(layout) if layout
+
+      # FIXME: this should only work if globs are enabled
+      pat = Nanoc::Int::Pattern.from(arg)
+      layout = @layouts.find { |l| pat.match?(l.identifier) }
+      return view_class.new(layout) if layout
+
+      nil
+    end
   end
 end
