1b8dd30798dce8890ce489f41a3071d85b227cfc
Yannick Ihmels <yannick@ihmels.com>
2015-07-27 18:23:14 +0200

    Exclude URLs from external links check

--- test/extra/checking/checks/test_external_links.rb
+++ test/extra/checking/checks/test_external_links.rb
@@ -72,4 +72,15 @@ class Nanoc::Extra::Checking::Checks::ExternalLinksTest < Nanoc::TestCase
       assert_equal '/meow?foo=bar', check.send(:path_for_url, URI.parse('http://example.com/meow?foo=bar'))
     end
   end
+
+  def test_excluded
+    with_site do |site|
+      # Create check
+      check = Nanoc::Extra::Checking::Checks::ExternalLinks.create(site)
+      site.config.update({ checks: { external_links: { exclude: ['^http://example.com$'] } } })
+
+      # Test
+      assert check.send(:excluded?, 'http://example.com')
+    end
+  end
 end
