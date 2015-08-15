752dfe54e7779bd954a55ade0e053baa557eb776
Denis Defreyne <denis.defreyne@stoneship.org>
2015-08-15 10:51:14 +0200

    Disallow storing document (views) in attributes

--- lib/nanoc/base/views/mixins/mutable_document_view_mixin.rb
+++ lib/nanoc/base/views/mixins/mutable_document_view_mixin.rb
@@ -1,11 +1,34 @@
 module Nanoc
   module MutableDocumentViewMixin
+    # @api private
+    class DisallowedAttributeValueError < Nanoc::Error
+      attr_reader :value
+
+      def initialize(value)
+        @value = value
+      end
+
+      def message
+        "The #{value.class} cannot be stored inside an attribute. Store its identifier instead."
+      end
+    end
+
     # Sets the value for the given attribute.
     #
     # @param [Symbol] key
     #
     # @see Hash#[]=
     def []=(key, value)
+      disallowed_value_classes = Set.new([
+        Nanoc::Int::Item,
+        Nanoc::Int::Layout,
+        Nanoc::ItemView,
+        Nanoc::LayoutView,
+      ])
+      if disallowed_value_classes.include?(value.class)
+        raise DisallowedAttributeValueError.new(value)
+      end
+
       unwrap.attributes[key] = value
     end
 
