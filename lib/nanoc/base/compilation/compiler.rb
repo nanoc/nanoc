b777759b17162c2f5a3f6f603116bff5b9a88b28
Denis Defreyne <denis.defreyne@stoneship.org>
2015-11-28 11:46:25 +0100

    Enable IndentHash cop

--- lib/nanoc/base/compilation/compiler.rb
+++ lib/nanoc/base/compilation/compiler.rb
@@ -275,7 +275,7 @@ module Nanoc::Int
       end
 
       # TODO: Do not expose @site (necessary for captures store though…)
-      content_or_filename_assigns.merge({
+      content_or_filename_assigns.merge(
         item: Nanoc::ItemView.new(rep.item),
         rep: Nanoc::ItemRepView.new(rep),
         item_rep: Nanoc::ItemRepView.new(rep),
@@ -283,7 +283,7 @@ module Nanoc::Int
         layouts: Nanoc::LayoutCollectionView.new(site.layouts),
         config: Nanoc::ConfigView.new(site.config),
         site: Nanoc::SiteView.new(site),
-      })
+      )
     end
 
     # @return [Nanoc::Int::OutdatednessChecker] The outdatedness checker
@@ -291,7 +291,8 @@ module Nanoc::Int
       Nanoc::Int::OutdatednessChecker.new(
         site: @site,
         checksum_store: checksum_store,
-        dependency_tracker: dependency_tracker)
+        dependency_tracker: dependency_tracker,
+      )
     end
     memoize :outdatedness_checker
 
@@ -421,11 +422,11 @@ module Nanoc::Int
 
     # Returns a preprocessor context, creating one if none exists yet.
     def preprocessor_context
-      Nanoc::Int::Context.new({
+      Nanoc::Int::Context.new(
         config: Nanoc::MutableConfigView.new(@site.config),
         items: Nanoc::MutableItemCollectionView.new(@site.items),
         layouts: Nanoc::MutableLayoutCollectionView.new(@site.layouts),
-      })
+      )
     end
     memoize :preprocessor_context
 
