d3aa893eb364e4dc9b51148ac04e04d1defaac32
Denis Defreyne <denis.defreyne@stoneship.org>
2015-12-20 15:16:50 +0100

    Let #breadcrumbs_trail raise for full identifiers

--- test/helpers/test_breadcrumbs.rb
+++ test/helpers/test_breadcrumbs.rb
@@ -3,7 +3,7 @@ class Nanoc::Helpers::BreadcrumbsTest < Nanoc::TestCase
 
   def test_breadcrumbs_trail_at_root
     @items = Nanoc::Int::IdentifiableCollection.new({})
-    item = Nanoc::Int::Item.new('root', {}, '/')
+    item = Nanoc::Int::Item.new('root', {}, Nanoc::Identifier.new('/', type: :legacy))
     @items << item
     @item = item
 
@@ -12,8 +12,8 @@ class Nanoc::Helpers::BreadcrumbsTest < Nanoc::TestCase
 
   def test_breadcrumbs_trail_with_1_parent
     @items = Nanoc::Int::IdentifiableCollection.new({})
-    parent_item = Nanoc::Int::Item.new('parent', {}, '/')
-    child_item  = Nanoc::Int::Item.new('child',  {}, '/foo/')
+    parent_item = Nanoc::Int::Item.new('parent', {}, Nanoc::Identifier.new('/', type: :legacy))
+    child_item  = Nanoc::Int::Item.new('child',  {}, Nanoc::Identifier.new('/foo/', type: :legacy))
     @items << parent_item
     @items << child_item
     @item = child_item
@@ -23,9 +23,9 @@ class Nanoc::Helpers::BreadcrumbsTest < Nanoc::TestCase
 
   def test_breadcrumbs_trail_with_many_parents
     @items = Nanoc::Int::IdentifiableCollection.new({})
-    grandparent_item = Nanoc::Int::Item.new('grandparent', {}, '/')
-    parent_item      = Nanoc::Int::Item.new('parent',      {}, '/foo/')
-    child_item       = Nanoc::Int::Item.new('child',       {}, '/foo/bar/')
+    grandparent_item = Nanoc::Int::Item.new('grandparent', {}, Nanoc::Identifier.new('/', type: :legacy))
+    parent_item      = Nanoc::Int::Item.new('parent',      {}, Nanoc::Identifier.new('/foo/', type: :legacy))
+    child_item       = Nanoc::Int::Item.new('child',       {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
     @items << grandparent_item
     @items << parent_item
     @items << child_item
@@ -36,12 +36,25 @@ class Nanoc::Helpers::BreadcrumbsTest < Nanoc::TestCase
 
   def test_breadcrumbs_trail_with_nils
     @items = Nanoc::Int::IdentifiableCollection.new({})
-    grandparent_item = Nanoc::Int::Item.new('grandparent', {}, '/')
-    child_item       = Nanoc::Int::Item.new('child',       {}, '/foo/bar/')
+    grandparent_item = Nanoc::Int::Item.new('grandparent', {}, Nanoc::Identifier.new('/', type: :legacy))
+    child_item       = Nanoc::Int::Item.new('child',       {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
     @items << grandparent_item
     @items << child_item
     @item = child_item
 
     assert_equal [grandparent_item, nil, child_item], breadcrumbs_trail
+  end
+
+  def test_breadcrumbs_trail_with_non_legacy_identifiers
+    @items = Nanoc::Int::IdentifiableCollection.new({})
+    parent_item = Nanoc::Int::Item.new('parent', {}, '/')
+    child_item  = Nanoc::Int::Item.new('child',  {}, '/foo/')
+    @items << parent_item
+    @items << child_item
+    @item = child_item
+
+    assert_raises Nanoc::Helpers::Breadcrumbs::CannotGetBreadcrumbsForNonLegacyItem do
+      breadcrumbs_trail
+    end
   end
 end
