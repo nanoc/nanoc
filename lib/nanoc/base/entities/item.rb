9eeebd4852d495efdc9b0d3e34f24fdc1968a768
Denis Defreyne <denis.defreyne@stoneship.org>
2015-12-20 21:32:51 +0100

    Remove redundant override

--- lib/nanoc/base/entities/item.rb
+++ lib/nanoc/base/entities/item.rb
@@ -1,11 +1,6 @@
 module Nanoc::Int
   # @api private
   class Item < ::Nanoc::Int::Document
-    # @see Document#initialize
-    def initialize(content, attributes, identifier)
-      super
-    end
-
     # Returns an object that can be used for uniquely identifying objects.
     #
     # @api private
