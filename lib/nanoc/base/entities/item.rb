fa5fda8f1d7ee15ae394840cfe25049e029a31f1
Denis Defreyne <denis.defreyne@stoneship.org>
2015-12-20 21:11:24 +0100

    Remove forced_outdated

--- lib/nanoc/base/entities/item.rb
+++ lib/nanoc/base/entities/item.rb
@@ -4,8 +4,6 @@ module Nanoc::Int
     # @see Document#initialize
     def initialize(content, attributes, identifier)
       super
-
-      @forced_outdated_status = ForcedOutdatedStatus.new
     end
 
     # Returns an object that can be used for uniquely identifying objects.
@@ -15,30 +13,6 @@ module Nanoc::Int
     # @return [Object] An unique reference to this object
     def reference
       [:item, identifier.to_s]
-    end
-
-    # Hack to allow a frozen item to still have modifiable frozen status.
-    #
-    # FIXME: Remove this.
-    class ForcedOutdatedStatus
-      attr_accessor :bool
-
-      def initialize
-        @bool = false
-      end
-
-      def freeze
-      end
-    end
-
-    # @api private
-    def forced_outdated=(bool)
-      @forced_outdated_status.bool = bool
-    end
-
-    # @api private
-    def forced_outdated?
-      @forced_outdated_status.bool
     end
   end
 end
