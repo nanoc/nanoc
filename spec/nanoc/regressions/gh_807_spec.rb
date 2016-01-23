3844b6d0ea0422cbe9d1a69db3cda13095d7ab7e
Denis Defreyne <denis.defreyne@stoneship.org>
2016-01-23 10:57:11 +0100

    Pass view context with dependency tracker to rule mem calculator
    No actual dependency tracking needs to be done at this point. If
    anything would cause a dependency to be created, it’d either do so at a
    later stage, or would cause the rule memory to change.

---
+++ spec/nanoc/regressions/gh_807_spec.rb
@@ -0,0 +1,17 @@
+describe 'GH-897', site: true, stdio: true do
+  before do
+    File.write('content/item.md', 'Stuff!')
+    File.write('Rules', <<EOS)
+  compile '/**/*' do
+    filter :erb if item[:dynamic]
+    write item.identifier.without_ext + '.html'
+  end
+EOS
+  end
+
+  it 'does not crash' do
+    Nanoc::CLI.run(['compile'])
+
+    expect(File.read('output/item.html')).to eql('Stuff!')
+  end
+end
