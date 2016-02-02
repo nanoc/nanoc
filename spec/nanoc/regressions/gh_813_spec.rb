6dd70da3d32fcb1cc8590e49a7967fc36c895349
Denis Defreyne <denis.defreyne@stoneship.org>
2016-02-02 18:57:26 +0100

    Generate diff for correct path
    `rep.raw_path` will only be the correct path if the snapshot is `:last`,
    but that’s not guaranteed.

---
+++ spec/nanoc/regressions/gh_813_spec.rb
@@ -0,0 +1,22 @@
+describe 'GH-813', site: true, stdio: true do
+  before do
+    File.write('nanoc.yaml', "enable_output_diff: true\n")
+    File.write('content/greeting.md', 'Hallöchen!')
+    File.write('Rules', <<EOS)
+  compile '/**/*' do
+    snapshot :donkey, path: '/donkey.html'
+    filter :kramdown
+  end
+EOS
+
+    Nanoc::CLI.run(['compile'])
+  end
+
+  specify 'Nanoc generates diff for proper path' do
+    File.write('content/greeting.md', 'Hellosies!')
+    Nanoc::CLI.run(['compile'])
+
+    diff = File.read('output.diff')
+    expect(diff).to start_with("--- output/donkey.html\n+++ output/donkey.html\n")
+  end
+end
