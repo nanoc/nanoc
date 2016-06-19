867655674af62d3784ce1b06db8a560874a4c3f6
Remi Barraquand <dev@remibarraquand.com>
2016-06-19 14:47:04 +0200

    fixup! Add Nanoc::Int::Store.tmp_path_for test

--- test/base/test_store.rb
+++ test/base/test_store.rb
@@ -32,4 +32,16 @@ class Nanoc::Int::StoreTest < Nanoc::TestCase
     store.load
     assert_equal(nil, store.data)
   end
+
+  def test_tmp_path_with_nil_env
+    tmp_path_for_checksum = Nanoc::Int::Store.tmp_path_for(nil, 'checksum')
+    assert_equal('tmp/checksum', tmp_path_for_checksum)
+  end
+
+  def test_tmp_path_with_test_env
+    tmp_path_for_checksum = Nanoc::Int::Store.tmp_path_for('test', 'checksum')
+    tmp_path_for_dependencies = Nanoc::Int::Store.tmp_path_for(:test, 'dependencies')
+    assert_equal('tmp/test/checksum', tmp_path_for_checksum)
+    assert_equal('tmp/test/dependencies', tmp_path_for_dependencies)
+  end
 end
