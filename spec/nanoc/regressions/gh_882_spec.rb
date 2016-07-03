88ddbee400d8c3de91ea5cbddb4f950337315189
Denis Defreyne <denis.defreyne@stoneship.org>
2016-07-03 11:20:12 +0200

    Add spec for #882

---
+++ spec/nanoc/regressions/gh_882_spec.rb
@@ -0,0 +1,29 @@
+describe 'GH-882', site: true, stdio: true do
+  before do
+    File.write('content/foo.md', 'I am foo!')
+    File.write('content/bar.md', 'I am bar!')
+
+    File.write('Rules', <<EOS)
+  compile '/**/*' do
+    write item.identifier.without_ext + '.html'
+  end
+
+  postprocess do
+    modified_reps = items.flat_map(&:modified)
+    modified_reps.each do |rep|
+      puts "Modified: \#{rep.item.identifier} - \#{rep.name}"
+    end
+  end
+EOS
+  end
+
+  example do
+    Nanoc::CLI.run(%w(compile))
+
+    File.write('content/bar.md', 'I am bar! Modified!')
+    expect { Nanoc::CLI.run(%w(compile)) }.to output(%r{^Modified: /bar.md - default$}).to_stdout
+
+    File.write('content/bar.md', 'I am bar! Modified again!')
+    expect { Nanoc::CLI.run(%w(compile)) }.not_to output(%r{^Modified: /foo.md - default$}).to_stdout
+  end
+end
