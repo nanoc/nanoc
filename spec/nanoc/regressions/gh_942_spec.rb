79c93a99d5f998b968f2c973ec890041fcad75a5
Denis Defreyne <denis.defreyne@stoneship.org>
2016-09-26 20:48:21 +0200

    Prune before compiling (fixes #942)

---
+++ spec/nanoc/regressions/gh_942_spec.rb
@@ -0,0 +1,21 @@
+describe 'GH-942', site: true, stdio: true do
+  before do
+    File.write('content/foo.md', 'Foo!')
+    File.write('Rules', <<EOS)
+  compile '/foo.*' do
+    write '/parent/foo'
+  end
+EOS
+
+    File.open('nanoc.yaml', 'w') do |io|
+      io << 'prune:' << "\n"
+      io << '  auto_prune: true' << "\n"
+    end
+  end
+
+  example do
+    File.write('output/parent', 'Hahaaa! I am a file and not a directory!')
+    Nanoc::CLI.run(%w(compile))
+    expect(File.read('output/parent/foo')).to eq('Foo!')
+  end
+end
