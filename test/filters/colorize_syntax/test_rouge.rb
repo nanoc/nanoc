15c710372838c45d86eb1f7f89c907c1018d7b55
Remi Barraquand <dev@remibarraquand.com>
2016-10-17 14:27:13 +0200

    Fix existing Rouge tests for both 1.x and 2.x

--- test/filters/colorize_syntax/test_rouge.rb
+++ test/filters/colorize_syntax/test_rouge.rb
@@ -80,7 +80,19 @@ EOS
       filter = ::Nanoc::Filters::ColorizeSyntax.new
 
       # Get expected output
-      expected_output = <<EOS
+      expected_rouge_1_output = <<EOS
+before
+<pre><code class="language-ruby"><table style="border-spacing: 0"><tbody><tr>
+<td class="gutter gl" style="text-align: right"><pre class="lineno">1
+2</pre></td>
+<td class="code"><pre>  <span class="k">def</span> <span class="nf">foo</span>
+  <span class="k">end</span><span class="w">
+</span></pre></td>
+</tr></tbody></table></code></pre>
+after
+EOS
+
+      expected_rouge_2_output = <<EOS
 before
 <pre><code class="language-ruby"><table class="rouge-table"><tbody><tr>
 <td class="rouge-gutter gl"><pre class="lineno">1
@@ -99,7 +111,7 @@ EOS
         rouge: { legacy: true, line_numbers: true },
       )
 
-      assert_equal(expected_output, actual_output)
+      assert_equal(Rouge.version < '2' ? expected_rouge_1_output : expected_rouge_2_output, actual_output)
     end
   end
 end
