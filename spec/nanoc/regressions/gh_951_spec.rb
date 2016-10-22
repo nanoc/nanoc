7d26d5ee357b9b19322b970f24b540091d5de3ee
Denis Defreyne <denis.defreyne@stoneship.org>
2016-10-22 17:54:56 +0200

    Fix support for legacy patterns combined with full identifiers

---
+++ spec/nanoc/regressions/gh_951_spec.rb
@@ -0,0 +1,19 @@
+describe 'GH-951', site: true, stdio: true do
+  before do
+    File.write('content/foo.md', 'Foo!')
+
+    File.open('nanoc.yaml', 'w') do |io|
+      io << 'string_pattern_type: legacy' << "\n"
+    end
+
+    File.write('Rules', <<EOS)
+  passthrough '/foo.md'
+EOS
+  end
+
+  it 'copies foo.md' do
+    Nanoc::CLI.run(%w(compile))
+
+    expect(File.file?('output/foo.md')).to eq(true)
+  end
+end
