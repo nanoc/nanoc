7c3a68f607f4c791ff560c2a1963a43ab62bca90
Denis Defreyne <denis.defreyne@stoneship.org>
2016-10-28 07:57:41 +0200

    Add regression spec for GH-913

---
+++ spec/nanoc/regressions/gh_913_spec.rb
@@ -0,0 +1,24 @@
+describe 'GH-891', site: true, stdio: true do
+  before do
+    File.write('content/hello.html', 'hi!')
+
+    File.write('Rules', <<EOS)
+  postprocess do
+    items.map(&:compiled_content)
+  end
+
+  compile '/**/*' do
+    write item.identifier.without_ext + '.html'
+  end
+
+  layout '/foo.*', :erb
+EOS
+  end
+
+  example do
+    2.times do
+      Nanoc::CLI.run(%w(compile))
+      expect(File.read('output/hello.html')).to eq('hi!')
+    end
+  end
+end
