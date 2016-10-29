7ffd07c230b36285dc2ab2e7e5f478858aafd2fa
Denis Defreyne <denis.defreyne@stoneship.org>
2016-10-29 10:33:46 +0200

    Add regression specs for GH-970

---
+++ spec/nanoc/regressions/gh_970a_spec.rb
@@ -0,0 +1,17 @@
+describe 'GH-970 (show-rules)', site: true, stdio: true do
+  before do
+    File.write('content/foo.md', 'foo')
+
+    File.write('Rules', <<EOS)
+compile '/foo.*' do
+  write '/donkey.html'
+end
+EOS
+  end
+
+  it 'shows reps' do
+    expect { Nanoc::CLI.run(%w(show-rules --no-color)) }.to(
+      output(/^Item \/foo\.md:\n  Rep default: \/foo\.\*$/).to_stdout,
+    )
+  end
+end
---
+++ spec/nanoc/regressions/gh_970b_spec.rb
@@ -0,0 +1,50 @@
+describe 'GH-970 (show-data)', site: true, stdio: true do
+  before do
+    File.write('content/foo.md', 'foo')
+    File.write('content/bar.md', '<%= @items["/foo.*"].compiled_content %>')
+
+    File.write('Rules', <<EOS)
+compile '/foo.*' do
+  write '/foo.html'
+end
+
+compile '/bar.*' do
+  filter :erb
+  write '/bar.html'
+end
+EOS
+  end
+
+  before { Nanoc::CLI.run(%w(compile)) }
+
+  it 'shows default rep outdatedness' do
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/foo\.md, rep default:\n  is not outdated/).to_stdout,
+    )
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/bar\.md, rep default:\n  is not outdated/).to_stdout,
+    )
+  end
+
+  it 'shows file as outdated after modification' do
+    File.write('content/bar.md', 'JUST BAR!')
+
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/foo\.md, rep default:\n  is not outdated/).to_stdout,
+    )
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/bar\.md, rep default:\n  is outdated: /).to_stdout,
+    )
+  end
+
+  it 'shows file and dependencies as outdated after modification' do
+    File.write('content/foo.md', 'FOO!')
+
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/foo\.md, rep default:\n  is outdated: /).to_stdout,
+    )
+    expect { Nanoc::CLI.run(%w(show-data --no-color)) }.to(
+      output(/^item \/bar\.md, rep default:\n  is outdated: /).to_stdout,
+    )
+  end
+end
