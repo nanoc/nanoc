fc692a0589e0a8f6e26d908790e8e4a78a1aab32
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-06 11:00:08 +0100

    Test null dependency tracker

--- spec/nanoc/base/services/dependency_tracker_spec.rb
+++ spec/nanoc/base/services/dependency_tracker_spec.rb
@@ -1,5 +1,5 @@
 describe Nanoc::Int::DependencyTracker do
-  subject(:tracker) { described_class.new(store) }
+  let(:tracker) { described_class.new(store) }
 
   let(:store) { Nanoc::Int::DependencyStore.new([]) }
 
@@ -7,9 +7,41 @@ describe Nanoc::Int::DependencyTracker do
   let(:item_b) { Nanoc::Int::Item.new('b', {}, '/b.md') }
   let(:item_c) { Nanoc::Int::Item.new('c', {}, '/c.md') }
 
+  shared_examples 'a null dependency tracker' do
+    let(:tracker) { Nanoc::Int::DependencyTracker::Null.new }
+
+    example do
+      expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_a) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_b) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_c) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.objects_outdated_due_to(item_a) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.objects_outdated_due_to(item_b) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.objects_outdated_due_to(item_c) }
+    end
+  end
+
   describe '#enter and exit' do
     context 'enter' do
-      subject { tracker.enter(item_a) }
+      subject do
+        tracker.enter(item_a)
+      end
+
+      it_behaves_like 'a null dependency tracker'
 
       example do
         expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_a) }
@@ -41,6 +73,8 @@ describe Nanoc::Int::DependencyTracker do
         tracker.enter(item_a)
         tracker.enter(item_b)
       end
+
+      it_behaves_like 'a null dependency tracker'
 
       it 'changes predecessors of item A' do
         expect { subject }.to change { store.objects_causing_outdatedness_of(item_a) }
@@ -77,6 +111,8 @@ describe Nanoc::Int::DependencyTracker do
         tracker.enter(item_c)
       end
 
+      it_behaves_like 'a null dependency tracker'
+
       it 'changes predecessors of item A' do
         expect { subject }.to change { store.objects_causing_outdatedness_of(item_a) }
           .from([]).to([item_b, item_c])
@@ -111,6 +147,8 @@ describe Nanoc::Int::DependencyTracker do
         tracker.bounce(item_b)
         tracker.enter(item_c)
       end
+
+      it_behaves_like 'a null dependency tracker'
 
       it 'changes predecessors of item A' do
         expect { subject }.to change { store.objects_causing_outdatedness_of(item_a) }
