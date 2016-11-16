69d1190070f060ca12dffbc6d24e754cbe84fe3d
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-16 09:01:06 +0100

    Add Nanoc::Extra::{Checking,Deployer} aliases

--- lib/nanoc/extra.rb
+++ lib/nanoc/extra.rb
@@ -1,8 +1,17 @@
+require 'nanoc/checking'
+require 'nanoc/deploying'
+
 # @api private
 module Nanoc::Extra
   autoload 'LinkCollector',       'nanoc/extra/link_collector.rb'
   autoload 'Piper',               'nanoc/extra/piper'
   autoload 'JRubyNokogiriWarner', 'nanoc/extra/jruby_nokogiri_warner'
+
+  # @deprecated
+  Checking = Nanoc::Checking
+
+  # @deprecated
+  Deployer = Nanoc::Deploying::Deployer
 end
 
 require 'nanoc/extra/core_ext'
