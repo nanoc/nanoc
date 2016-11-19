19060cf81a1ae3c5924acef9bf1acdfcec7c15e0
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-19 09:41:21 +0100

    Remove most autoloads from base.rb

--- lib/nanoc/base.rb
+++ lib/nanoc/base.rb
@@ -1,16 +1,5 @@
-module Nanoc
-  autoload 'Error',                'nanoc/base/error'
-  autoload 'Filter',               'nanoc/base/compilation/filter'
-end
-
 # @api private
 module Nanoc::Int
-  # Load helper classes
-  autoload 'Errors',               'nanoc/base/errors'
-  autoload 'Memoization',          'nanoc/base/memoization'
-  autoload 'PluginRegistry',       'nanoc/base/plugin_registry'
-
-  # Load compilation classes
   autoload 'Compiler',             'nanoc/base/compilation/compiler'
   autoload 'OutdatednessChecker',  'nanoc/base/compilation/outdatedness_checker'
   autoload 'OutdatednessReasons',  'nanoc/base/compilation/outdatedness_reasons'
@@ -18,9 +7,15 @@ end
 
 require_relative 'base/core_ext'
 require_relative 'base/contracts_support'
+require_relative 'base/memoization'
+require_relative 'base/plugin_registry'
+require_relative 'base/error'
+require_relative 'base/errors'
 
 require_relative 'base/entities'
 require_relative 'base/feature'
 require_relative 'base/repos'
 require_relative 'base/services'
 require_relative 'base/views'
+
+require_relative 'base/compilation/filter'
