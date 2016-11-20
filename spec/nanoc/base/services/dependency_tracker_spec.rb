3f1ff8e0854480a76405d130d335790968bf5422
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-20 11:34:35 +0100

    Add prop tests to DependencyTracker spec

--- spec/nanoc/base/services/dependency_tracker_spec.rb
+++ spec/nanoc/base/services/dependency_tracker_spec.rb
@@ -33,9 +33,21 @@ describe Nanoc::Int::DependencyTracker do
     example do
       expect { subject }.not_to change { store.objects_outdated_due_to(item_c) }
     end
+
+    example do
+      expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_a) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_b) }
+    end
+
+    example do
+      expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_c) }
+    end
   end
 
-  describe '#enter and exit' do
+  describe '#enter and #exit' do
     context 'enter' do
       subject do
         tracker.enter(item_a)
@@ -100,6 +112,142 @@ describe Nanoc::Int::DependencyTracker do
 
       example do
         expect { subject }.not_to change { store.objects_outdated_due_to(item_c) }
+      end
+    end
+
+    context 'enter + enter with props' do
+      subject do
+        tracker.enter(item_a)
+        tracker.enter(item_b, compiled_content: true)
+      end
+
+      it_behaves_like 'a null dependency tracker'
+
+      it 'changes predecessors of item A' do
+        expect { subject }.to change { store.objects_causing_outdatedness_of(item_a) }
+          .from([]).to([item_b])
+      end
+
+      it 'changes successors of item B' do
+        expect { subject }.to change { store.objects_outdated_due_to(item_b) }
+          .from([]).to([item_a])
+      end
+
+      it 'changes dependencies causing outdatedness of item A' do
+        expect { subject }.to change { store.dependencies_causing_outdatedness_of(item_a).size }
+          .from(0).to(1)
+      end
+
+      it 'creates correct new dependency causing outdatedness of item A' do
+        subject
+        dep = store.dependencies_causing_outdatedness_of(item_a)[0]
+
+        expect(dep.from).to eql(item_b)
+        expect(dep.to).to eql(item_a)
+      end
+
+      it 'creates dependency with correct props causing outdatedness of item A' do
+        subject
+        dep = store.dependencies_causing_outdatedness_of(item_a)[0]
+
+        expect(dep.compiled_content?).to eq(true)
+
+        expect(dep.raw_content?).to eq(false)
+        expect(dep.attributes?).to eq(false)
+        expect(dep.path?).to eq(false)
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_b) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_c) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_outdated_due_to(item_a) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_outdated_due_to(item_c) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_b) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_c) }
+      end
+    end
+
+    context 'enter + enter with prop + exit + enter with prop' do
+      subject do
+        tracker.enter(item_a)
+        tracker.enter(item_b, compiled_content: true)
+        tracker.exit
+        tracker.enter(item_b, attributes: true)
+      end
+
+      it_behaves_like 'a null dependency tracker'
+
+      it 'changes predecessors of item A' do
+        expect { subject }.to change { store.objects_causing_outdatedness_of(item_a) }
+          .from([]).to([item_b])
+      end
+
+      it 'changes successors of item B' do
+        expect { subject }.to change { store.objects_outdated_due_to(item_b) }
+          .from([]).to([item_a])
+      end
+
+      it 'changes dependencies causing outdatedness of item A' do
+        expect { subject }.to change { store.dependencies_causing_outdatedness_of(item_a).size }
+          .from(0).to(1)
+      end
+
+      it 'creates correct new dependency causing outdatedness of item A' do
+        subject
+        dep = store.dependencies_causing_outdatedness_of(item_a)[0]
+
+        expect(dep.from).to eql(item_b)
+        expect(dep.to).to eql(item_a)
+      end
+
+      it 'creates dependency with correct props causing outdatedness of item A' do
+        subject
+        dep = store.dependencies_causing_outdatedness_of(item_a)[0]
+
+        expect(dep.compiled_content?).to eq(true)
+        expect(dep.attributes?).to eq(true)
+
+        expect(dep.raw_content?).to eq(false)
+        expect(dep.path?).to eq(false)
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_b) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_causing_outdatedness_of(item_c) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_outdated_due_to(item_a) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.objects_outdated_due_to(item_c) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_b) }
+      end
+
+      example do
+        expect { subject }.not_to change { store.dependencies_causing_outdatedness_of(item_c) }
       end
     end
 
