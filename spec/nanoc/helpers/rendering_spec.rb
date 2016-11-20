6131624b4fa64586428e7be9a7d46879c45f63c9
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-20 09:54:26 +0100

    Pass dep properties to dependency tracker

--- spec/nanoc/helpers/rendering_spec.rb
+++ spec/nanoc/helpers/rendering_spec.rb
@@ -30,7 +30,8 @@ describe Nanoc::Helpers::Rendering, helper: true do
           it { is_expected.to eql('blah') }
 
           it 'tracks proper dependencies' do
-            expect(ctx.dependency_tracker).to receive(:enter).with(layout)
+            expect(ctx.dependency_tracker).to receive(:enter)
+              .with(layout, raw_content: false, attributes: false, compiled_content: false, path: false)
             subject
           end
         end
