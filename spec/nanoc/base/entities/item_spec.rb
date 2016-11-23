8119a8f2b92681e0d6b5316a6899a6d6f4860bac
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-23 20:36:06 +0100

    Remove useless #freeze spec
    Item#freeze is tested in the shared document examples.

--- spec/nanoc/base/entities/item_spec.rb
+++ spec/nanoc/base/entities/item_spec.rb
@@ -1,13 +1,3 @@
 describe Nanoc::Int::Item do
   it_behaves_like 'a document'
-
-  describe '#freeze' do
-    let(:item) { described_class.new('Hallo', { foo: { bar: 'asdf' } }, '/foo.md') }
-
-    before do
-      item.freeze
-    end
-
-    # FIXME: Where are the tests?
-  end
 end
