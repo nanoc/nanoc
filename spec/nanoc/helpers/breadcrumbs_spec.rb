f28deae0abb9fa9ac997556833022b933be316ee
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-17 08:29:26 +0100

    Refactor: split breadcrumbs spec in legacy/non-legacy

--- spec/nanoc/helpers/breadcrumbs_spec.rb
+++ spec/nanoc/helpers/breadcrumbs_spec.rb
@@ -7,55 +7,57 @@ describe Nanoc::Helpers::Breadcrumbs, helper: true do
   describe '#breadcrumbs_trail' do
     subject { helper.breadcrumbs_trail }
 
-    context 'root' do
-      before do
-        ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
+    context 'legacy identifiers' do
+      context 'root' do
+        before do
+          ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
 
-        ctx.item = ctx.items['/']
+          ctx.item = ctx.items['/']
+        end
+
+        it 'returns an array with the item' do
+          expect(subject).to eql([ctx.items['/']])
+        end
       end
 
-      it 'returns an array with the item' do
-        expect(subject).to eql([ctx.items['/']])
-      end
-    end
+      context 'root and direct child' do
+        before do
+          ctx.create_item('child', {}, Nanoc::Identifier.new('/foo/', type: :legacy))
+          ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
 
-    context 'root and direct child' do
-      before do
-        ctx.create_item('child', {}, Nanoc::Identifier.new('/foo/', type: :legacy))
-        ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
+          ctx.item = ctx.items['/foo/']
+        end
 
-        ctx.item = ctx.items['/foo/']
-      end
-
-      it 'returns an array with the items' do
-        expect(subject).to eql([ctx.items['/'], ctx.items['/foo/']])
-      end
-    end
-
-    context 'root, child and grandchild' do
-      before do
-        ctx.create_item('grandchild', {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
-        ctx.create_item('child', {}, Nanoc::Identifier.new('/foo/', type: :legacy))
-        ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
-
-        ctx.item = ctx.items['/foo/bar/']
+        it 'returns an array with the items' do
+          expect(subject).to eql([ctx.items['/'], ctx.items['/foo/']])
+        end
       end
 
-      it 'returns an array with the items' do
-        expect(subject).to eql([ctx.items['/'], ctx.items['/foo/'], ctx.items['/foo/bar/']])
+      context 'root, child and grandchild' do
+        before do
+          ctx.create_item('grandchild', {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
+          ctx.create_item('child', {}, Nanoc::Identifier.new('/foo/', type: :legacy))
+          ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
+
+          ctx.item = ctx.items['/foo/bar/']
+        end
+
+        it 'returns an array with the items' do
+          expect(subject).to eql([ctx.items['/'], ctx.items['/foo/'], ctx.items['/foo/bar/']])
+        end
       end
-    end
 
-    context 'root, missing child and grandchild' do
-      before do
-        ctx.create_item('grandchild', {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
-        ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
+      context 'root, missing child and grandchild' do
+        before do
+          ctx.create_item('grandchild', {}, Nanoc::Identifier.new('/foo/bar/', type: :legacy))
+          ctx.create_item('root', {}, Nanoc::Identifier.new('/', type: :legacy))
 
-        ctx.item = ctx.items['/foo/bar/']
-      end
+          ctx.item = ctx.items['/foo/bar/']
+        end
 
-      it 'returns an array with the items' do
-        expect(subject).to eql([ctx.items['/'], nil, ctx.items['/foo/bar/']])
+        it 'returns an array with the items' do
+          expect(subject).to eql([ctx.items['/'], nil, ctx.items['/foo/bar/']])
+        end
       end
     end
 
