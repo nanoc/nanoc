dee6af59eee15e26e672aa893857ec326628e97a
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-18 17:33:27 +0100

    Add contract to Props

--- lib/nanoc/base/entities/props.rb
+++ lib/nanoc/base/entities/props.rb
@@ -1,6 +1,9 @@
 module Nanoc::Int
   # @api private
   class Props
+    include Nanoc::Int::ContractsSupport
+
+    contract C::KeywordArgs[raw_content: C::Optional[C::Bool], attributes: C::Optional[C::Bool], compiled_content: C::Optional[C::Bool], path: C::Optional[C::Bool]] => C::Any
     def initialize(raw_content: false, attributes: false, compiled_content: false, path: false)
       @raw_content = raw_content
       @attributes = attributes
@@ -8,22 +11,39 @@ module Nanoc::Int
       @path = path
     end
 
+    contract C::None => String
+    def inspect
+      ''.tap do |s|
+        s << 'Props('
+        s << (raw_content? ? 'r' : '_')
+        s << (attributes? ? 'a' : '_')
+        s << (compiled_content? ? 'c' : '_')
+        s << (path? ? 'p' : '_')
+        s << ')'
+      end
+    end
+
+    contract C::None => C::Bool
     def raw_content?
       @raw_content
     end
 
+    contract C::None => C::Bool
     def attributes?
       @attributes
     end
 
+    contract C::None => C::Bool
     def compiled_content?
       @compiled_content
     end
 
+    contract C::None => C::Bool
     def path?
       @path
     end
 
+    contract Nanoc::Int::Props => Nanoc::Int::Props
     def merge(other)
       Props.new(
         raw_content: raw_content? || other.raw_content?,
@@ -33,6 +53,7 @@ module Nanoc::Int
       )
     end
 
+    contract C::None => Set
     def active
       Set.new.tap do |pr|
         pr << :raw_content if raw_content?
