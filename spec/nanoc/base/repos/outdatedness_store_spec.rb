9af7e60e9511776af1109a52aa2b75b63d9de236
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-31 11:57:09 +0100

    Test: Add spec for OutdatednessStore

---
+++ spec/nanoc/base/repos/outdatedness_store_spec.rb
@@ -0,0 +1,88 @@
+describe Nanoc::Int::OutdatednessStore do
+  subject(:store) { described_class.new(site: site, reps: reps) }
+
+  let(:site) { double(:site) }
+  let(:reps) { Nanoc::Int::ItemRepRepo.new }
+
+  let(:item) { Nanoc::Int::Item.new('foo', {}, '/foo.md') }
+  let(:rep) { Nanoc::Int::ItemRep.new(item, :foo) }
+
+  let(:site) do
+    Nanoc::Int::Site.new(
+      config: config,
+      code_snippets: code_snippets,
+      items: items,
+      layouts: layouts,
+    )
+  end
+
+  let(:config) { Nanoc::Int::Configuration.new.with_defaults }
+  let(:items) { [] }
+  let(:layouts) { [] }
+  let(:code_snippets) { [] }
+
+  describe '#include?, #add and #remove' do
+    subject { store.include?(rep) }
+
+    context 'nothing added' do
+      it { is_expected.not_to be }
+    end
+
+    context 'rep added' do
+      before { store.add(rep) }
+      it { is_expected.to be }
+    end
+
+    context 'rep added and removed' do
+      before do
+        store.add(rep)
+        store.remove(rep)
+      end
+
+      it { is_expected.not_to be }
+    end
+
+    context 'rep added, removed, and added again' do
+      before do
+        store.add(rep)
+        store.remove(rep)
+        store.add(rep)
+      end
+
+      it { is_expected.to be }
+    end
+  end
+
+  describe 'reloading' do
+    subject do
+      store.store
+      store.load
+      store.include?(rep)
+    end
+
+    context 'not added' do
+      context 'rep part of new reps' do
+        before { reps << rep }
+        it { is_expected.not_to be }
+      end
+
+      context 'rep not part of new reps' do
+        it { is_expected.not_to be }
+      end
+    end
+
+    context 'added' do
+      before { store.add(rep) }
+
+      context 'rep part of new reps' do
+        before { reps << rep }
+        it { is_expected.to be }
+      end
+
+      context 'rep not part of new reps' do
+        # FIXME: this should not be -- do not store expired info
+        it { is_expected.to be }
+      end
+    end
+  end
+end
