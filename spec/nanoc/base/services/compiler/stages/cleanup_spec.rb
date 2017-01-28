0c3b1ab49f537e225ccef5defca38060bdabb164
Denis Defreyne <denis.defreyne@stoneship.org>
2017-01-28 11:33:28 +0100

    Test: Add spec for deleting temp textual/binary items

--- spec/nanoc/base/services/compiler/stages/cleanup_spec.rb
+++ spec/nanoc/base/services/compiler/stages/cleanup_spec.rb
@@ -8,7 +8,25 @@ describe Nanoc::Int::Compiler::Stages::Cleanup do
   describe '#run' do
     subject { stage.run }
 
-    example do
+    it 'removes temporary binary items' do
+      a = Nanoc::Int::TempFilenameFactory.instance.create(Nanoc::Filter::TMP_BINARY_ITEMS_DIR)
+      File.write(a, 'hello there')
+
+      expect { subject }
+        .to change { File.file?(a) }
+        .from(true).to(false)
+    end
+
+    it 'removes temporary textual items' do
+      a = Nanoc::Int::TempFilenameFactory.instance.create(Nanoc::Int::ItemRepWriter::TMP_TEXT_ITEMS_DIR)
+      File.write(a, 'hello there')
+
+      expect { subject }
+        .to change { File.file?(a) }
+        .from(true).to(false)
+    end
+
+    it 'removes stores for unused output paths' do
       FileUtils.mkdir_p('tmp/nanoc/2f0692fb1a1d')
       FileUtils.mkdir_p('tmp/nanoc/1a2195bfef6c')
       FileUtils.mkdir_p('tmp/nanoc/1029d67644815')
