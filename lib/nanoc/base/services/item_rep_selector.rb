8a08cbedd2f1ec4e5799bc3ba621981b6544624f
Denis Defreyne <denis.defreyne@stoneship.org>
2017-01-29 09:44:06 +0100

    Add prioritised reps

--- lib/nanoc/base/services/item_rep_selector.rb
+++ lib/nanoc/base/services/item_rep_selector.rb
@@ -10,6 +10,7 @@ module Nanoc::Int
     def each
       graph = Nanoc::Int::DirectedGraph.new(@reps)
 
+      prioritised = Set.new
       loop do
         break if graph.roots.empty?
         rep = graph.roots.each { |e| break e }
@@ -18,7 +19,7 @@ module Nanoc::Int
           yield(rep)
           graph.delete_vertex(rep)
         rescue => e
-          handle_error(e, rep, graph)
+          handle_error(e, rep, graph, prioritised)
         end
       end
 
@@ -28,7 +29,7 @@ module Nanoc::Int
       end
     end
 
-    def handle_error(e, rep, graph)
+    def handle_error(e, rep, graph, prioritised)
       actual_error =
         if e.is_a?(Nanoc::Int::Errors::CompilationError)
           e.unwrap
@@ -37,14 +38,15 @@ module Nanoc::Int
         end
 
       if actual_error.is_a?(Nanoc::Int::Errors::UnmetDependency)
-        handle_dependency_error(actual_error, rep, graph)
+        handle_dependency_error(actual_error, rep, graph, prioritised)
       else
         raise(e)
       end
     end
 
-    def handle_dependency_error(e, rep, graph)
+    def handle_dependency_error(e, rep, graph, prioritised)
       other_rep = e.rep
+      prioritised << other_rep
       graph.add_edge(other_rep, rep)
       unless graph.vertices.include?(other_rep)
         graph.add_vertex(other_rep)
