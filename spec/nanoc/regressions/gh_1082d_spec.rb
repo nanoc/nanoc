2e005ae754bc71d4fef5bf961d562c77c54f889e
Denis Defreyne <denis.defreyne@stoneship.org>
2017-02-16 21:49:34 +0100

    Add regression specs for GH-1082

---
+++ spec/nanoc/regressions/gh_1082d_spec.rb
@@ -0,0 +1,17 @@
+describe 'GH-1082', site: true, stdio: true do
+  before do
+    File.write('content/a.erb', '<%= @item.reps[:default].binary? %>')
+
+    File.write('Rules', <<EOS)
+  compile '/*' do
+    filter :erb
+    write item.identifier.without_ext + '.txt'
+  end
+EOS
+  end
+
+  it 'does not require any items to be compiled' do
+    Nanoc::CLI.run(%w(compile))
+    expect(File.read('output/a.txt')).to eql('false')
+  end
+end
