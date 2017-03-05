9b02830f33fd028e4e5c1ca2ea94a733ae222979
Denis Defreyne <denis.defreyne@stoneship.org>
2017-03-05 11:14:25 +0100

    Fix undefined method reverse_each

---
+++ spec/nanoc/regressions/gh_1107_spec.rb
@@ -0,0 +1,13 @@
+describe 'GH-1107', site: true, stdio: true do
+  before do
+    File.write('Rules', <<EOS)
+  compile '/**/*.html' do
+    garbage(data/**/*)
+  end
+EOS
+  end
+
+  it 'raises the proper exception' do
+    expect { Nanoc::CLI.run(%w(compile --verbose)) }.to raise_error(SyntaxError)
+  end
+end
