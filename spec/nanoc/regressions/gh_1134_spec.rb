4f8ffacc328c6d62ea601c2da568c4d768479d24
Denis Defreyne <denis.defreyne@stoneship.org>
2017-03-26 14:18:38 +0200

    Check existance of all snapshot files, not just :last

---
+++ spec/nanoc/regressions/gh_1134_spec.rb
@@ -0,0 +1,27 @@
+describe 'GH-1134', site: true, stdio: true do
+  before do
+    File.write('content/foo.txt', 'asdf')
+
+    File.write('Rules', <<EOS)
+compile '/**/*' do
+  write '/first.html'
+  filter :erb
+  write '/last.html'
+end
+EOS
+  end
+
+  it 'detects missing output file of non-default rep' do
+    Nanoc::CLI.run(%w(compile))
+    expect(File.file?('output/first.html')).to be
+    expect(File.file?('output/last.html')).to be
+
+    FileUtils.rm_f('output/first.html')
+    expect(File.file?('output/first.html')).not_to be
+    expect(File.file?('output/last.html')).to be
+
+    Nanoc::CLI.run(%w(compile))
+    expect(File.file?('output/first.html')).to be
+    expect(File.file?('output/last.html')).to be
+  end
+end
