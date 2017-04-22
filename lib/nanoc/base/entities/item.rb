741b9659ae7d1b864d70f8d9ea66d117a2104301
Denis Defreyne <denis.defreyne@stoneship.org>
2017-04-22 12:27:15 +0200

    Remove obsolete comment

--- lib/nanoc/base/entities/item.rb
+++ lib/nanoc/base/entities/item.rb
@@ -1,11 +1,6 @@
 module Nanoc::Int
   # @api private
   class Item < ::Nanoc::Int::Document
-    # Returns an object that can be used for uniquely identifying objects.
-    #
-    # @api private
-    #
-    # @return [Object] An unique reference to this object
     def reference
       [:item, identifier.to_s]
     end
