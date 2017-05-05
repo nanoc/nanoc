1cf986c76b55bc6bc7aef551bb1ccbceec56c96b
Alexander Groß <agross@therightstuff.de>
2017-05-06 00:29:36 +0200

    Report file name when slim encounters compilation errors

--- test/filters/test_slim.rb
+++ test/filters/test_slim.rb
@@ -28,4 +28,28 @@ class Nanoc::Filters::SlimTest < Nanoc::TestCase
       assert_equal('<p>The rabbit is on the branch.</p>', result)
     end
   end
+
+  def new_view_context
+    Nanoc::ViewContext.new(
+      reps: :__irrelevat_reps,
+      items: :__irrelevat_items,
+      dependency_tracker: :__irrelevant_dependency_tracker,
+      compilation_context: :__irrelevat_compiler,
+      snapshot_repo: :__irrelevant_snapshot_repo,
+    )
+  end
+
+  def test_filter_slim_reports_filename
+    if_have 'slim' do
+      layout = Nanoc::Int::Layout.new('', {}, '/layout.slim')
+      layout = Nanoc::LayoutView.new(layout, new_view_context)
+
+      assigns = { layout: layout }
+
+      filter = ::Nanoc::Filters::Slim.new(assigns)
+
+      error = assert_raises(NameError) { filter.setup_and_run('deliberate=failure') }
+      assert_match(%r{^layout /layout.slim}, error.backtrace[1])
+    end
+  end
 end
