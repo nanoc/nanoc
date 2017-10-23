cdd5629d680f5cbc5c4eb65b1e15bdd61a9f2ae1
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-23 10:38:44 +0200

    Let live command serve

--- lib/nanoc/cli/commands/live.rb
+++ lib/nanoc/cli/commands/live.rb
@@ -2,14 +2,25 @@
 
 usage 'live'
 summary 'auto-recompile and serve'
-description 'TODO'
+description <<~EOS
+  Starts the live recompiler along with the static web server. Unless specified,
+  the web server will run on port 3000 and listen on all IP addresses. Running
+  this static web server requires `adsf` (not `asdf`!).
+EOS
+
+required :H, :handler, 'specify the handler to use (webrick/mongrel/...)'
+required :o, :host,    'specify the host to listen on (default: 127.0.0.1)'
+required :p, :port,    'specify the port to listen on (default: 3000)'
 
 module Nanoc::CLI::Commands
   class Live < ::Nanoc::CLI::CommandRunner
     def run
       self.class.enter_site_dir
 
-      # TODO: listen web
+      Thread.new do
+        Thread.current.abort_on_exception = true
+        Nanoc::CLI::Commands::View.new(options, [], self).run
+      end
 
       Nanoc::Extra::LiveRecompiler.new(command_runner: self).run
     end
