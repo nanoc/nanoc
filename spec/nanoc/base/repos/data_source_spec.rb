f2cf5b10f9bce3788047ee02a3a019caf05bb061
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-27 15:10:02 +0200

    Refactor: Convert data source tests into specs

--- spec/nanoc/base/repos/data_source_spec.rb
+++ spec/nanoc/base/repos/data_source_spec.rb
@@ -5,6 +5,64 @@ describe Nanoc::DataSource, stdio: true do
     described_class.new({}, nil, nil, {})
   end
 
+  it 'has an empty #up implementation' do
+    data_source.up
+  end
+
+  it 'has an empty #down implementation' do
+    data_source.down
+  end
+
+  it 'returns empty #items' do
+    expect(data_source.items).to be_empty
+  end
+
+  it 'returns empty #layouts' do
+    expect(data_source.layouts).to be_empty
+  end
+
+  describe '#new_item' do
+    it 'supports checksum data' do
+      item = data_source.new_item('stuff', { title: 'Stuff!' }, '/asdf', checksum_data: 'abcdef')
+
+      expect(item.content.string).to eql('stuff')
+      expect(item.attributes[:title]).to eql('Stuff!')
+      expect(item.identifier).to eql(Nanoc::Identifier.new('/asdf'))
+      expect(item.checksum_data).to eql('abcdef')
+    end
+
+    it 'supports content/attributes checksum data' do
+      item = data_source.new_item('stuff', { title: 'Stuff!' }, '/asdf', content_checksum_data: 'con-cs', attributes_checksum_data: 'attr-cs')
+
+      expect(item.content.string).to eql('stuff')
+      expect(item.attributes[:title]).to eql('Stuff!')
+      expect(item.identifier).to eql(Nanoc::Identifier.new('/asdf'))
+      expect(item.content_checksum_data).to eql('con-cs')
+      expect(item.attributes_checksum_data).to eql('attr-cs')
+    end
+  end
+
+  describe '#new_layout' do
+    it 'supports checksum data' do
+      layout = data_source.new_layout('stuff', { title: 'Stuff!' }, '/asdf', checksum_data: 'abcdef')
+
+      expect(layout.content.string).to eql('stuff')
+      expect(layout.attributes[:title]).to eql('Stuff!')
+      expect(layout.identifier).to eql(Nanoc::Identifier.new('/asdf'))
+      expect(layout.checksum_data).to eql('abcdef')
+    end
+
+    it 'supports content/attributes checksum data' do
+      layout = data_source.new_layout('stuff', { title: 'Stuff!' }, '/asdf', content_checksum_data: 'con-cs', attributes_checksum_data: 'attr-cs')
+
+      expect(layout.content.string).to eql('stuff')
+      expect(layout.attributes[:title]).to eql('Stuff!')
+      expect(layout.identifier).to eql(Nanoc::Identifier.new('/asdf'))
+      expect(layout.content_checksum_data).to eql('con-cs')
+      expect(layout.attributes_checksum_data).to eql('attr-cs')
+    end
+  end
+
   describe '#item_changes' do
     subject { data_source.item_changes }
 
