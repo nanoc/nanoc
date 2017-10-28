1592d8f8e22f47b2482209b4adfce2a95b8ee419
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-28 20:55:37 +0200

    Improve manifest spec assertions

--- spec/manifest_spec.rb
+++ spec/manifest_spec.rb
@@ -12,11 +12,11 @@ describe 'manifest', chdir: false do
 
   it 'contains all files in gemspec' do
     missing_from_manifest = gemspec_lines - manifest_lines
-    expect(missing_from_manifest).to be_empty
+    expect(missing_from_manifest).to be_empty, "Found files that appear in the gemspec, but not in the manifest: #{missing_from_manifest.join(', ')}"
   end
 
   it 'contains no files not in gemspec' do
     extra_in_manifest = manifest_lines - gemspec_lines
-    expect(extra_in_manifest).to be_empty
+    expect(extra_in_manifest).to be_empty, "Found files that appear in the manifest, but not in the gemspec: #{extra_in_manifest.join(', ')}"
   end
 end
