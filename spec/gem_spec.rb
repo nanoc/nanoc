8ff80ace6275e5e548f2af4cca86b214d51cc789
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-30 11:13:54 +0100

    Refactor: Convert gem tests to specs

---
+++ spec/gem_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+describe 'nanoc.gem', chdir: false, stdio: true do
+  around do |ex|
+    Dir['nanoc-*.gem'].each { |f| FileUtils.rm(f) }
+    ex.run
+    Dir['nanoc-*.gem'].each { |f| FileUtils.rm(f) }
+  end
+
+  subject do
+    piper = Nanoc::Extra::Piper.new(stdout: $stdout, stderr: $stderr)
+    piper.run(%w[gem build nanoc.gemspec], nil)
+  end
+
+  it 'builds gem' do
+    expect { subject }
+      .to change { Dir['**/*.gem'] }
+      .from([])
+      .to(include(match(/^nanoc-.*\.gem$/)))
+  end
+end
