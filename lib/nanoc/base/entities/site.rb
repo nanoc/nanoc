eeaf4fbd1f4c87bddc0d4ac634f52eb9f784b7d7
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-30 15:37:57 +0100

    Refactor: Convert rule tests to specs

--- lib/nanoc/base/entities/site.rb
+++ lib/nanoc/base/entities/site.rb
@@ -9,6 +9,7 @@ module Nanoc::Int
     attr_reader :config
     attr_accessor :data_source
 
+    # FIXME: make data source mandatory
     contract C::KeywordArgs[config: Nanoc::Int::Configuration, code_snippets: C::IterOf[Nanoc::Int::CodeSnippet], data_source: C::Maybe[C::Named['Nanoc::DataSource']]] => C::Any
     def initialize(config:, code_snippets:, data_source:)
       @config = config
