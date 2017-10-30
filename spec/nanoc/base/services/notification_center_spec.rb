92fbed5d9eef9a62b17cd00308ee81ddc6135c68
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-30 10:53:44 +0100

    Refactor: Convert notification center tests into specs

---
+++ spec/nanoc/base/services/notification_center_spec.rb
@@ -0,0 +1,23 @@
+describe Nanoc::Int::NotificationCenter do
+  it 'receives notification after subscribing' do
+    ping_received = false
+    Nanoc::Int::NotificationCenter.on :ping_received, :test do
+      ping_received = true
+    end
+
+    Nanoc::Int::NotificationCenter.post :ping_received
+    expect(ping_received).to be
+  end
+
+  it 'does not receive notification after unsubscribing' do
+    ping_received = false
+    Nanoc::Int::NotificationCenter.on :ping_received, :test do
+      ping_received = true
+    end
+
+    Nanoc::Int::NotificationCenter.remove :ping_received, :test
+
+    Nanoc::Int::NotificationCenter.post :ping_received
+    expect(ping_received).not_to be
+  end
+end
