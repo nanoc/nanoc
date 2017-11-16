21657ced26dd81a87a0082287105d644e8add6be
Denis Defreyne <denis.defreyne@stoneship.org>
2017-11-16 10:19:24 +0100

    Pass non-preprocessed output_dirs to cleanup stage

---
+++ spec/nanoc/regressions/gh_1248_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe 'GH-1248', site: true, stdio: true do
+  before do
+    File.write('content/stuff.html', 'hi')
+
+    File.write('Rules', <<~EOS)
+      preprocess do
+        @config[:output_dir] = 'ootpoot'
+      end
+
+      passthrough '/**/*'
+    EOS
+  end
+
+  before do
+    Nanoc::CLI.run(%w[compile])
+  end
+
+  example do
+    expect { Nanoc::CLI.run(%w[compile --verbose]) }
+      .not_to output(/identical .* ootpoot\/stuff.html/).to_stdout
+  end
+end
