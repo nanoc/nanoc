3d0f542d9434828f605d559aceae1b3f2684cf17
Denis Defreyne <denis.defreyne@stoneship.org>
2017-12-31 11:35:04 +0100

    Refactor: Let RoutingRule inherit from Rule

--- nanoc/lib/nanoc/rule_dsl/routing_rule.rb
+++ nanoc/lib/nanoc/rule_dsl/routing_rule.rb
@@ -1,46 +1,17 @@
 # frozen_string_literal: true
 
 module Nanoc::RuleDSL
-  # Contains the processing information for a item.
-  #
-  # @api private
-  class RoutingRule
+  class RoutingRule < Rule
     include Nanoc::Int::ContractsSupport
 
-    # @return [Symbol] The name of the representation that will be compiled
-    #   using this rule
-    attr_reader :rep_name
-
-    # @return [Symbol] The name of the snapshot this rule will apply to.
+    # contract C::None => C::Maybe[Symbol]
     attr_reader :snapshot_name
 
-    attr_reader :pattern
-
-    # Creates a new item routing rule.
-    #
-    # @param [Nanoc::Int::Pattern] pattern
-    #
-    # @param [String, Symbol] rep_name The name of the item representation
-    #   where this rule can be applied to
-    #
-    # @param [Proc] block A block that will be called when matching items are
-    #   compiled
-    #
-    # @param [Symbol, nil] snapshot_name The name of the snapshot this rule will
-    #   apply to.
+    contract Nanoc::Int::Pattern, Symbol, Proc, C::KeywordArgs[snapshot_name: C::Optional[Symbol]] => C::Any
     def initialize(pattern, rep_name, block, snapshot_name: nil)
-      @pattern = pattern
-      @rep_name = rep_name.to_sym
-      @snapshot_name = snapshot_name
-      @block = block
-    end
+      super(pattern, rep_name, block)
 
-    # @param [Nanoc::Int::Item] item The item to check
-    #
-    # @return [Boolean] true if this rule can be applied to the given item
-    #   rep, false otherwise
-    def applicable_to?(item)
-      @pattern.match?(item.identifier)
+      @snapshot_name = snapshot_name
     end
 
     contract Nanoc::Int::ItemRep, C::KeywordArgs[
@@ -53,16 +24,6 @@ module Nanoc::RuleDSL
       )
 
       context.instance_exec(matches(rep.item.identifier), &@block)
-    end
-
-    # Matches the rule regexp against items identifier and gives back group
-    # captures if any
-    #
-    # @param [String] identifier Identifier to capture groups for
-    #
-    # @return [nil, Array] Captured groups, if any
-    def matches(identifier)
-      @pattern.captures(identifier)
     end
   end
 end
