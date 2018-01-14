d1f27a65d652462d1875bf5fc394d3f2a7d049f5
Denis Defreyne <denis.defreyne@stoneship.org>
2018-01-14 15:04:51 +0100

    Convert parser tests into specs

---
+++ nanoc/spec/nanoc/data_sources/filesystem/parser_spec.rb
@@ -0,0 +1,390 @@
+# frozen_string_literal: true
+
+describe Nanoc::DataSources::Filesystem::Parser do
+  subject(:parser) { described_class.new(config: config) }
+
+  let(:config) do
+    Nanoc::Int::Configuration.new.with_defaults
+  end
+
+  describe '#call' do
+    subject { parser.call(content_filename, meta_filename) }
+
+    # common:
+    #   utf-8 bom
+    #   \r\n
+    #   metadata is empty
+    #   metadata is not hash
+
+    let(:content_filename) { nil }
+    let(:meta_filename) { nil }
+
+    context 'only meta file' do
+      let(:meta_filename) { Tempfile.open('test') { |fn| fn << 'asdf' }.path }
+
+      before do
+        File.write(meta_filename, meta)
+      end
+
+      context 'simple metadata' do
+        let(:meta) { "foo: bar\n" }
+
+        it 'reads attributes' do
+          expect(subject.attributes).to eq('foo' => 'bar')
+        end
+
+        it 'has no content' do
+          expect(subject.content).to eq('')
+        end
+      end
+
+      context 'UTF-8 bom' do
+        let(:meta) { [0xEF, 0xBB, 0xBF].map(&:chr).join + "foo: bar\r\n" }
+
+        it 'strips UTF-8 BOM' do
+          expect(subject.attributes).to eq('foo' => 'bar')
+        end
+
+        it 'has no content' do
+          expect(subject.content).to eq('')
+        end
+      end
+
+      context 'CRLF' do
+        let(:meta) { "foo: bar\r\n" }
+
+        it 'handles CR+LF line endings' do
+          expect(subject.attributes).to eq('foo' => 'bar')
+        end
+
+        it 'has no content' do
+          expect(subject.content).to eq('')
+        end
+      end
+
+      context 'metadata is empty' do
+        let(:meta) { '' }
+
+        it 'has no attributes' do
+          expect(subject.attributes).to eq({})
+        end
+
+        it 'has no content' do
+          expect(subject.content).to eq('')
+        end
+      end
+
+      context 'metadata is not hash' do
+        let(:meta) { "- stuff\n" }
+
+        it 'raises' do
+          expect { subject }
+            .to raise_error(Nanoc::DataSources::Filesystem::Errors::InvalidMetadata, /has invalid metadata \(expected key-value pairs, found Array instead\)/)
+        end
+      end
+    end
+
+    context 'only content file' do
+      let(:content_filename) { Tempfile.open('test') { |fn| fn << 'asdf' }.path }
+
+      before do
+        File.write(content_filename, content)
+      end
+
+      context 'no metadata section' do
+        context 'simple' do
+          let(:content) { "Hello!\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'UTF-8 bom' do
+          let(:content) { [0xEF, 0xBB, 0xBF].map(&:chr).join + "Hello!\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'strips UTF-8 BOM' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'CRLF' do
+          let(:content) { "Hello!\r\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'retains CR+LF' do
+            # FIXME: Is this the right thing to do?
+            expect(subject.content).to eq("Hello!\r\n")
+          end
+        end
+      end
+
+      context 'metadata section' do
+        context 'three dashes' do
+          let(:content) { "---\ntitle: Welcome\n---\nHello!\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'five dashes' do
+          let(:content) { "-----\ntitle: Welcome\n-----\nHello!\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'trailing spaces' do
+          let(:content) { "---   \ntitle: Welcome   \n---   \nHello!   \n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!   \n")
+          end
+        end
+
+        context 'diff' do
+          let(:content) { "--- a/foo\n+++ b/foo\nblah blah\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq(content)
+          end
+        end
+
+        context 'separator not at beginning' do
+          let(:content) { "foo\n---\ntitle: Welcome\n---\nStuff\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq(content)
+          end
+        end
+
+        context 'unterminated metadata section' do
+          let(:content) { "---\ntitle: Welcome\n" }
+
+          it 'raises' do
+            expect { subject }.to raise_error(Nanoc::DataSources::Filesystem::Errors::InvalidFormat)
+          end
+        end
+
+        context 'non-hash metadata section' do
+          let(:content) { "---\nWelcome\n---\nHello!\n" }
+
+          it 'raises' do
+            expect { subject }.to raise_error(Nanoc::DataSources::Filesystem::Errors::InvalidMetadata)
+          end
+        end
+
+        context 'empty metadata section' do
+          let(:content) { "---\n---\nHello!\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'leading newline' do
+          let(:content) { "---\ntitle: Welcome\n---\n\nHello!\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'two leading newlines' do
+          let(:content) { "---\ntitle: Welcome\n---\n\n\nHello!\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content with one leading newline' do
+            expect(subject.content).to eq("\nHello!\n")
+          end
+        end
+
+        context 'no content' do
+          let(:content) { "---\ntitle: Welcome\n---\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has no content' do
+            expect(subject.content).to eq('')
+          end
+        end
+
+        context 'UTF-8 bom' do
+          let(:content) { [0xEF, 0xBB, 0xBF].map(&:chr).join + "---\ntitle: Welcome\n---\nHello!\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'strips UTF-8 BOM' do
+            expect(subject.content).to eq("Hello!\n")
+          end
+        end
+
+        context 'CRLF' do
+          let(:content) { "---\r\ntitle: Welcome\r\n---\r\nHello!\r\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'retains CR+LF' do
+            # FIXME: Is this the right thing to do?
+            expect(subject.content).to eq("Hello!\r\n")
+          end
+        end
+
+        context 'four dashes' do
+          let(:content) { "----\ntitle: Welcome\n----\nHello!\n" }
+
+          it 'has no attributes' do
+            expect(subject.attributes).to eq({})
+          end
+
+          it 'has unparsed content' do
+            expect(subject.content).to eq(content)
+          end
+        end
+
+        context 'additional separators' do
+          let(:content) { "---\ntitle: Welcome\n---\nHello!\n---\nStuff\n" }
+
+          it 'has attributes' do
+            expect(subject.attributes).to eq('title' => 'Welcome')
+          end
+
+          it 'has content' do
+            expect(subject.content).to eq("Hello!\n---\nStuff\n")
+          end
+        end
+      end
+    end
+
+    context 'meta and content file' do
+      let(:content_filename) { Tempfile.open('test') { |fn| fn << 'asdf' }.path }
+      let(:meta_filename) { Tempfile.open('test') { |fn| fn << 'asdf' }.path }
+
+      before do
+        File.write(content_filename, content)
+        File.write(meta_filename, meta)
+      end
+
+      context 'simple' do
+        let(:content) { "Hello\n" }
+        let(:meta) { "title: Welcome\n" }
+
+        it 'has attributes' do
+          expect(subject.attributes).to eq('title' => 'Welcome')
+        end
+
+        it 'has content' do
+          expect(subject.content).to eq("Hello\n")
+        end
+      end
+
+      context 'apparent metadata section' do
+        let(:content) { "---\nauthor: Denis\n---\nHello!\n" }
+        let(:meta) { "title: Welcome\n" }
+
+        it 'has attributes' do
+          expect(subject.attributes).to eq('title' => 'Welcome')
+        end
+
+        it 'does not parse content' do
+          expect(subject.content).to eq(content)
+        end
+      end
+
+      context 'CRLF' do
+        let(:content) { "Hello!\r\n" }
+        let(:meta) { "title: Welcome\r\n" }
+
+        it 'has attributes' do
+          expect(subject.attributes).to eq('title' => 'Welcome')
+        end
+
+        it 'has content' do
+          # FIXME: Is this the right thing to do?
+          expect(subject.content).to eq("Hello!\r\n")
+        end
+      end
+
+      context 'UTF-8 bom' do
+        let(:content) { [0xEF, 0xBB, 0xBF].map(&:chr).join + "Hello!\n" }
+        let(:meta) { [0xEF, 0xBB, 0xBF].map(&:chr).join + "title: Welcome\n" }
+
+        it 'has attributes' do
+          expect(subject.attributes).to eq('title' => 'Welcome')
+        end
+
+        it 'has content' do
+          expect(subject.content).to eq("Hello!\n")
+        end
+      end
+    end
+  end
+
+  # test_parse_embedded_diff
+  # test_parse_embedded_empty_meta
+  # test_parse_embedded_full_meta
+  # test_parse_embedded_invalid_2
+  # test_parse_embedded_meta_only_1
+  # test_parse_embedded_meta_only_2
+  # test_parse_embedded_meta_only_3
+  # test_parse_embedded_no_meta
+  # test_parse_embedded_separators_but_not_metadata
+  # test_parse_embedded_with_extra_spaces
+  # test_parse_external
+  # test_parse_external_bad_metadata
+  # test_parse_internal_bad_metadata
+  # test_parse_internal_four_dashes
+  # test_parse_utf8_bom
+  # test_parse_with_one_blank_line_after_metadata
+  # test_parse_with_two_blank_lines_after_metadata
+end
