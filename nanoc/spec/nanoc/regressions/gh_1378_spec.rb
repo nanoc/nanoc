3f3c76d2525170e5d010d9f96e3270e1264adc64
Denis Defreyne <denis.defreyne@stoneship.org>
2018-12-01 09:35:19 +0100

    Allow Sass to import files even if they don’t have an item

---
+++ nanoc/spec/nanoc/regressions/gh_1378_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+describe 'GH-1378', site: true, stdio: true do
+  before do
+    FileUtils.mkdir_p('content')
+    File.write('outside.scss', 'p { color: red; }')
+    File.write('content/style.scss', '@import "../outside.scss";')
+
+    File.write('Rules', <<~EOS)
+      compile '/*' do
+        filter :sass, syntax: :scss
+        write ext: 'css'
+      end
+    EOS
+  end
+
+  example do
+    expect { Nanoc::CLI.run([]) }
+      .to change { File.file?('output/style.css') }
+      .from(false)
+      .to(true)
+
+    expect(File.read('output/style.css')).to match(/p\s*{\s*color:\s*red;\s*}/)
+  end
+end
