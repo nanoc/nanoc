3ad7062cf4796669cd53720eef87362fa4e00610
Denis Defreyne <denis.defreyne@stoneship.org>
2018-12-08 11:30:23 +0100

    Improve debug logging: Color messages based on type

--- nanoc/lib/nanoc/cli/commands/compile_listeners/debug_printer.rb
+++ nanoc/lib/nanoc/cli/commands/compile_listeners/debug_printer.rb
@@ -7,56 +7,77 @@ module Nanoc::CLI::Commands::CompileListeners
       command_runner.debug?
     end
 
+    COLOR_MAP = {
+      'compilation' => "\e[31m",
+      'cache' => "\e[32m",
+      'filtering' => "\e[33m",
+      'dependency_tracking' => "\e[34m",
+      'phase' => "\e[35m",
+    }.freeze
+
     # @see Listener#start
     def start
       on(:compilation_started) do |rep|
-        puts "*** Started compilation of #{rep}"
+        log('compilation', "Started compilation of #{rep}")
       end
 
       on(:compilation_ended) do |rep|
-        puts "*** Ended compilation of #{rep}"
-        puts
+        log('compilation', "Ended compilation of #{rep}")
+        log('compilation', '')
       end
 
       on(:compilation_suspended) do |rep, target_rep, snapshot_name|
-        puts "*** Suspended compilation of #{rep}: depends on #{target_rep}, snapshot #{snapshot_name}"
+        log('compilation', "Suspended compilation of #{rep}: depends on #{target_rep}, snapshot #{snapshot_name}")
       end
 
       on(:cached_content_used) do |rep|
-        puts "*** Used cached compiled content for #{rep} instead of recompiling"
+        log('cache', "Used cached compiled content for #{rep} instead of recompiling")
       end
 
       on(:filtering_started) do |rep, filter_name|
-        puts "*** Started filtering #{rep} with #{filter_name}"
+        log('filtering', "Started filtering #{rep} with #{filter_name}")
       end
 
       on(:filtering_ended) do |rep, filter_name|
-        puts "*** Ended filtering #{rep} with #{filter_name}"
+        log('filtering', "Ended filtering #{rep} with #{filter_name}")
       end
 
       on(:dependency_created) do |src, dst|
-        puts "*** Dependency created from #{src.inspect} onto #{dst.inspect}"
+        log('dependency_tracking', "Dependency created from #{src.inspect} onto #{dst.inspect}")
       end
 
       on(:phase_started) do |phase_name, rep|
-        puts "*** Phase started: #{phase_name} (rep: #{rep})"
+        log('phase', "Phase started: #{phase_name} (rep: #{rep})")
       end
 
       on(:phase_yielded) do |phase_name, rep|
-        puts "*** Phase yielded: #{phase_name} (rep: #{rep})"
+        log('phase', "Phase yielded: #{phase_name} (rep: #{rep})")
       end
 
       on(:phase_resumed) do |phase_name, rep|
-        puts "*** Phase resumed: #{phase_name} (rep: #{rep})"
+        log('phase', "Phase resumed: #{phase_name} (rep: #{rep})")
       end
 
       on(:phase_ended) do |phase_name, rep|
-        puts "*** Phase ended: #{phase_name} (rep: #{rep})"
+        log('phase', "Phase ended: #{phase_name} (rep: #{rep})")
       end
 
       on(:phase_aborted) do |phase_name, rep|
-        puts "*** Phase aborted: #{phase_name} (rep: #{rep})"
+        log('phase', "Phase aborted: #{phase_name} (rep: #{rep})")
       end
+    end
+
+    def log(progname, msg)
+      logger.info(progname) { msg }
+    end
+
+    def logger
+      @_logger ||=
+        Logger.new($stdout).tap do |l|
+          l.formatter = proc do |_severity, _datetime, progname, msg|
+            "*** #{COLOR_MAP[progname]}#{msg}\e[0m\n"
+          end
+        end
     end
   end
 end
