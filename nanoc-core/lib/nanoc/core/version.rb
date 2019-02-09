31b19328debc93a47d643ac7721a2593b15f6c30
Denis Defreyne <denis.defreyne@stoneship.org>
2019-02-09 12:47:37 +0100

    Set version to 4.11.1

--- nanoc-core/lib/nanoc/core/version.rb
+++ nanoc-core/lib/nanoc/core/version.rb
@@ -2,6 +2,6 @@
 
 module Nanoc
   module Core
-    VERSION = '4.11.0'
+    VERSION = '4.11.1'
   end
 end
--- nanoc/lib/nanoc/version.rb
+++ nanoc/lib/nanoc/version.rb
@@ -2,5 +2,5 @@
 
 module Nanoc
   # The current Nanoc version.
-  VERSION = '4.11.0'
+  VERSION = '4.11.1'
 end
