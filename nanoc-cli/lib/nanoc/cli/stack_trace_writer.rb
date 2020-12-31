272043410f9b2cfab1480c194d64ed0b82cc9b6f
Denis Defreyne <denis.defreyne@stoneship.org>
2020-12-31 12:22:22 +0100

    Always write stack trace backwards
    While Ruby 2.5 changed this to be forwards, Ruby 3.0 changes it back to the old-school backwards ordering.

--- nanoc-cli/lib/nanoc/cli/stack_trace_writer.rb
+++ nanoc-cli/lib/nanoc/cli/stack_trace_writer.rb
@@ -3,22 +3,11 @@
 module Nanoc
   module CLI
     class StackTraceWriter
-      def initialize(stream, forwards:)
+      def initialize(stream)
         @stream = stream
-        @forwards = forwards
       end
 
       def write(error, verbose:)
-        if @forwards
-          write_forwards(error, verbose: verbose)
-        else
-          write_backwards(error, verbose: verbose)
-        end
-      end
-
-      private
-
-      def write_backwards(error, verbose:)
         count = verbose ? -1 : 10
 
         error.backtrace[0...count].each_with_index do |item, index|
@@ -27,23 +16,6 @@ module Nanoc
 
         if !verbose && error.backtrace.size > count
           @stream.puts "  ... #{error.backtrace.size - count} lines omitted (see crash.log for details)"
-        end
-      end
-
-      def write_forwards(error, verbose:)
-        count = 10
-        backtrace = verbose ? error.backtrace : error.backtrace.take(count)
-
-        if !verbose && error.backtrace.size > count
-          @stream.puts "  ... #{error.backtrace.size - count} lines omitted (see crash.log for details)"
-        end
-
-        backtrace.each_with_index.to_a.reverse_each do |(item, index)|
-          if index.zero?
-            @stream.puts "  #{item}"
-          else
-            @stream.puts "  #{index}. from #{item}"
-          end
         end
       end
     end
--- nanoc-cli/spec/nanoc/cli/error_handler_spec.rb
+++ nanoc-cli/spec/nanoc/cli/error_handler_spec.rb
@@ -3,26 +3,6 @@
 describe Nanoc::CLI::ErrorHandler, stdio: true do
   subject(:error_handler) { described_class.new }
 
-  describe '#forwards_stack_trace?' do
-    subject { error_handler.forwards_stack_trace? }
-
-    context 'Ruby 2.4' do
-      before do
-        expect(error_handler).to receive(:ruby_version).and_return('2.4.2')
-      end
-
-      it { is_expected.to be(false) }
-    end
-
-    context 'Ruby 2.5' do
-      before do
-        expect(error_handler).to receive(:ruby_version).and_return('2.5.0')
-      end
-
-      it { is_expected.to be(true) }
-    end
-  end
-
   describe '#trivial?' do
     subject { error_handler.trivial?(error) }
 
