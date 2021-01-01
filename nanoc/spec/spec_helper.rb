2853113cf0c954c5011ef21bd69598478d66ef8d
Denis Defreyne <denis.defreyne@stoneship.org>
2021-01-01 21:14:08 +0100

    Debug why nanoc tests sometimes time out

--- nanoc/spec/spec_helper.rb
+++ nanoc/spec/spec_helper.rb
@@ -2,3 +2,13 @@
 
 require_relative '../../common/spec/spec_helper_head'
 require_relative '../../common/spec/spec_helper_foot'
+
+# Eww!
+require 'timeout'
+RSpec.configure do |c|
+  c.around do |ex|
+    puts "*** start #{ex.full_description}"
+    Timeout.timeout(10) { ex.run }
+    puts "*** end #{ex.full_description}"
+  end
+end
