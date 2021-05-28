d01e49416d109d56b495af43cebb559667449ffc
Denis Defreyne <denis.defreyne@stoneship.org>
2021-05-28 11:33:52 +0200

    Set version to 4.12.2

--- nanoc-cli/lib/nanoc/cli/version.rb
+++ nanoc-cli/lib/nanoc/cli/version.rb
@@ -2,6 +2,6 @@
 
 module Nanoc
   module CLI
-    VERSION = '4.12.1'
+    VERSION = '4.12.2'
   end
 end
--- nanoc-core/lib/nanoc/core/version.rb
+++ nanoc-core/lib/nanoc/core/version.rb
@@ -2,6 +2,6 @@
 
 module Nanoc
   module Core
-    VERSION = '4.12.1'
+    VERSION = '4.12.2'
   end
 end
