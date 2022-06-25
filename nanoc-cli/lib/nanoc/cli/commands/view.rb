292895fd11b166a9ebf5c0331543f659b4e66186
Denis Defreyne <denis.defreyne@stoneship.org>
2022-06-25 21:27:40 +0200

    Remove mentions of mongrel and use puma instead

--- nanoc-cli/lib/nanoc/cli/commands/view.rb
+++ nanoc-cli/lib/nanoc/cli/commands/view.rb
@@ -8,7 +8,7 @@ description <<~EOS
   `adsf` (not `asdf`!).
 EOS
 
-required :H, :handler, 'specify the handler to use (webrick/mongrel/...)'
+required :H, :handler, 'specify the handler to use (webrick/puma/...)'
 required :o, :host,    'specify the host to listen on (default: 127.0.0.1)', default: '127.0.0.1'
 required :p, :port,    'specify the port to listen on (default: 3000)', transform: Nanoc::CLI::Transform::Port, default: 3000
 flag :L, :'live-reload', 'reload on changes'
