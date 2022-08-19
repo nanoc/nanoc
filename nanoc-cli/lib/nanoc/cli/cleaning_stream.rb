4ae9bdb1da6d4a5010c55e1aa833d6ad7320db24
Jamie McCarthy <jamie@mccarthy.vg>
2022-08-19 17:53:30 -0500

    Update rubocop version and remove obsolete gem refs

--- nanoc-cli/lib/nanoc/cli/cleaning_stream.rb
+++ nanoc-cli/lib/nanoc/cli/cleaning_stream.rb
@@ -139,11 +139,9 @@ module Nanoc
       end
 
       # @see ARGF.set_encoding
-      # rubocop:disable Naming/AccessorMethodName, Lint/RedundantCopDisableDirective
       def set_encoding(*args)
         @stream.set_encoding(*args)
       end
-      # rubocop:enable Naming/AccessorMethodName, Lint/RedundantCopDisableDirective
 
       protected
 
