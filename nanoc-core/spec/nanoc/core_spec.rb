6867169e51585b747974d7c252db10efcb081627
Denis Defreyne <git@denisdefreyne.com>
2023-01-29 09:20:12 +0100

    Remove copyright end year

--- nanoc-core/spec/nanoc/core_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-describe Nanoc::Core do
-  it 'has up-to-date version information' do
-    current_year = Date.today.year
-    expect(described_class.version_information).to match(/–#{current_year} /)
-  end
-end
