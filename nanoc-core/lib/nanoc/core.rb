2266bb1b463bb014c45a4ebaeedf90da3b92dc74
Denis Defreyne <git@denisdefreyne.com>
2023-08-30 16:03:19 +0200

    Don’t refer to `Nanoc` namespace from core
    Nanoc::VERSION violates a boundary, but luckily we can use Nanoc::Core::VERSION easily.

--- nanoc-core/lib/nanoc/core.rb
+++ nanoc-core/lib/nanoc/core.rb
@@ -47,7 +47,7 @@ module Nanoc
     #
     # @api private
     def self.version_information
-      "Nanoc #{Nanoc::VERSION} © 2007–… Denis Defreyne.\n" \
+      "Nanoc #{Nanoc::Core::VERSION} © 2007–… Denis Defreyne.\n" \
       "Running #{RUBY_ENGINE} #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) on #{RUBY_PLATFORM} with RubyGems #{Gem::VERSION}.\n"
     end
 
