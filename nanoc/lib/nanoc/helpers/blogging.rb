ed1daca01ae971c990e1897fd4460081d8c49001
Denis Defreyne <git@denisdefreyne.com>
2023-12-08 09:03:56 +0100

    Fix Rubocop offenses

--- nanoc/lib/nanoc/helpers/blogging.rb
+++ nanoc/lib/nanoc/helpers/blogging.rb
@@ -120,8 +120,8 @@ module Nanoc::Helpers
           xml.updated(updated.__nanoc_to_iso8601_time)
 
           # Add links
-          xml.link(rel: 'alternate', href: (alt_link || root_url), type: 'text/html')
-          xml.link(rel: 'self',      href: feed_url,               type: 'application/atom+xml')
+          xml.link(rel: 'alternate', href: alt_link || root_url, type: 'text/html')
+          xml.link(rel: 'self',      href: feed_url,             type: 'application/atom+xml')
 
           # Add author information
           xml.author do
