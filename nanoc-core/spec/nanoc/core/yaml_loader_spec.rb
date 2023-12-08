d8509c39dd73ad8ab62da490d616109bae6d0812
Denis Defreyne <git@denisdefreyne.com>
2023-12-08 09:15:41 +0100

    Fix style

--- nanoc-core/spec/nanoc/core/yaml_loader_spec.rb
+++ nanoc-core/spec/nanoc/core/yaml_loader_spec.rb
@@ -2,29 +2,30 @@
 
 describe Nanoc::Core::YamlLoader do
   subject(:loader) { described_class }
+
   let(:yaml_input) do
     <<~YAML
-    point: &point_data
-      x: 42
-      y: 43
-    rect:
-      origin: *point_data
-      extent: {w: 10, h: 11}
+      point: &point_data
+        x: 42
+        y: 43
+      rect:
+        origin: *point_data
+        extent: {w: 10, h: 11}
     YAML
   end
   let(:expected_output) do
     {
-      'point' => {'x' => 42, 'y' => 43},
+      'point' => { 'x' => 42, 'y' => 43 },
       'rect' => {
-        'origin' => {'x' => 42, 'y' => 43},
-        'extent' => {'w' => 10, 'h' => 11},
-      }
+        'origin' => { 'x' => 42, 'y' => 43 },
+        'extent' => { 'w' => 10, 'h' => 11 },
+      },
     }
   end
 
   describe 'load' do
     it 'accepts YAML aliases' do
-      expect(loader.load yaml_input).to eq expected_output
+      expect(loader.load(yaml_input)).to eq expected_output
     end
   end
 end
