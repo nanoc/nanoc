64042d1f9c969e8bc8ae41ca82f82aa3b296a867
Denis Defreyne <git@denisdefreyne.com>
2024-11-03 09:20:05 +0100

    Improve documentation for when nanoc-live detects Guard::Nanoc

--- nanoc-live/lib/nanoc/live/command_runners/live.rb
+++ nanoc-live/lib/nanoc/live/command_runners/live.rb
@@ -10,9 +10,9 @@ module Nanoc
             $stderr.puts 'NOTE:'
             $stderr.puts 'You are using the `nanoc live` command provided by `nanoc-live`, but the `guard-nanoc` gem is also installed, which also provides a `nanoc live` command.'
             if defined?(Bundler)
-              $stderr.puts 'Recommendation: Remove `guard-nanoc` from your Gemfile.'
+              $stderr.puts 'Recommendation: Remove `guard-nanoc` from your Gemfile, either manually or by running the `bundle remove guard-nanoc` command.'
             else
-              $stderr.puts 'Recommendation: Uninstall `guard-nanoc`.'
+              $stderr.puts 'Recommendation: Uninstall `guard-nanoc` (run the `gem uninstall guard-nanoc` command).'
             end
             $stderr.puts '-' * 40
           end
