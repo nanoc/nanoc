3d4c53487985339d8cc22573bae6abaa14c09197
Denis Defreyne <git@denisdefreyne.com>
2025-11-16 10:06:41 +0100

    Refactor: Remove outdated `micro_graph` name in specs
    The ItemRepPriorityQueue used to be called a ‘micro graph’ a long time ago, but the name stuck in tests.

--- nanoc-core/spec/nanoc/core/item_rep_selector/item_rep_priority_queue_spec.rb
+++ nanoc-core/spec/nanoc/core/item_rep_selector/item_rep_priority_queue_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Nanoc::Core::ItemRepSelector::ItemRepPriorityQueue do
-  subject(:micro_graph) { described_class.new(reps) }
+  subject(:priority_queue) { described_class.new(reps) }
 
   let(:items) do
     [
@@ -25,192 +25,192 @@ describe Nanoc::Core::ItemRepSelector::ItemRepPriorityQueue do
 
   context 'when there are no dependencies' do
     it 'runs through reps in order' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
   context 'when is a simple dependency' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
   context 'when is a transitive dependency' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
   context 'when is a circular dependency of size 2' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      expect { micro_graph.mark_failed(reps[0]) }
+      expect(priority_queue.next).to eq(reps[2])
+      expect { priority_queue.mark_failed(reps[0]) }
         .to raise_error(Nanoc::Core::Errors::DependencyCycle)
     end
   end
 
   context 'when is a circular dependency of size 3' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      expect { micro_graph.mark_failed(reps[0]) }
+      expect(priority_queue.next).to eq(reps[4])
+      expect { priority_queue.mark_failed(reps[0]) }
         .to raise_error(Nanoc::Core::Errors::DependencyCycle)
     end
   end
 
   context 'when is a circular dependency of size 4' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_failed(reps[3])
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_failed(reps[3])
 
-      expect(micro_graph.next).to eq(reps[3])
-      expect { micro_graph.mark_failed(reps[0]) }
+      expect(priority_queue.next).to eq(reps[3])
+      expect { priority_queue.mark_failed(reps[0]) }
         .to raise_error(Nanoc::Core::Errors::DependencyCycle)
     end
   end
 
   context 'when there are two regular dependencies' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
   context 'when there is one regular dependency and one cyclical dependency' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      expect { micro_graph.mark_failed(reps[1]) }
+      expect(priority_queue.next).to eq(reps[4])
+      expect { priority_queue.mark_failed(reps[1]) }
         .to raise_error(Nanoc::Core::Errors::DependencyCycle)
     end
   end
 
   context 'when there is a transitive dependency' do
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[1])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[1])
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[3])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[3])
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
@@ -222,34 +222,34 @@ describe Nanoc::Core::ItemRepSelector::ItemRepPriorityQueue do
     # 2 -> 3
     # 3 OK
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[4])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[4])
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_failed(reps[2])
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_failed(reps[2])
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_failed(reps[3])
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_failed(reps[3])
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 
@@ -259,28 +259,28 @@ describe Nanoc::Core::ItemRepSelector::ItemRepPriorityQueue do
     # 1 -> 0
     # 0 OK
     it 'schedules the dependency next up' do
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_failed(reps[3])
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_failed(reps[3])
 
-      expect(micro_graph.next).to eq(reps[3])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[3])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_failed(reps[0])
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_failed(reps[0])
 
-      expect(micro_graph.next).to eq(reps[0])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[0])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[2])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[2])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[4])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[4])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to eq(reps[1])
-      micro_graph.mark_ok
+      expect(priority_queue.next).to eq(reps[1])
+      priority_queue.mark_ok
 
-      expect(micro_graph.next).to be_nil
+      expect(priority_queue.next).to be_nil
     end
   end
 end
