1d8c1d740b78b3862ba300702aa205398207f3fa
Denis Defreyne <git@denisdefreyne.com>
2025-11-27 10:01:52 +0100

    Don’t write error detail (redundant)
    I believe that long ago, Ruby’s exceptions did not implement
    `#full_message`, and that method was a convention that I had for my own
    exceptions.

--- nanoc-cli/lib/nanoc/cli/error_handler.rb
+++ nanoc-cli/lib/nanoc/cli/error_handler.rb
@@ -124,7 +124,6 @@ module Nanoc::CLI
       stream.puts 'Captain! We’ve been hit!'
 
       write_error_message(stream, error)
-      write_error_detail(stream, error)
       write_item_rep(stream, error)
       write_stack_trace(stream, error)
 
@@ -143,7 +142,6 @@ module Nanoc::CLI
       stream.puts "Crash log created at #{Time.now}"
 
       write_error_message(stream, error, verbose: true)
-      write_error_detail(stream, error)
       write_item_rep(stream, error, verbose: true)
       write_stack_trace(stream, error, verbose: true)
       write_version_information(stream, verbose: true)
@@ -301,15 +299,6 @@ module Nanoc::CLI
         "\n" + error.errors.map { |e| "  * #{e.pointer}: #{e.message}" }.join("\n")
       else
         error.message
-      end
-    end
-
-    def write_error_detail(stream, error)
-      error = unwrap_error(error)
-
-      if error.respond_to?(:full_message)
-        stream.puts
-        stream.puts error.full_message
       end
     end
 
